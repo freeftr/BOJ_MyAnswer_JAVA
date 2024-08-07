9279b9098161be6616e0bca50d30d8d6c8e87418
Pilot Park <130902228+freeftr@users.noreply.github.com>
2024-08-07 17:27:49 +0900

    [Silver III] Title: N과 M (1), Time: 328 ms, Memory: 33816 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int N, M;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        arr = new int[M+1];
+        func(0);
+        bw.write(sb.toString());
+        bw.close();
+    }
+    public static void func(int depth){
+        if(depth==M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <=N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[depth] = i;
+                func(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
