70563f8122e095dc2a3eb4d3455b99d829346bec
Pilot Park <130902228+freeftr@users.noreply.github.com>
2024-08-19 08:45:20 +0900

    [Silver III] Title: 모든 순열, Time: 264 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -4,6 +4,7 @@ public class Main {
     static int N;
     static boolean[] visited;
     static int[] answer;
+    static StringBuilder sb = new StringBuilder();
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,16 +13,14 @@ public class Main {
         visited = new boolean[N+1];
         answer = new int[N+1];
         permutation(1);
+        System.out.println(sb.toString());
     }
     public static void permutation(int depth) throws IOException{
         if(depth==N+1){
-            StringBuilder sb = new StringBuilder();
             for(int i = 1; i <= N; i++){
                 sb.append(answer[i] + " ");
             }
             sb.append("\n");
-            bw.write(sb.toString());
-            bw.flush();
             return;
         }
         for(int i = 1; i<=N; i++){
