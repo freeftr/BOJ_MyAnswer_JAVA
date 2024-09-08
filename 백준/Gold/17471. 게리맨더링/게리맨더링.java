4e8aa1dfb5d6e5cbba0251b4394f98881f9f361b
Pilot Park <130902228+freeftr@users.noreply.github.com>
2024-09-08 10:26:09 +0900

    [Gold III] Title: 게리맨더링, Time: 148 ms, Memory: 16340 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,101 +1,114 @@
 import java.io.*;
 import java.util.*;
-import java.lang.*;
-import java.util.concurrent.DelayQueue;
 
 public class Main {
+
     static int N;
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
     static int[] population;
-    static boolean[] visited;
-    static int min,sum=0;
-    public static void main(String[] args) throws IOException {
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int ans = Integer.MAX_VALUE;
+    static int sum = 0;
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        population = new int[N+1];
+
         for(int i = 0; i <= N; i++){
             graph.add(new ArrayList<>());
         }
-        //선거구를 두개로 나누고 모두 연결되어 있어야
-        //두개로 나누는데 인구차이를 최소로 구하기
-        population = new int[N+1];
+
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= N; i++){
             population[i] = Integer.parseInt(st.nextToken());
             sum+=population[i];
         }
-        min=sum;
-        for(int i = 1; i <= N; i++){
+
+        ans = sum;
+
+        for (int i = 1; i <= N; i++){
             st = new StringTokenizer(br.readLine());
-            int M = Integer.parseInt(st.nextToken());
-            for(int j = 0; j < M; j++){
-                int a = Integer.parseInt(st.nextToken());
-                graph.get(i).add(a);
+            int m = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < m; j++){
+                int b = Integer.parseInt(st.nextToken());
+                graph.get(i).add(b);
             }
         }
+
         for(int i = 1; i <= N/2; i++){
-            Deque<Integer> dq = new ArrayDeque<>();
-            combinations(0,i,dq,1);
+            ArrayDeque<Integer> dq1 = new ArrayDeque<>();
+            dfs(0,dq1,i,1);
         }
-        if(min==sum){
+        if(ans==sum){
             System.out.println(-1);
             System.exit(0);
         }
-        bw.write(min+"");
-        bw.close();
+
+        System.out.println(ans);
     }
-    //도시선택
-    public static void combinations(int depth, int target, Deque<Integer> dq, int start){
-        if(depth==target){
-            //연결확인
-            Deque<Integer> dq2 = new ArrayDeque<>();
-            visited = new boolean[N+1];
+
+    public static void dfs(int depth, ArrayDeque<Integer> dq1, int r, int start){
+        if(depth==r){
+            ArrayDeque<Integer> dq2 = new ArrayDeque<>();
+
             for(int i = 1; i <= N; i++){
-                if(!dq.contains(i)){
+                if(!dq1.contains(i)){
                     dq2.add(i);
                 }
             }
-            int a = 0, b=0;
-            if(bfs(dq) && bfs(dq2)){
-                for(int i: dq){
-                    a+=population[i];
+
+            if(bfs(dq1) && bfs(dq2)){
+                int sum1 = 0;
+                int sum2 = 0;
+                for(int i : dq1){
+                    sum1+=population[i];
                 }
-                for(int j: dq2){
-                    b+=population[j];
+                for(int i : dq2){
+                    sum2+=population[i];
                 }
-                if (Math.abs(a - b) < min) {
-                    min = Math.abs(a - b);
+
+                if(ans>Math.abs(sum1-sum2)){
+                    ans = Math.abs(sum1-sum2);
                 }
             }
+
             return;
         }
+
+
         for(int i = start; i <= N; i++){
-            dq.addLast(i);
-            combinations(depth+1,target,dq,start+1);
-            dq.removeLast();
+            dq1.addLast(i);
+            dfs(depth + 1, dq1, r, i +1 );
+            dq1.removeLast();
         }
     }
-    public static boolean bfs(Deque<Integer> dq){
-        Queue<Integer> q = new LinkedList<>();
+
+    public static boolean bfs(ArrayDeque<Integer> dq){
+        boolean[] visited = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque<>();
+        int cnt = 1;
+
         visited[dq.peekFirst()] = true;
         q.add(dq.peekFirst());
-        int cnt = 1;
+
         while(!q.isEmpty()){
-            int cur = q.poll();
-            for(int nv : graph.get(cur)){
-                if(visited[nv])continue;
+            int v = q.poll();
+            for(int nv : graph.get(v)){
                 if(!dq.contains(nv))continue;
-                cnt++;
-                visited[nv] = true;
-                q.add(nv);
+                if(!visited[nv]){
+                    q.add(nv);
+                    visited[nv] = true;
+                    cnt++;
+                }
             }
         }
-        if(cnt==dq.size()){
+
+        if(cnt == dq.size()){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
 }
