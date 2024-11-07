a9a8ecd3e814d723f28680217a636b626a6296fc
Pilot Park <130902228+freeftr@users.noreply.github.com>
2024-11-07 10:01:42 +0900

    [Gold II] Title: 물통, Time: 2000 ms, Memory: 316536 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14867. 물통/물통.java
+++ 백준/Gold/14867. 물통/물통.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 
     static int A, B, a, b;
-    static boolean[][] visited;
+    static HashSet<String> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,71 +17,84 @@ public class Main {
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[A+1][B+1];
-
         System.out.println(bfs());
     }
 
-
-    static int bfs(){
+    static int bfs() {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 0});
-        visited[0][0] = true;
-        while(!q.isEmpty()){
+        q.add(new int[] { 0, 0, 0 });
+        set.add(0 + " " + 0);
+
+        while (!q.isEmpty()) {
             int[] arr = q.poll();
             int nowA = arr[0], nowB = arr[1];
             int dist = arr[2];
 
-            if(nowA == a && nowB == b){
+            if (nowA == a && nowB == b) {
                 return dist;
             }
 
-            if(!visited[A][nowB]){
-                visited[A][nowB] = true;
-                q.add(new int[]{A, nowB, dist+1});
+            // 물통 A를 가득 채우는 경우
+            if (!set.contains(A + " " + nowB)) {
+                set.add(A + " " + nowB);
+                q.add(new int[] { A, nowB, dist + 1 });
             }
 
-            if(!visited[nowA][B]){
-                visited[nowA][B] = true;
-                q.add(new int[]{nowA, B, dist+1});
+            // 물통 B를 가득 채우는 경우
+            if (!set.contains(nowA + " " + B)) {
+                set.add(nowA + " " + B);
+                q.add(new int[] { nowA, B, dist + 1 });
             }
 
-            if(!visited[0][nowB]){
-                visited[0][nowB] = true;
-                q.add(new int[]{0, nowB, dist+1});
+            // 물통 A를 비우는 경우
+            if (!set.contains(0 + " " + nowB)) {
+                set.add(0 + " " + nowB);
+                q.add(new int[] { 0, nowB, dist + 1 });
             }
 
-            if(!visited[nowA][0]){
-                visited[nowA][0] = true;
-                q.add(new int[]{nowA, 0, dist+1});
+            // 물통 B를 비우는 경우
+            if (!set.contains(nowA + " " + 0)) {
+                set.add(nowA + " " + 0);
+                q.add(new int[] { nowA, 0, dist + 1 });
             }
 
-            //B -> A 인데 A가 넘치는 경우
-            if(nowA+nowB>A){
-                if(!visited[A][nowB-(A-nowA)]){
-                    visited[A][nowB-(A-nowA)] = true;
-                    q.add(new int[]{A, nowB-(A-nowA), dist+1});
-                }
-            }
-            //B -> A 인데 A가 안 넘치는 경우
-            if(nowA+nowB<=A){
-                if(!visited[nowA+nowB][0]){
-                    visited[nowA+nowB][0] = true;
-                    q.add(new int[]{nowA+nowB, 0, dist+1});
-                }
-            }
-
-            if(nowA+nowB>B){
-                if(!visited[nowA-(B-nowB)][B]){
-                    visited[nowA-(B-nowB)][B] = true;
-                    q.add(new int[]{nowA - (B-nowB), B, dist+1});
+            // B -> A로 이동하는 경우 (A가 넘치는 경우)
+            if (nowA + nowB > A) {
+                int newA = A;
+                int newB = nowB - (A - nowA);
+                if (!set.contains(newA + " " + newB)) {
+                    set.add(newA + " " + newB);
+                    q.add(new int[] { newA, newB, dist + 1 });
                 }
             }
 
-            if(nowA+nowB<=B){
-                if(!visited[0][nowA+nowB]){
-                    visited[0][nowA+nowB] = true;
-                    q.add(new int[]{0, nowA+nowB, dist+1});
+            // B -> A로 이동하는 경우 (A가 안 넘치는 경우)
+            if (nowA + nowB <= A) {
+                int newA = nowA + nowB;
+                int newB = 0;
+                if (!set.contains(newA + " " + newB)) {
+                    set.add(newA + " " + newB);
+                    q.add(new int[] { newA, newB, dist + 1 });
+                }
+            }
+
+            // A -> B로 이동하는 경우 (B가 넘치는 경우)
+            if (nowA + nowB > B) {
+                int newA = nowA - (B - nowB);
+                int newB = B;
+                if (!set.contains(newA + " " + newB)) {
+                    set.add(newA + " " + newB);
+                    q.add(new int[] { newA, newB, dist + 1 });
+                }
+            }
+
+            // A -> B로 이동하는 경우 (B가 안 넘치는 경우)
+            if (nowA + nowB <= B) {
+                int newA = 0;
+                int newB = nowA + nowB;
+                if (!set.contains(newA + " " + newB)) {
+                    set.add(newA + " " + newB);
+                    q.add(new int[] { newA, newB, dist + 1 });
                 }
             }
         }
