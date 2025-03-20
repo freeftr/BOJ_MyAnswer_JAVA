8534ed4398e4ce6587aa3b45863a6620622a05ee
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-03-20 09:13:16 +0900

    [Gold IV] Title: 두 로봇, Time: 808 ms, Memory: 93252 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/15971. 두 로봇/두 로봇.java
+++ 백준/Gold/15971. 두 로봇/두 로봇.java
@@ -6,62 +6,62 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	static int[][] cost;
-	static int ans = Integer.MAX_VALUE;
-	static boolean[] visited;
+    static int N;
+    static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+    static int ans = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		int robotA = Integer.parseInt(st.nextToken());
-		int robotB = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int robotA = Integer.parseInt(st.nextToken());
+        int robotB = Integer.parseInt(st.nextToken());
 
-		cost = new int[N+1][N+1];
-		visited = new boolean[N+1];
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-		for (int i = 0; i <= N; i++){
-			graph.add(new ArrayList<>());
-		}
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-		 for (int i = 0; i < N-1; i++) {
-			 st = new StringTokenizer(br.readLine());
-			 int a = Integer.parseInt(st.nextToken());
-			 int b = Integer.parseInt(st.nextToken());
+            graph.get(a).add(new int[] {b, cost});
+            graph.get(b).add(new int[] {a, cost});
+        }
 
-			 graph.get(a).add(b);
-			 graph.get(b).add(a);
-			 cost[a][b] = Integer.parseInt(st.nextToken());
-			 cost[b][a] =cost[a][b];
-		 }
-		 visited[robotA] = true;
-		 dfs(robotA,robotA,robotB, new ArrayList<>());
+        dfs(robotA, robotB, 0, 0, new boolean[N + 1]);
 
-		System.out.println(ans);
-	}
+        System.out.println(ans);
+    }
 
-	static void dfs(int v, int robotA, int robotB, ArrayList<Integer> path) {
-		if (v==robotB){
-			int max = 0;
-			int sum = 0;
-			for (int a : path){
-				max = Math.max(max, a);
-				sum += a;
-			}
-			ans = Math.min(ans, sum-max);
-			return;
-		}
+    static void dfs(int v, int robotB, int dist, int max, boolean[] visited) {
+        if (v == robotB) {
+            ans = Math.min(ans, dist - max);
+            return;
+        }
 
-		for (int nv : graph.get(v)){
-			if (visited[nv]) continue;
-			visited[nv] = true;
-			path.add(cost[v][nv]);
-			dfs(nv, robotA, robotB, path);
-			path.remove(path.size()-1);
-		}
-	}
+        visited[v] = true;
+
+        for (int[] edge : graph.get(v)) {
+            int nv = edge[0];
+            int weight = edge[1];
+
+            if (visited[nv]) continue;
+
+            int tempMax = max;
+            dist += weight;
+            max = Math.max(max, weight);
+
+            dfs(nv, robotB, dist, max, visited);
+
+            dist -= weight;
+            max = tempMax;
+        }
+
+        visited[v] = false;
+    }
 }
