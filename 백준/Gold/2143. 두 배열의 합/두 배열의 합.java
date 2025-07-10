132738abc1c59045512e34783d19daf19491e8db
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-07-10 15:17:42 +0900

    [Gold III] Title: 두 배열의 합, Time: 880 ms, Memory: 55120 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -1,79 +1,84 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
+    static int T, n, m;
+    static int[] arr1, arr2;
+    static ArrayList<Long> sumA = new ArrayList<>();
+    static ArrayList<Long> sumB = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        long T = Long.parseLong(br.readLine());
-        int n = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
 
-        int[] A = new int[n+1];
+        n = Integer.parseInt(br.readLine());
+        arr1 = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            A[i] = Integer.parseInt(st.nextToken()) + A[i-1];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
 
-        int m = Integer.parseInt(br.readLine());
-        int[] B = new int[m+1];
+        m = Integer.parseInt(br.readLine());
+        arr2 = new int[m];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= m; i++) {
-            B[i] = Integer.parseInt(st.nextToken()) + B[i-1];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
         }
 
-        ArrayList<Long> aSum = new ArrayList<>();
-        ArrayList<Long> bSum = new ArrayList<>();
-
-        // A의 모든 부분 합 구하기
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
-                aSum.add((long) (A[j] - A[i-1]));
+        for (int i = 0; i < n; i++) {
+            long sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr1[j];
+                sumA.add(sum);
             }
         }
 
-        // B의 모든 부분 합 구하기
-        for (int i = 1; i <= m; i++) {
-            for (int j = i; j <= m; j++) {
-                bSum.add((long) (B[j] - B[i-1]));
+        for (int i = 0; i < m; i++) {
+            long sum = 0;
+            for (int j = i; j < m; j++) {
+                sum += arr2[j];
+                sumB.add(sum);
             }
         }
 
-        // A의 부분합 리스트는 그대로 두고, B는 역순으로 정렬 (이분 탐색 준비)
-        Collections.sort(aSum);
-        Collections.sort(bSum, Collections.reverseOrder());
+        Collections.sort(sumB);
 
-        int left = 0;
-        int right = 0;
-        long ans = 0;
+        long answer = 0;
+        for (long a : sumA) {
+            long target = T - a;
+            int lower = lowerBound(sumB, target);
+            int upper = upperBound(sumB, target);
+            answer += (upper - lower);
+        }
 
-        // 투 포인터를 이용하여 두 리스트에서 합이 T가 되는 경우 찾기
-        while (left < aSum.size() && right < bSum.size()) {
-            long sumA = aSum.get(left);
-            long sumB = bSum.get(right);
-            long sum = sumA + sumB;
+        System.out.println(answer);
+    }
 
-            if (sum == T) {
-                long aCount = 0;
-                long bCount = 0;
-
-                while (left < aSum.size() && aSum.get(left) == sumA) {
-                    left++;
-                    aCount++;
-                }
-
-                while (right < bSum.size() && bSum.get(right) == sumB) {
-                    right++;
-                    bCount++;
-                }
-
-                ans += aCount * bCount;
-            } else if (sum < T) {
-                left++;
+    static int lowerBound(List<Long> list, long target) {
+        int left = 0, right = list.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) < target) {
+                left = mid + 1;
             } else {
-                right++;
+                right = mid;
             }
         }
+        return left;
+    }
 
-        System.out.println(ans);
+    static int upperBound(List<Long> list, long target) {
+        int left = 0, right = list.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 }
