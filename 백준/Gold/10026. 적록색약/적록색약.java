7a741425ff7fe893707a428cfc9d680a48f88d3a
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-07-10 14:57:06 +0900

    [Gold V] Title: 적록색약, Time: 148 ms, Memory: 17060 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,72 +1,109 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	static char[][] board;
-	static int N;
-	static boolean[][] visited;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int Ncnt = 0, Scnt = 0;
-		N = Integer.parseInt(br.readLine());
-		board = new char[N][N];
-		visited = new boolean[N][N];
-		for(int i = 0; i < N; i++) {
-			String s = br.readLine();
-			for(int j = 0; j < N; j++) {
-				board[i][j] = s.charAt(j);
-			}
-		}
-		//일반인 구역 구하기 
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					Ncnt++;
-					bfs(i, j);
-				}
-			}
-		}
-		visited = new boolean[N][N];
-		//색약 구역
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(board[i][j] == 'R') {
-					board[i][j] = 'G';
-				}
-			}
-		}
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					Scnt++;
-					bfs(i, j);
-				}
-			}
-		}
-		bw.write(Ncnt + " " + Scnt);
-		bw.close();
-	}
-	public static void bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {x, y});
-		visited[x][y] = true;
-		int[] dx = {1,-1,0,0};
-		int[] dy = {0,0,-1,1};
-		while(!q.isEmpty()) {
-			int[] arr = q.poll();
-			int X = arr[0];
-			int Y = arr[1];
-			for(int i = 0; i < 4; i++) {
-				int nx = X + dx[i];
-				int ny = Y + dy[i];
-				if(0 <= nx & nx < N && 0 <= ny && ny < N) {
-					if(!visited[nx][ny] && board[X][Y] == board[nx][ny]) {
-						visited[nx][ny] = true;
-						q.offer(new int[] {nx, ny});
-					}
-				}
-			}
-		}
-	}
+
+    static int N;
+    static char[][] map;
+    static boolean[][] visited;
+
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        int G = 0, R = 0, B = 0;
+
+        map = new char[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i][j]) continue;
+
+                bfs(i, j);
+
+                if (map[i][j] == 'G') {
+                    G++;
+                } else if (map[i][j] == 'R') {
+                    R++;
+                } else if (map[i][j] == 'B') {
+                    B++;
+                }
+            }
+        }
+
+        sb.append(G + R + B + " ");
+
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 'G') {
+                    map[i][j] = 'R';
+                }
+            }
+        }
+
+        G = 0;
+        R = 0;
+        B = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i][j]) continue;
+
+                bfs(i, j);
+
+                if (map[i][j] == 'G') {
+                    G++;
+                } else if (map[i][j] == 'R') {
+                    R++;
+                } else if (map[i][j] == 'B') {
+                    B++;
+                }
+            }
+        }
+        sb.append(R + B);
+
+        System.out.println(sb);
+    }
+
+    static void bfs(int a, int b) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{a, b});
+        visited[a][b] = true;
+
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                if (visited[nx][ny]) continue;
+
+                if (map[nx][ny]==map[a][b]) {
+                    q.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
 }
