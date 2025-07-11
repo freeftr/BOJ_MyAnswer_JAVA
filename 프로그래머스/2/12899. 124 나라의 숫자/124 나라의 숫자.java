da51f64c68acf80ed5427b70c4c6da9864be59f7
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-07-11 13:32:28 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,16 +1,21 @@
 class Solution {
     public String solution(int n) {
         StringBuilder answer = new StringBuilder();
+        
         while (n > 0) {
-            int num = n % 3;
-            n /= 3;
-            if (num == 0) {
+            int mod = n % 3;
+            if (mod == 0) {
                 answer.append("4");
-                n--;
+                n = n / 3 - 1;
+            } else if (mod == 1) {
+                answer.append("1");
+                n /= 3;
             } else {
-                answer.append(num);
+                answer.append("2");
+                n /= 3;
             }
         }
+        
         return answer.reverse().toString();
     }
 }
