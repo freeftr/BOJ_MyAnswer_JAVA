22f03e3149a1150931dff8630d4dd0bb0e01d4a2
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-08-15 11:20:48 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,23 +1,43 @@
-import java.util.*;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int cnt = 0;
-		int[] materials = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			materials[i] = sc.nextInt();
-		}
-		for(int i : materials) {
-			if(IntStream.of(materials).anyMatch(x -> x == M - i)) {
-				cnt++;
-			}
-		}
-		System.out.println(cnt/2);
-	}
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] components = new int[N];
+        for (int i = 0; i < N; i++) {
+            components[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(components);
+
+        int left = 0;
+        int right = N - 1;
+
+        int answer = 0;
+
+        while (left < right) {
+            int sum = components[left] + components[right];
+
+            if (sum < M) {
+                left++;
+            } else if (sum == M) {
+                answer++;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
