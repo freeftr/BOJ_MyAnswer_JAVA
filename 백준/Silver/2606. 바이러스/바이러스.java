4defe19ff1341fd26a23f062c41e258c9c1d5595
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-08-18 13:24:27 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,39 +1,62 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int N, M;
-    static boolean[] visited;
+
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        visited = new boolean[N+1];
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<Integer>());
+            graph.add(new ArrayList<>());
         }
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
-        dfs(1);
-        bw.write(depth-1+"");
-        bw.close();
+
+        int result = bfs(N);
+
+        System.out.println(result);
     }
-    static int depth=0;
-    static void dfs(int start){
-        visited[start] = true;
-        depth++;
-        for(int i : graph.get(start)){
-            int next = i;
-            if(!visited[i]){
-                dfs(next);
+
+    static int bfs(int N) {
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[N + 1];
+        int cnt = -1;
+
+        q.add(1);
+        visited[1] = true;
+
+        while (!q.isEmpty()) {
+            int v = q.poll();
+
+            cnt++;
+
+            for (int nv : graph.get(v)) {
+                if (visited[nv]) continue;
+
+                visited[nv] = true;
+                q.add(nv);
             }
         }
+
+        return cnt;
     }
 }
