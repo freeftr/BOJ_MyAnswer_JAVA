0de58a592397decb2aa28baebe153580c7542ad2
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-08-18 13:00:31 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 308 ms, Memory: 25092 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -1,36 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] square = new int[N+1][M+1];
-        for(int i = 1; i <= N; i++) {
-            String[] S = br.readLine().split("");
-            for(int j = 1; j <= M; j++) {
-                square[i][j] = Integer.parseInt(S[j-1]);
+
+        char[][] map = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j);
             }
         }
-        int[][] dp = new int[N+1][M+1];
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= M; j++) {
-                if(square[i][j]==1){
-                    int temp = Math.min(dp[i-1][j],dp[i][j-1]);
-                    dp[i][j] = Math.min(temp,dp[i-1][j-1])+1;
+
+        int[][] dp = new int[N + 1][M + 1];
+        int answer = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (map[i - 1][j - 1] =='1') {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i][j]) + 1;
+                    answer = Math.max(answer, dp[i][j]);
                 }
             }
         }
-        int max=0;
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j <= M; j++) {
-                if(max<=dp[i][j]){
-                    max=dp[i][j];
-                }
-            }
-        }
-        System.out.println(max*max);
+
+        System.out.println(answer * answer);
     }
 }
+
+/*
+0100
+0111
+1110
+0010
+
+0000
+0111
+012
+ */
