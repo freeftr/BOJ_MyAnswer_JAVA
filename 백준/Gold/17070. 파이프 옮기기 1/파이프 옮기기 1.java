39b0d0cd388b47eb2c8b1ddccac7a73338fb5a90
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-08-19 09:05:01 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,55 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int[][] map;
-    static int n, cnt=0;
-    public static void main(String[] args) throws IOException{
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        n = Integer.parseInt(br.readLine());
-        map = new int[n+2][n+2];
-        for(int i = 1; i <= n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= n; j++){
-                map[i][j]=Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[N][N];
+        int[][][] dp = new int[N][N][3];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        dfs(1,2,1);
-        System.out.println(cnt);
-    }
-    static public void dfs(int x, int y, int dir){
-        if(x==n && y==n){
-            cnt++;
-            return;
+
+        for (int i = 1; i < N; i++) {
+            if (map[0][i] == 1) break;
+            dp[0][i][0] = 1;
         }
-        if(x<=n && y<=n){
-            if(dir == 1){
-                if(map[x][y+1] == 0 && y+1<=n){
-                    dfs(x,y+1,1);
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                if (map[i][j] == 1) continue;
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][1];
+                if (map[i - 1][j] == 0 && map[i][j - 1] == 0) {
+                    dp[i][j][1] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
                 }
-                if(map[x][y+1]==0 && map[x+1][y]==0 && map[x+1][y+1] == 0 && x+1<=n && y+1<=n){
-                    dfs(x+1,y+1,2);
-                }
-            }
-            if(dir == 2){
-                if(map[x][y+1] == 0 && y+1<=n){
-                    dfs(x,y+1,1);
-                }
-                if(map[x+1][y] == 0 && x+1<=n){
-                    dfs(x+1,y,3);
-                }
-                if(map[x][y+1]==0 && map[x+1][y]==0 && map[x+1][y+1] == 0 && x+1<=n && y+1<=n){
-                    dfs(x+1,y+1,2);
-                }
-            }
-            if(dir == 3){
-                if(map[x+1][y] == 0 && x+1<=n){
-                    dfs(x+1,y,3);
-                }
-                if(map[x][y+1]==0 && map[x+1][y]==0 && map[x+1][y+1] == 0 && x+1<=n && y+1<=n){
-                    dfs(x+1,y+1,2);
-                }
+                dp[i][j][2] = dp[i - 1][j][2] + dp[i - 1][j][1];
             }
         }
+
+        int answer = dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2] == 0 ? 0 : dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2];
+        System.out.println(answer);
     }
 }
