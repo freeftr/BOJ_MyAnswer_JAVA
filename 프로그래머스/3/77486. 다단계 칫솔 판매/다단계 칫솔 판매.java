27800f421be89d61a3316c8b1ee9375b71bf212f
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-08-21 22:42:10 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 25.44 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,62 +1,77 @@
 import java.util.*;
-
 class Solution {
     
-    static ArrayList<Node> emps = new ArrayList<>();
-    static HashMap<String, Integer> empCache = new HashMap<>();
+    static ArrayList<Node> list = new ArrayList<>();
+    static HashMap<String, Node> map = new HashMap<>();
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = {};
+        
+        
+        map.put("minho", new Node(0, "minho"));
+        
+        for (int i = 0; i < enroll.length; i++) {
+            Node node = new Node(0, enroll[i]);
+            map.put(enroll[i], node);
+        }
+        
+        for (int i = 0; i < referral.length; i++) {
+            Node child = map.get(enroll[i]);
+            if (referral[i].equals("-")) {
+                child.makeParent(map.get("minho"));
+            } else {
+                child.makeParent(map.get(referral[i]));
+            }
+        }
+        
+        for (int i = 0; i < seller.length; i++) {
+            int fee = amount[i] * 100;
+            String sellerName = seller[i];
+            
+            Node sell = map.get(sellerName);
+            sell.pay(fee);
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < enroll.length; i++) {
+            int balance = map.get(enroll[i]).balance;
+            result.add(balance);
+        }
+        
+        answer = new int[result.size()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = result.get(i);
+        }
+        
+        return answer;
+    }
     
     static class Node {
         int balance;
-        String name;
         Node parent;
+        String name;
         
-        public Node(int balance, String name, Node parent) {
+        Node (int balance, String name) {
             this.balance = balance;
             this.name = name;
+        }
+        
+        public void makeParent(Node parent) {
             this.parent = parent;
         }
         
-        public void payFee(int money) {
-            if (this.parent == null || money < 1) return;
-            int fee = money / 10;
-            this.balance += (money - fee);
-            this.parent.payFee(fee);
-        }
-    }
-    
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int idx = 0;
-        
-        emps.add(new Node(0, "minho", null));
-        empCache.put("minho", idx++);
-        
-        for (String name : enroll) {
-            emps.add(new Node(0, name, null));
-            empCache.put(name, idx++);
-        }
+         public void pay(int fee) {
+            int commission = fee / 10;
+            int keep = fee - commission;
+            this.balance += keep;
 
-        for (int i = 0; i < enroll.length; i++) {
-            if (!referral[i].equals("-")) {
-                int nowId = empCache.get(enroll[i]);
-                int parentId = empCache.get(referral[i]);
-                emps.get(nowId).parent = emps.get(parentId);
-            } else {
-                int nowId = empCache.get(enroll[i]);
-                emps.get(nowId).parent = emps.get(0);
+            if (commission == 0) return;
+
+            if (parent != null) {
+                parent.pay(commission);
             }
         }
-
-        for (int i = 0; i < seller.length; i++) {
-            int id = empCache.get(seller[i]);
-            int money = amount[i] * 100;
-            emps.get(id).payFee(money);
-        }
-
-        int[] answer = new int[enroll.length];
-        for (int i = 0; i < enroll.length; i++) {
-            answer[i] = emps.get(empCache.get(enroll[i])).balance;
-        }
-
-        return answer;
     }
 }
