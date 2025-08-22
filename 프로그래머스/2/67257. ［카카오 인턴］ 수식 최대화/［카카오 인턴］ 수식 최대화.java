c6988dc7bd3c04044f0860d074ed8514dff84158
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-08-22 12:21:49 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 3.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,65 +1,64 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    
-    static String[] operations = new String[]{"+", "-", "*"};
+
+    static ArrayList<Long> nums = new ArrayList<>();
+    static ArrayList<String> ops = new ArrayList<>();
+    static String[] operations = {"+", "-", "*"};
 
     public long solution(String expression) {
         long answer = 0;
-        
-        ArrayList<String> words = new ArrayList<>();
-        StringBuilder s = new StringBuilder();
-        
-        for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
-            if (ch == '*' || ch == '+' || ch == '-') {
-                if (s.length() > 0) words.add(s.toString());
-                words.add(String.valueOf(ch));
-                s.setLength(0);
-            } else {
-                s.append(ch);
-            }
-        }
-        if (s.length() > 0) words.add(s.toString());
+        rebuild(expression);
 
         for (int a = 0; a < 3; a++) {
             for (int b = 0; b < 3; b++) {
-                if (a == b) continue;
                 for (int c = 0; c < 3; c++) {
-                    if (c == a || c == b) continue;
-                    long result = calculate(operations[a], operations[b], operations[c], words);
+                    if (a == b || b == c || a == c) continue;
+                    rebuild(expression);
+                    long result = calculate(a, b, c);
                     answer = Math.max(answer, Math.abs(result));
                 }
             }
         }
         return answer;
     }
-    
-    static long calculate(String first, String second, String third, ArrayList<String> words) {
-        ArrayList<Long> nums = new ArrayList<>();
-        ArrayList<String> ops = new ArrayList<>();
 
-        for (String word : words) {
-            if (word.equals("*") || word.equals("+") || word.equals("-")) {
-                ops.add(word);
+    static void rebuild(String expression) {
+        nums = new ArrayList<>();
+        ops  = new ArrayList<>();
+        Deque<String> dq = new ArrayDeque<>();
+        String[] s = expression.split("");
+        for (int i = 0; i < s.length; i++) {
+            String ch = s[i];
+            if (ch.equals("+") || ch.equals("-") || ch.equals("*")) {
+                StringBuilder temp = new StringBuilder();
+                while (!dq.isEmpty()) temp.append(dq.pollFirst());
+                if (temp.length() != 0) nums.add(Long.parseLong(temp.toString()));
+                ops.add(ch);
             } else {
-                nums.add(Long.parseLong(word));
+                dq.addLast(ch);
             }
         }
-        
-        String[] order = { first, second, third };
-        for (String op : order) {
-            for (int i = 0; i < ops.size(); ) {
-                if (ops.get(i).equals(op)) {
-                    long a = nums.get(i);
-                    long b = nums.get(i + 1);
-                    long r;
-                    if (op.equals("*"))      r = a * b;
-                    else if (op.equals("+")) r = a + b;
-                    else                     r = a - b;
+        if (!dq.isEmpty()) {
+            StringBuilder temp = new StringBuilder();
+            while (!dq.isEmpty()) temp.append(dq.pollFirst());
+            nums.add(Long.parseLong(temp.toString()));
+        }
+    }
 
-                    nums.set(i, r);
+    static long calculate(int a, int b, int c) {
+        String[] order = {operations[a], operations[b], operations[c]};
+        for (String od : order) {
+            int i = 0;
+            while (i < ops.size()) {
+                if (ops.get(i).equals(od)) {
+                    long left = nums.get(i);
+                    long right = nums.get(i + 1);
+                    long val;
+                    if (od.equals("+")) val = left + right;
+                    else if (od.equals("-")) val = left - right;
+                    else val = left * right;
+                    nums.set(i, val);
                     nums.remove(i + 1);
                     ops.remove(i);
                 } else {
