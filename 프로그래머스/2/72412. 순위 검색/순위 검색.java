a82fdf49dfc32f1f56acbafeef165ad6dee5c6be
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-08-26 11:36:35 +0900

    [level 2] Title: 순위 검색, Time: 970.82 ms, Memory: 198 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,59 +1,63 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static HashMap<String, ArrayList<Integer>> infoMap = new HashMap<>();
-
+    
+    static HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+    
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-
-        for (String s : info) {
-            String[] elements = s.split(" ");
-            int score = Integer.parseInt(elements[4]);
-            comb(0, elements, "", score);
+        
+        for (String inf : info) {
+            String[] s = inf.split(" ");
+            dfs(0, s, "");
         }
 
-        for (ArrayList<Integer> list : infoMap.values()) {
+        for (ArrayList<Integer> list : map.values()) {
             Collections.sort(list);
         }
-
-        for (int i = 0; i < query.length; i++) {
-            String q = query[i].replaceAll(" and", "");
-            String[] parts = q.split(" ");
-            String key = parts[0] + parts[1] + parts[2] + parts[3];
-            int targetScore = Integer.parseInt(parts[4]);
-
-            ArrayList<Integer> scores = infoMap.getOrDefault(key, new ArrayList<>());
-            int idx = lowerBound(scores, targetScore);
-            answer[i] = scores.size() - idx;
-        }
-
-        return answer;
-    }
-
-    static void comb(int depth, String[] elements, String current, int score) {
-        if (depth == 4) {
-            infoMap.computeIfAbsent(current, k -> new ArrayList<>()).add(score);
-            return;
-        }
-
-        comb(depth + 1, elements, current + elements[depth], score);
-        comb(depth + 1, elements, current + "-", score);
-    }
-
-    static int lowerBound(List<Integer> list, int target) {
-        int left = 0;
-        int right = list.size();
-
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (list.get(mid) < target) {
-                left = mid + 1;
+        
+        int idx = 0;
+        for (String s : query) {
+            String temp = s.replace(" and", "");
+            String[] elements = temp.split(" ");
+            int target = Integer.parseInt(elements[elements.length - 1]);
+            String key = "";
+            
+            for (int i = 0; i < elements.length - 1; i++) {
+                key += elements[i];
+            }
+            
+            ArrayList<Integer> scores = map.get(key);
+            
+            int cnt = 0;
+            if (scores == null) {
+                answer[idx++] = 0;
             } else {
-                right = mid;
+                int left = 0;
+                int right = scores.size();
+                
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (scores.get(mid) < target) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                answer[idx++] = scores.size() - left;
             }
         }
-
-        return left;
+        
+        return answer;
+    }
+    
+    static void dfs(int depth, String[] info, String cur) {
+        if (depth == 4) {
+            map.computeIfAbsent(cur, k -> new ArrayList<Integer>()).add(Integer.parseInt(info[4]));
+            return;
+        }
+        
+        dfs(depth + 1, info, cur + info[depth]);
+        dfs(depth + 1, info, cur + "-");
     }
 }
