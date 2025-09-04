201919d711fd851b1fa64c7f168339c4cc72d68f
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-05 08:50:36 +0900

    [level 2] Title: [1차] 캐시, Time: 33.00 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,29 +1,34 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
+        
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        
+        for (int i = 0; i < cities.length; i++) {
+            cities[i] = cities[i].toLowerCase();
+        }
+        
         ArrayList<String> cache = new ArrayList<>();
         
-        if (cacheSize==0) {
-            return cities.length*5;
-        }
-        
         for (String city : cities) {
-            if (cache.contains(city.toLowerCase())) {
-                cache.remove(city.toLowerCase());
-                cache.add(city.toLowerCase());
+            if (cache.contains(city)) {
+                cache.remove(cache.indexOf(city));
+                cache.add(city);
                 answer++;
             } else {
-                if (cache.size() >= cacheSize) {
+                if (cache.size() < cacheSize) {
+                    cache.add(city);
+                } else {
                     cache.remove(0);
+                    cache.add(city);
                 }
-                cache.add(city.toLowerCase());
-                answer+=5;
+                answer += 5;
             }
         }
-        
         return answer;
     }
 }
