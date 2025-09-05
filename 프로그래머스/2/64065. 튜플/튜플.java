b68fe62b9657e245970f0d39fd35abff9957a52f
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-05 10:02:18 +0900

    [level 2] Title: 튜플, Time: 141.77 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,32 +1,38 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
         int[] answer = {};
-        s = s.substring(2);
-        s = s.substring(0, s.length() - 2);
-        String[] tuples = s.split("},\\{");
-        Arrays.sort(tuples, (a, b) -> a.length() - b.length());
         
-        ArrayList<Integer> list = new ArrayList<>();
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> a.length() - b.length());
+        s = s.substring(2, s.length() - 2);
+        String[] tuples = s.split("\\},\\{");
         
         for (String tuple : tuples) {
-            String[] temps = tuple.split(",");
-            for (String temp : temps) {
-                int a = Integer.parseInt(temp);
-                
-                if (list.contains(a)) {
-                    continue;
-                } else {
-                    list.add(a);
-                }
+            pq.add(tuple);
+        }
+        ArrayList<String> result = new ArrayList<>();
+        
+        while (!pq.isEmpty()) {
+            String cur = pq.poll();
+            String[] temp = cur.split(",");
+            
+            for (String t : temp) {
+                if (result.contains(t)) continue;
+                result.add(t);
             }
         }
-        answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+        
+        answer = new int[result.size()];
+        
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = Integer.parseInt(result.get(i));
         }
+        
         return answer;
     }
 }
+
+/*
+- 순서가 다르면 다른 튜플
+*/
