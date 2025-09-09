1114400ee390b99a5a21a1eeab4522a9ba21d01a
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-09 16:29:21 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 7.89 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,24 +1,34 @@
 import java.util.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        int deliverySum = 0;
-        int pickupSum = 0;
-
+        int del = 0;
+        int pick = 0;
+        
         for (int i = n - 1; i >= 0; i--) {
             int cnt = 0;
-            deliverySum += deliveries[i];
-            pickupSum += pickups[i];
-
-            while (deliverySum > 0 || pickupSum > 0) {
-                deliverySum -= cap;
-                pickupSum -= cap;
+            del += deliveries[i];
+            pick += pickups[i];
+            
+            while (del > 0 || pick > 0) {
+                del -= cap;
+                pick -= cap;
                 cnt++;
             }
-
+            
             answer += (i + 1) * 2L * cnt;
         }
-
+        
         return answer;
     }
 }
+
+/*
+n개의 집에 배달.
+cap만큼 실을 수 있음.
+각 집에 배달하고 빈거 가져오기.
+
+- 가장 먼집부터 처리
+- 최소 이동 거리 구하기
+*/
