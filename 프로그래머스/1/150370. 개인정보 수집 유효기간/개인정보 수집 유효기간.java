a5d8e5c713b5e8e18ec9ffaf6a2f045dd909a348
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-09 16:10:34 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.25 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,44 +1,55 @@
 import java.util.*;
 
 class Solution {
-
-    static HashMap<String, Integer> termsMap = new HashMap<>();
-
     public int[] solution(String today, String[] terms, String[] privacies) {
-        String[] todayArr = today.split("\\.");
-        int curYear = Integer.parseInt(todayArr[0]);
-        int curMonth = Integer.parseInt(todayArr[1]);
-        int curDay = Integer.parseInt(todayArr[2]);
-        int todayTotal = curYear * 12 * 28 + curMonth * 28 + curDay;
-
-        for (String term : terms) {
-            String[] t = term.split(" ");
-            termsMap.put(t[0], Integer.parseInt(t[1]));
-        }
-
+        int[] answer = {};
+        int length = privacies.length;
+        HashMap<String, Integer> termMap = new HashMap<>();
         ArrayList<Integer> result = new ArrayList<>();
-
-        for (int i = 0; i < privacies.length; i++) {
-            String[] parts = privacies[i].split(" ");
-            String[] date = parts[0].split("\\.");
-            String type = parts[1];
-
-            int year = Integer.parseInt(date[0]);
-            int month = Integer.parseInt(date[1]);
-            int day = Integer.parseInt(date[2]);
-
-            int total = year * 12 * 28 + month * 28 + day + (termsMap.get(type) * 28) - 1;
-
-            if (total < todayTotal) {
+        
+        for (String term : terms) {
+            String type = term.split(" ")[0];
+            int due = Integer.parseInt(term.split(" ")[1]);
+            
+            termMap.put(type, due);
+        }
+        
+        
+        String[] t = today.split("\\.");
+        int tYear = Integer.parseInt(t[0]);
+        int tMonth = Integer.parseInt(t[1]);
+        int tDay = Integer.parseInt(t[2]);
+            
+        int tm = tYear * 12 * 28 + tMonth * 28 + tDay;
+        
+        for (int i = 0; i < length; i++) {
+            String[] s1 = privacies[i].split(" ");
+            String[] s = s1[0].split("\\.");
+            int year = Integer.parseInt(s[0]);
+            int month = Integer.parseInt(s[1]);
+            int day = Integer.parseInt(s[2]);
+            String type = s1[1];
+            int due = termMap.get(type);
+            
+            int m = year * 12 * 28 + month * 28 + day + due * 28 - 1;
+            
+            if (tm > m) {
                 result.add(i + 1);
             }
         }
-
-        int[] answer = new int[result.size()];
+        
+        Collections.sort(result);
+        answer = new int[result.size()];
         for (int i = 0; i < result.size(); i++) {
             answer[i] = result.get(i);
         }
-
+        
         return answer;
     }
 }
+
+/*
+n개의 개인정보
+약관 유효기간.
+- 파기 해야할 개인정보 번호 구하기.
+*/
