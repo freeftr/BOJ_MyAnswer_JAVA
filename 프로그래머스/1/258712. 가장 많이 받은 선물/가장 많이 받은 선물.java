03d0e21e14bef767194f5f0861e3309ecf821348
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-10 17:03:48 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 17.57 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,77 +1,46 @@
-import java.io.*;
 import java.util.*;
 
-// 두 사람이 선물을 주고 받은 기록이 있는 경우
-// 더 많은 선물을 준 사람이 다음 달에 선물을 하나 받음
-
-// 두 사람이 선물을 주고 받은 기록이 하나도 없거나 같은 경우
-// 선물 지수가 더 큰 사람이 선물 지수가 더 작은 사람에게 하나 받음
-// 선물 지수 = 이번 달 자신이 친구들에게 준 선물의 수 - 받은 선물
-// 선물 지수도 같다면 다음 달에 주고받지 않는다.
-
-// 선물을 가장 많이 받을 친구가 받을 선물의 수
 class Solution {
-    
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
+        int n = friends.length;
+        int[][] record = new int[n][n];
+        int[] result = new int[n];
+        HashMap<String, Integer> idMap = new HashMap<>();
         
-        int length = friends.length;
-        int[] result = new int[length];
-        
-        // 친구 목록 캐싱
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < length; i++) {
-            map.put(friends[i], i);
+        int idx = 0;
+        for (String friend : friends) {
+            idMap.put(friend, idx++);
         }
-        
-        //서로 주고 받은 선물의 개수 표현
-        int[][] info = new int[length][length];
-        //자기가 준 개수, 자기가 받은 개수 기록
-        int[][] jisoo = new int[length][2];
         
         for (String gift : gifts) {
-            String[] temp = gift.split(" ");
-            String a = temp[0];
-            String b = temp[1];
-            
-            info[map.get(a)][map.get(b)]++;
-            jisoo[map.get(a)][0]++;
-            jisoo[map.get(b)][1]++;
+            String[] parts = gift.split(" ");
+            int giver = idMap.get(parts[0]);
+            int receiver = idMap.get(parts[1]);
+            record[giver][receiver]++;
         }
         
-        for (int i = 0; i < length; i++){
-            for (int j = 0; j < length; j++){
-                
-                if (i==j) continue;
-                
-                // 같거나 주고 받은 경우가 없는 경우
-                if (info[i][j]==info[j][i] || info[i][j]==0 && info[j][i]==0) {
-                    if ((jisoo[i][0]-jisoo[i][1]) > (jisoo[j][0]-jisoo[j][1])) {
-                        result[i]++;
-                    } else if ((jisoo[i][0]-jisoo[i][1]) == (jisoo[j][0]-jisoo[j][1])) {
-                        continue;
-                    } else{
-                        result[j]++;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (record[i][j] > record[j][i]) {
+                    result[i]++;
+                } else if (record[i][j] < record[j][i]) {
+                    result[j]++;
+                } else {
+                    int A = 0, B = 0;
+                    for (int a = 0; a < n; a++) {
+                        A += record[i][a] - record[a][i];
+                        B += record[j][a] - record[a][j];
                     }
-                }
-                
-                //주고 받은 경우가 있는 경우
-                else {
-                    if (info[i][j] > info[j][i]) {
-                        result[i]++;
-                    }
-                    else{
-                        result[j]++;
-                    }
+                    if (A > B) result[i]++;
+                    else if (A < B) result[j]++;
                 }
             }
         }
         
-        for (int i = 0; i < length; i++) {
-            answer = Math.max(answer, result[i]);
+        int answer = 0;
+        for (int r : result) {
+            answer = Math.max(answer, r);
         }
-        
-        return answer/2;
+        return answer;
     }
 }
