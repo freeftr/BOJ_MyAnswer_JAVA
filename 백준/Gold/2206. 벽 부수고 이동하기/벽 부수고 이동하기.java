434c31cff5877154da67ad8077e3b29c34ad3137
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-11 09:44:28 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 724 ms, Memory: 113784 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,64 +1,76 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
+
     static int N, M;
-    static int[][] map;
-    static boolean[][][] visited;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static char[][] map;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visited = new boolean[N][M][2];
+
+        map = new char[N][M];
+
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(s.charAt(j) + "");
+                map[i][j] = s.charAt(j);
             }
         }
-        bfs();
+
+        System.out.println(bfs());
     }
-    public static void bfs(){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0,1,0});
+
+    static int bfs() {
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][][] visited = new boolean[N][M][2];
+
+        q.add(new int[]{0,0,0,1});
         visited[0][0][0] = true;
-        while(!q.isEmpty()){
+
+        while (!q.isEmpty()) {
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-            int cnt = cur[2];
-            int check = cur[3];
-            if(x==N-1 && y==M-1){
-                System.out.println(cnt);
-                System.exit(0);
-                return;
+            int wall = cur[2];
+            int dist = cur[3];
+
+            if (x == N - 1 && y == M - 1) {
+                return dist;
             }
-            for(int i = 0; i < 4; i++){
+
+            for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(nx>=N || ny>=M || nx<0 || ny<0)continue;
-                if(visited[nx][ny][check])continue;
-                if(map[nx][ny] == 0){
-                    visited[nx][ny][check]=true;
-                    q.add(new int[]{nx,ny,cnt+1,check});
 
-                }
-                else if(map[nx][ny] == 1){
-                    if(check==0){
-                        check++;
-                        visited[nx][ny][check]=true;
-                        q.add(new int[]{nx,ny,cnt+1,check});
-                        check--;
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (visited[nx][ny][wall]) continue;
+
+                if (wall == 0) {
+                    if (map[nx][ny] == '0') {
+                        q.add(new int[]{nx, ny, 0, dist + 1});
+                        visited[nx][ny][0] = true;
+                    } else {
+                        q.add(new int[]{nx, ny, 1, dist + 1});
+                        visited[nx][ny][1] = true;
                     }
+                } else {
+                    if (map[nx][ny] == '1') continue;
+                    q.add(new int[]{nx, ny, 1, dist + 1});
+                    visited[nx][ny][1] = true;
                 }
             }
         }
-        System.out.println(-1);
-        System.exit(0);
+        return -1;
     }
 }
