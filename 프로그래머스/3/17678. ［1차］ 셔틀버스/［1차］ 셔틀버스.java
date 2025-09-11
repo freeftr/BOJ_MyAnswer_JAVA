abd7ad1b9d9f25945e5314dbc13344a67bc31256
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-11 15:16:43 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 7.90 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,45 +1,38 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
-        int crewCnt = timetable.length;
-        int[] intTimeTable = new int[crewCnt];
+        int[] arr = new int[timetable.length];
+        for (int i = 0; i < timetable.length; i++) arr[i] = stringToInt(timetable[i]);
+        Arrays.sort(arr);
 
-        for (int i = 0; i < crewCnt; i++) {
-            intTimeTable[i] = makeIntTime(timetable[i]);
-        }
-        Arrays.sort(intTimeTable);
-
-        int crewIdx = 0;
-        int answerIntTime = 0;
+        int bus = 9 * 60;
+        int idx = 0;
+        int lastBoarded = -1;
 
         for (int i = 0; i < n; i++) {
-            int capacity = m;
-            int curBusTime = 9 * 60 + i * t;
-
-            while (capacity > 0 && crewIdx < crewCnt && intTimeTable[crewIdx] <= curBusTime) {
-                if (i == n - 1 && capacity == 1) {
-                    answerIntTime = intTimeTable[crewIdx] - 1;
-                }
-                crewIdx++;
-                capacity--;
+            int seats = m;
+            while (seats > 0 && idx < arr.length && arr[idx] <= bus) {
+                lastBoarded = arr[idx];
+                idx++;
+                seats--;
             }
-
-            if (i == n - 1 && capacity > 0) {
-                answerIntTime = curBusTime;
+            if (i == n - 1) {
+                if (seats > 0) return intToString(bus);
+                else           return intToString(lastBoarded - 1);
             }
+            bus += t;
         }
-
-        int hour  = answerIntTime / 60;
-        int minute = answerIntTime % 60;
-        return String.format("%02d:%02d", hour, minute);
+        return intToString(bus);
     }
 
-    static int makeIntTime(String time) {
-        String[] s = time.split(":");
-        int hour = Integer.parseInt(s[0]);
-        int min = Integer.parseInt(s[1]);
-        return hour * 60 + min;
+    static int stringToInt(String s) {
+        String[] p = s.split(":");
+        return Integer.parseInt(p[0]) * 60 + Integer.parseInt(p[1]);
+    }
+
+    static String intToString(int minutes) {
+        int h = minutes / 60, m = minutes % 60;
+        return String.format("%02d:%02d", h, m);
     }
 }
