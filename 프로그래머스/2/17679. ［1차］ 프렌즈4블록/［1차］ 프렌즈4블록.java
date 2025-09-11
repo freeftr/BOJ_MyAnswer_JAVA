ae42181d9aac8c0d0808aa0b58983c61d63fb177
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-11 14:49:05 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 28.52 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,83 +1,108 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-
-    static boolean[][] destroyList; 
-    static char[][] map; 
-    static int M; 
-    static int N; 
-
+    
+    static int N, M, answer = 0;
+    static char[][] map;
+    static HashSet<String> set = new HashSet<>();
+    
     public int solution(int m, int n, String[] board) {
-        int answer = 0;
-        destroyList = new boolean[m][n];
-        map = new char[m][n];
         M = m;
         N = n;
-
+        map = new char[M][N];
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 map[i][j] = board[i].charAt(j);
             }
         }
-
-        while (true) {
-            int removedBlocks = destroy(); 
-            if (removedBlocks == 0) { 
-                break;
-            }
+        // print();
+        // erase();
+        // print();
+        // down();
+        // print();
+        // erase();
+        // print();
+        
+        boolean flag = true;
+        while (flag) {
+            flag = erase();
             down();
-            answer += removedBlocks; 
         }
-
         return answer;
     }
-
-    static int destroy() {
-        int cnt = 0;
-
+    
+    static boolean erase() {
+        boolean flag = false;
         for (int i = 0; i < M - 1; i++) {
             for (int j = 0; j < N - 1; j++) {
-                if (map[i][j] != '0' && 
-                    map[i][j] == map[i + 1][j] &&
-                    map[i][j] == map[i][j + 1] &&
-                    map[i][j] == map[i + 1][j + 1]) {
-                    destroyList[i][j] = true;
-                    destroyList[i + 1][j] = true;
-                    destroyList[i][j + 1] = true;
-                    destroyList[i + 1][j + 1] = true;
+                if (map[i][j] == ' ') continue;
+                if (map[i][j] == map[i][j + 1] 
+                    && map[i][j] == map[i + 1][j + 1]
+                    && map[i][j] == map[i + 1][j]) {
+                    String s = i + " " + j;
+                    set.add(s);
+                    s = (i + 1) + " " + j;
+                    set.add(s);
+                    s = (i + 1) + " " + (j + 1);
+                    set.add(s);
+                    s = i + " " + (j + 1);
+                    set.add(s);
                 }
             }
         }
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (destroyList[i][j]) {
-                    map[i][j] = '0'; 
-                    destroyList[i][j] = false; 
-                    cnt++;
-                }
-            }
+        
+        for (String s : set) {
+            int x = Integer.parseInt(s.split(" ")[0]);
+            int y = Integer.parseInt(s.split(" ")[1]);
+            map[x][y] = ' ';
+            answer++;
+            flag = true;
         }
-
-        return cnt;
+        set.clear();
+        
+        return flag;
     }
-
+    
     static void down() {
-        for (int j = 0; j < N; j++) { 
-            for (int i = M - 1; i >= 0; i--) { 
-                if (map[i][j] == '0') { 
-                    int idx = i;
-                    while (idx > 0) {
-                        if (map[idx - 1][j] != '0') {
-                            map[i][j] = map[idx - 1][j]; 
-                            map[idx - 1][j] = '0'; 
+        for (int i = 0; i < N; i++) {
+            for (int j = M - 2; j >= 0; j--) {
+                if (map[j][i] != ' ') {
+                    char base = map[j][i];
+                    int x = j;
+                    while (x < M - 1) {
+                        int nx = x + 1;
+                        if (map[nx][i] == ' ') {
+                            map[nx][i] = base;
+                            map[x][i] = ' ';
+                            x = nx;
+                        } else {
                             break;
                         }
-                        idx--;
                     }
                 }
             }
         }
     }
+    
+    static void print() {
+        for (int i = 0; i < M; i++) {
+            System.out.println();
+            for (int j = 0; j < N; j++) {
+                System.out.print(map[i][j]);
+            }
+        }
+        System.out.println();
+    }
 }
+
+/*
+같은 모양 4개 정사각형 => 제거
+빈 캄 메꾸려고 내려감
+
+지워지는 거 개수 세기
+
+1. 있는거 지우기
+2. 내리기
+3. 1 반복
+*/
