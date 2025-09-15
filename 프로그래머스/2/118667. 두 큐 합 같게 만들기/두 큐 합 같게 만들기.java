881474096c387279c85881c58b5fe8462bf8fb48
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-15 15:19:09 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 90.12 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,45 +1,51 @@
-import java.io.*;
 import java.util.*;
+
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
+        int answer = 0;
+        
         long sum1 = 0;
         long sum2 = 0;
-        int l = queue1.length;
-        for(int i = 0; i < l; i++){
-            sum1+=queue1[i];
-            q1.add(queue1[i]);
-            sum2+=queue2[i];
-            q2.add(queue2[i]);
+        Queue<Integer> a = new LinkedList<>();
+        Queue<Integer> b = new LinkedList<>();
+        
+        for (int i = 0; i < queue1.length; i++) {
+            sum1 += queue1[i];
+            sum2 += queue2[i];
+            
+            a.add(queue1[i]);
+            b.add(queue2[i]);
         }
         
-        int ans = 0;
+        if (sum1 == sum2) return 0;
         
-        long target = (sum1+sum2)/2;
-        
-        while(true){
-            if(ans>2*l+5){
-                ans=-1;
-                break;
+        int idx = 0;
+        while (sum1 != sum2 && idx <= queue1.length * 2 + 1) {
+            if (sum1 > sum2) {
+                int at = a.poll();
+                sum1 -= at;
+                sum2 += at;
+                b.add(at);
+            } else {
+                int bt = b.poll();
+                sum2 -= bt;
+                sum1 += bt;
+                a.add(bt);
             }
-            if(sum1<target){
-                ans++;
-                int a = q2.poll();
-                q1.add(a);
-                sum1+=a;
-                sum2-=a;
-            } else if(sum2<target){
-                ans++;
-                int a = q1.poll();
-                q2.add(a);
-                sum1-=a;
-                sum2+=a;
-            }
-            else{
-                break;
-            }
+            idx++;
+            answer++;
         }
-        return ans;
+        
+        if (sum1 != sum2) return -1;
+        
+        return answer;
     }
 }
+
+/*
+각 큐의 원소의 합이 같도록
+최소 작업 수 구하기
+
+- 각 큐 합 구하면서 큐에 넣기.
+- 큰 쪽에서 작은 쪽으로 옮기기
+*/
