3ea6e786b600d602cde7a68e8a179d47ad40cb8f
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-16 10:39:13 +0900

    [level 1] Title: 신고 결과 받기, Time: 273.08 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,41 +3,34 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-        
-        HashMap<String, ArrayList<String>> reportMap = new HashMap<>();
-        HashMap<String, Integer> idMap = new HashMap<>();
-    
-        int idx = 0;
-        for (String id : id_list) {
-            idMap.put(id, idx++);
-        }
-        
+        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
+        HashMap<String, Integer> result = new HashMap<>();
         for (String rep : report) {
             String reporter = rep.split(" ")[0];
             String reported = rep.split(" ")[1];
-            
-            reportMap.computeIfAbsent(reported, a -> new ArrayList<>()).add(reporter);
+            reportMap.computeIfAbsent(reported, a -> new HashSet<>()).add(reporter);
         }
         
         for (String key : reportMap.keySet()) {
-            ArrayList<String> temp = reportMap.get(key);
-            Set<String> reporters = new HashSet<>();
-            reporters.addAll(temp);
-            
-            if (reporters == null) continue;
-            
+            HashSet<String> reporters = reportMap.get(key);
             if (reporters.size() >= k) {
                 for (String reporter : reporters) {
-                    answer[idMap.get(reporter)]++;   
+                    result.merge(reporter, 1, (a, b) -> a + b);
                 }
             }
         }
         
+        for (int i = 0; i < answer.length; i++) {
+            String key = id_list[i];
+            if (result.get(key) == null) continue;
+            answer[i] = result.get(key);
+            System.out.println(i + " " + key + " " + answer[i]);
+        }
         return answer;
     }
 }
 
 /*
-- 동일한 유저에 대한 신고는 1회
-- k번 이상 시 정지 + 신고한 사람들한테 메일
+한번에 한명 유저 신고
+k번 이상일 신고한 모든 유저에게 메일
 */
