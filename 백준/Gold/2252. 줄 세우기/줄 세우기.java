4d77a8c9f06bee005c5acec37dbca39fc1fdbb7f
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-19 08:56:46 +0900

    [Gold III] Title: 줄 세우기, Time: 448 ms, Memory: 46880 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,52 +1,51 @@
-import java.io.*;
-import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-        st = new StringTokenizer(br.readLine());
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
-        long M = Long.parseLong(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        ArrayList<Integer> answer = new ArrayList<>();
-        Queue<Integer> pq = new LinkedList<>();
-        long[] inDegree = new long[N+1];
+        int[] indeg = new int[N + 1];
 
-        for(int i = 0; i <= N; i++){
+        for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for(int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-
             graph.get(A).add(B);
-            inDegree[B]++;
+            indeg[B]++;
         }
 
-        for(int i = 1; i <= N; i++){
-            if(inDegree[i]==0){
-                pq.add(i);
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            if (indeg[i] == 0) q.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            sb.append(cur + " ");
+
+            for (int next : graph.get(cur)) {
+                indeg[next]--;
+                if (indeg[next] == 0) q.add(next);
             }
         }
 
-        while(!pq.isEmpty()){
-            int v = pq.poll();
-            answer.add(v);
-
-            for(int nv: graph.get(v)){
-                inDegree[nv]--;
-                if(inDegree[nv]==0){
-                    pq.add(nv);
-                }
-            }
-        }
-
-        for(int i : answer){
-            System.out.printf("%d ", i);
-        }
+        System.out.println(sb);
     }
 }
