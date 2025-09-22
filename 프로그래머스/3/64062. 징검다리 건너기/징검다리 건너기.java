861c4c74bd1099784f0cb11c5262cbe422c7c180
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-22 18:15:46 +0900

    [level 3] Title: 징검다리 건너기, Time: 26.55 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int[] stones, int k) {
         int answer = 0;
-        
         int left = 0;
         int right = 200000000;
         
         while (left < right) {
             int mid = (left + right) / 2;
-            
-            if (check(mid, stones, k)) {
+            if (check(stones, k, mid)) {
                 left = mid + 1;
             } else {
                 right = mid;
@@ -17,15 +15,14 @@ class Solution {
         return right;
     }
     
-    static boolean check(int target, int[] stones, int k) {
+    static boolean check(int[] stones, int k, int mid) {
         int cnt = 0;
         for (int i = 0; i < stones.length; i++) {
-            if (stones[i] - target <= 0) {
+            if (stones[i]  - mid <= 0) {
                 cnt++;
             } else {
                 cnt = 0;
             }
-            
             if (cnt == k) return false;
         }
         return true;
