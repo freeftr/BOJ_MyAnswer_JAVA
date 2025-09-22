774e71708ff4e5e4a25558919188e4b9d8be3d4c
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-22 17:31:53 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.11 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,31 +1,43 @@
 import java.util.*;
-
 class Solution {
+    static int[][] map;
+    static int N;
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
+        N = board.length;
+        
+        Queue[] q = new LinkedList[N];
         Stack<Integer> stack = new Stack<>();
-        int[] top = new int[board.length];
-        int size = board.length;
+        
+        for (int i = 0; i < N; i++) {
+            q[i] = new LinkedList<Integer>();
+            for (int j = 0; j < N; j++) {
+                if (board[j][i] == 0) continue;
+                q[i].add(board[j][i]);
+            }
+        }
         
         for (int move : moves) {
-            for (int i = 0; i < size; i++) {
-                if (board[i][move - 1] != 0) {
-                    if (stack.isEmpty()) {
-                        stack.push(board[i][move - 1]);
-                    } else {
-                        if (stack.peek() == board[i][move - 1]) {
-                            answer += 2;
-                            stack.pop();
-                        } else {
-                            stack.push(board[i][move - 1]);
-                        }
-                    }
-                    
-                    board[i][move - 1] = 0;
-                    break;
+            if (q[move - 1].isEmpty()) continue;
+            int cur = (int) q[move - 1].poll();
+            if (stack.isEmpty()) {
+                stack.push(cur);
+            } else {
+                if (stack.peek() == cur) {
+                    stack.pop();
+                    answer += 2;
+                } else {
+                    stack.push(cur);
                 }
             }
         }
         return answer;
     }
 }
+
+/*
+N * N 맵
+
+하나씩 뽑아서 장바구니에 넣음.
+장바구니 두개 연속이면 제거 => 스택
+*/
