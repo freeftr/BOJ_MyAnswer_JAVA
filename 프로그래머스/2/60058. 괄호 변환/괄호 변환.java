c761ee40756036d7a881ab3e8b50d9cb7dec9cad
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-24 17:11:10 +0900

    [level 2] Title: 괄호 변환, Time: 29.00 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,78 +1,71 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public String solution(String p) {
         String answer = "";
         
-        
         return dfs(p);
     }
     
     static String dfs(String s) {
+        if (s == "") return s;
+        
         String u = "";
         String v = "";
         
-        // 1.
-        if (s.equals("")) {
-            return "";
-        }
-        
-        String result = "";
-        
-        for (int i = 2; i <= s.length(); i++) {
-            if (check1(s.substring(0,i))) {
+        for (int i = 2; i <= s.length(); i+=2) {
+            if (balance(s.substring(0, i))) {
                 u = s.substring(0, i);
                 v = s.substring(i);
                 break;
             }
         }
         
-        if (check2(u))  {
+        if (check(u)) {
             return u + dfs(v);
         } else {
             String temp = "(" + dfs(v) + ")";
-            
-            StringBuilder sb = new StringBuilder(temp);
             for (int i = 1; i < u.length() - 1; i++) {
-                sb.append(u.charAt(i) == '(' ? ')' : '(');
+                temp += u.charAt(i) == '(' ? ')' : '(';
             }
-            return sb.toString();
+            return temp;
         }
     }
     
-    // 균형잡힌 문자열 검사
-    static boolean check1(String s) {
-        int left = 0, right = 0;
+    static boolean balance(String s) {
+        int left = 0;
+        int right = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                left++;
-            } else {
-                right++;
-            }
+            if (s.charAt(i) == '(') left++;
+            if (s.charAt(i) == ')') right++;
         }
-        
         return left == right;
     }
     
-    // 올바른 문자열 검사
-    static boolean check2(String s) {
-        Stack<Character> stack = new Stack<>();
-        
+    static boolean check(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
-            if (stack.isEmpty()) {
-                stack.push(s.charAt(i));
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
             } else {
-                if (stack.peek() == '(' && s.charAt(i) == ')') {
-                    stack.pop();
-                } else {
-                    stack.push(s.charAt(i));
-                }
+                if (stack.isEmpty()) return false;
+                stack.pop();
             }
         }
-        
         return stack.isEmpty();
     }
 }
 
-//()()()
+/*
+빈 문자열 -> 빈 문자열 반환
+u와 v로 분리
+- u는 더이상 균형잡힌 문자열이되면 안댐.
+- v는 빈 문자열일 수 있음.
+
+u가 올바른 문자열이 아니면 
+- 빈문자열에 (붙이기
+- v에 대해 재귀 수행
+- ')'다시 붙이기
+- u의 첫 마지막 제거, 
+*/
