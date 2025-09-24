075b2c357f0b6653a1daa0cb48371e4037adedaf
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-24 19:45:21 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 1.76 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,87 +1,59 @@
 import java.util.*;
 
 class Solution {
-    
-    static ArrayList<int[]> dolgis = new ArrayList<>();
-    
+    static int M, N;
+    static int[][] map;
+
     public boolean solution(int[][] key, int[][] lock) {
-        boolean answer = false;
+        M = key.length;
+        N = lock.length;
+        map = new int[N][N];
 
-        int N = key.length;
-        int M = lock.length;
-        
-        int[][] temp = new int[M][M];
-        
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < M; j++) {
-                temp[i][j] = lock[i][j];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = lock[i][j];
             }
         }
-        
-        boolean flag = false;
-        for (int i = -N + 1; i < M; i++) { 
-            for (int j = -N + 1; j < M; j++) { 
-                for (int x = 0; x < 4; x++) { 
-                    if (canUnlock(lock, key, i, j, N, M)) {
-                        flag = true;
-                        break;
-                    }
-                    rotate(key); 
-                }
-            }
-            if(flag) {
-                answer = true;
-                break;
-            }
+
+        for (int r = 0; r < 4; r++) {
+            if (check(key)) return true;
+            rotate();
         }
-        return answer;
+        return false;
     }
 
-    static boolean canUnlock(int[][] lock, int[][] key, int i, int j, int N, int M) {
-        int[][] temp = new int[M][M];
-
-        for (int a = 0; a < M; a++) {
-            for (int b = 0; b < M; b++) {
-                temp[a][b] = lock[a][b]; 
+    static boolean check(int[][] key) {
+        for (int offX = -(M - 1); offX <= N - 1; offX++) {
+            for (int offY = -(M - 1); offY <= N - 1; offY++) {
+                if (fits(offX, offY, key)) return true;
             }
         }
-
-        for (int a = 0; a < N; a++) {
-            for (int b = 0; b < N; b++) {
-                int x = i + a;
-                int y = j + b;
-                if (x >= 0 && x < M && y >= 0 && y < M) { 
-                    temp[x][y] += key[a][b];
-                }
-            }
-        }
-
-        return check(temp, M);
+        return false;
     }
 
-    static boolean check(int[][] map, int M) {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] != 1) {
-                    return false;
-                }
+    static boolean fits(int offX, int offY, int[][] key) {
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < N; y++) {
+                int sum = map[x][y];
+                int i = x - offX, j = y - offY;
+                if (0 <= i && i < M && 0 <= j && j < M) sum += key[i][j];
+                if (sum != 1) return false;
             }
         }
         return true;
     }
 
-    static void rotate(int[][] map) {
-        int N = map.length;
+    static void rotate() {
         int[][] rotated = new int[N][N];
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                rotated[j][N - i - 1] = map[i][j];
+                rotated[j][N - 1 - i] = map[i][j];
             }
         }
-
         for (int i = 0; i < N; i++) {
-            System.arraycopy(rotated[i], 0, map[i], 0, N);
+            for (int j = 0; j < N; j++) {
+                map[i][j] = rotated[i][j];
+            }
         }
     }
 }
