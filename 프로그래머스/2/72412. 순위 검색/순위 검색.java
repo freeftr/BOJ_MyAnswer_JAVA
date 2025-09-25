282bcc532bf9593a9d3b09e92ccd251c7ea020fc
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-25 16:48:48 +0900

    [level 2] Title: 순위 검색, Time: 966.54 ms, Memory: 189 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     
     static HashMap<String, ArrayList<Integer>> map = new HashMap<>();
@@ -7,11 +6,11 @@ class Solution {
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
         
-        for (String inf : info) {
-            String[] s = inf.split(" ");
-            dfs(0, s, "");
+        for (String in : info) {
+            String[] data = in.split(" ");
+            dfs(0, data, "");
         }
-
+        
         for (ArrayList<Integer> list : map.values()) {
             Collections.sort(list);
         }
@@ -22,14 +21,13 @@ class Solution {
             String[] elements = temp.split(" ");
             int target = Integer.parseInt(elements[elements.length - 1]);
             String key = "";
-            
+
             for (int i = 0; i < elements.length - 1; i++) {
                 key += elements[i];
             }
             
             ArrayList<Integer> scores = map.get(key);
             
-            int cnt = 0;
             if (scores == null) {
                 answer[idx++] = 0;
             } else {
@@ -38,12 +36,14 @@ class Solution {
                 
                 while (left < right) {
                     int mid = (left + right) / 2;
+                    
                     if (scores.get(mid) < target) {
                         left = mid + 1;
                     } else {
                         right = mid;
                     }
                 }
+                
                 answer[idx++] = scores.size() - left;
             }
         }
@@ -51,13 +51,13 @@ class Solution {
         return answer;
     }
     
-    static void dfs(int depth, String[] info, String cur) {
+    static void dfs(int depth, String[] data, String key) {
         if (depth == 4) {
-            map.computeIfAbsent(cur, k -> new ArrayList<Integer>()).add(Integer.parseInt(info[4]));
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(Integer.parseInt(data[4]));
             return;
         }
         
-        dfs(depth + 1, info, cur + info[depth]);
-        dfs(depth + 1, info, cur + "-");
+        dfs(depth + 1, data, key + data[depth]);
+        dfs(depth + 1, data, key + "-");
     }
 }
