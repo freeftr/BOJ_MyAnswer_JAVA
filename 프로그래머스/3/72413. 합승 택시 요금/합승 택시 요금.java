29c6aaeeb107331e06e1481b0fd3604f9bff18e9
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-25 15:47:30 +0900

    [level 3] Title: 합승 택시 요금, Time: 124.47 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,40 +1,48 @@
 import java.util.*;
-
 class Solution {
-    static int[][] dist;
-    static final int INF = 20000001;  
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        dist = new int[n+1][n+1];
-
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
-            dist[i][i] = 0;  
+        int answer = 0;
+        
+        int[][] dist = new int[n + 1][n + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-
+        
         for (int[] fare : fares) {
             dist[fare[0]][fare[1]] = fare[2];
             dist[fare[1]][fare[0]] = fare[2];
         }
-
+        
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
-                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
                         dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
                 }
             }
         }
-
-        int minCost = dist[s][a] + dist[s][b]; 
-
+        
+        int no = dist[s][a] + dist[s][b];
+        int yes = Integer.MAX_VALUE;
         for (int i = 1; i <= n; i++) {
-            if (dist[s][i] == INF || dist[i][a] == INF || dist[i][b] == INF) {
-                continue;
-            }
-            minCost = Math.min(minCost, dist[s][i] + dist[i][a] + dist[i][b]);
+            if (i == s) continue;
+            int A = dist[i][a];
+            int B = dist[i][b];
+            if (i == a) A = 0;
+            if (i == b) B = 0;
+            yes = Math.min(yes, dist[s][i] + A + B);
         }
-
-        return minCost;
+        
+        return Math.min(yes, no);
     }
 }
+
+/*
+노드 = 지점
+간선 = 택시노선, 예상요금
+
+플로이드 워셜로 각 지점별 최단거리 구하기.
+
+*/
