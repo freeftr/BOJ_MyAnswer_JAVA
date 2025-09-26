9fcd8daa591c8fd1394b36e1d5a46cc4bbdc70bc
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-26 09:47:46 +0900

    [level 2] Title: 주차 요금 계산, Time: 10.34 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,79 +1,98 @@
 import java.util.*;
 class Solution {
+    
     public int[] solution(int[] fees, String[] records) {
         int[] answer = {};
         
-        HashMap<String, Integer> timeMap = new HashMap<>();
-        HashMap<String, Integer> inMap = new HashMap<>();
+        HashMap<String, Integer> inTimes = new HashMap<>();
+        HashMap<String, Integer> totalTime = new HashMap<>();
         
         for (String record : records) {
-            String strTime = record.split(" ")[0];
-            String num = record.split(" ")[1];
+            int time = strToInt(record.split(" ")[0]);
+            String carNum = record.split(" ")[1];
             String type = record.split(" ")[2];
-            int time = strToint(strTime);
             
-            // 새로 입차하는 경우
-            if (type.equals("IN") && !inMap.containsKey(num)) {
-                inMap.put(num, time);
+            if (type.equals("IN")) {
+                inTimes.put(carNum, time);
+            }
+            
+            if (type.equals("OUT")) {
+                int in = inTimes.get(carNum);
+                int out = time;
+                int diff = out - in;
+                
+                totalTime.merge(carNum, diff, (a, b) -> a + b);
+                inTimes.remove(carNum);
+            }
+        }
+        
+        for (String carNum : inTimes.keySet()) {
+            int in = inTimes.get(carNum);
+            int out = 23 * 60 + 59;
+            int diff = out - in;
+            
+            totalTime.merge(carNum, diff, (a, b) -> a + b);
+        }
+        
+        ArrayList<Price> result = new ArrayList<>();
+        
+        for (String carNum : totalTime.keySet()) {
+            int time = totalTime.get(carNum);
+            int baseTime = fees[0];
+            int basePrice = fees[1];
+            int car = Integer.parseInt(carNum);
+            
+            //기본 시간 이하
+            if (time <= baseTime) {
+                result.add(new Price(car, basePrice));
+                continue;
+            }
+            
+            //기본 시간 초과
+            int totalPrice = 0;
+            totalPrice += basePrice;
+            time -= baseTime;
+            
+            //나누어떨어지는 경우 & 안되는 경우
+            if (time % fees[2] == 0) {
+                totalPrice += time / fees[2] * fees[3];
             } else {
-                int inTime = inMap.get(num);
-                int outTime = time;
-                
-                int diff = outTime - inTime;
-                
-                timeMap.merge(num, diff, (a, b) -> a + b);
-                
-                inMap.remove(num);
+                totalPrice += (time / fees[2] + 1) * fees[3];
             }
+            
+            result.add(new Price(car, totalPrice));
         }
         
-        if (!inMap.isEmpty()) {
-            for (String key : inMap.keySet()) {
-                int inTime = inMap.get(key);
-                int diff = 24 * 60 - 1 - inTime;
-                
-                timeMap.merge(key, diff, (a, b) -> a + b);
-            }
-        }
-        
-        ArrayList<String> result = new ArrayList<>(timeMap.keySet());
-        
-        Collections.sort(result);
-        
+        Collections.sort(result, (a, b) -> a.carNum - b.carNum);
         answer = new int[result.size()];
+        
         for (int i = 0; i < result.size(); i++) {
-            int time = timeMap.get(result.get(i));
-            if (time <= fees[0]) {
-                answer[i] = fees[1];
-            } else {
-                time -= fees[0];
-                answer[i] += fees[1];
-                if (time % fees[2] != 0) {
-                    time -= time % fees[2];
-                    time += fees[2];
-                }
-                answer[i] += fees[3] * time / fees[2];
-            }
+            answer[i] = result.get(i).fee;
         }
         
         return answer;
     }
     
-    static int strToint(String s) {
-        String[] t = s.split(":");
-        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+    static class Price {
+        int carNum;
+        int fee;
+        Price (int carNum, int fee) {
+            this.carNum = carNum;
+            this.fee = fee;
+        }
+    }
+    
+    static int strToInt(String s) {
+        int hour = Integer.parseInt(s.split(":")[0]);
+        int min = Integer.parseInt(s.split(":")[1]);
+        
+        return hour * 60 + min;
     }
 }
 
 /*
-기본 시간 이하 -> 기본 요금
-기본 시간 초과 -> 기본 요금 + 초과 시간 * 단위 요금
-- 초과시간 안맞으면 올림
+입차했다가 나간 경우
+입차하고 안나간 경우
 
-차량 번호가 작은 자동차부터 요금 출력
-
-입차 맵 생성
-
-출차 내용 들어오면 입차랑 비교해서 요금 맵에 기록
-마지막에 남아있는 차들 정각 기준으로 계산해서 요금 기록
+누적 시간 구해놓고 마지막에 계산
 */
