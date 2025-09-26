b4496a09cb18d6f3eb0d0589d61286592eda76f0
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-26 14:14:45 +0900

    [level 2] Title: 양궁대회, Time: 0.80 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,60 +1,58 @@
 class Solution {
-    static int bestDiff = -1;
-    static int[] bestShots = new int[11];
-
+    static int[] answer;
+    static int result = 0;
     public int[] solution(int n, int[] info) {
-        int[] shots = new int[11];
-        dfs(n, 0, shots, info, 0);
-
-        if (bestDiff <= 0) return new int[]{-1};
-        return bestShots;
+        answer = new int[11];
+        dfs(0, info, new int[11], 0, n);
+        if (result == 0) return new int[]{-1};
+        return answer;
     }
-
-    static void dfs(int n, int depth, int[] shots, int[] info, int used) {
+    
+    static void dfs(int depth, int[] info, int[] shots, int used, int n) {
         if (depth == 11) {
-            if (used < n) {
-                shots[10] += (n - used);
-                calculate(shots, info);
-                shots[10] -= (n - used);
-            } else {
-                calculate(shots, info);
-            }
+            shots[10] = n - used;
+            calculate(shots, info);
             return;
         }
-
-        dfs(n, depth + 1, shots, info, used);
-
+        
+        // 안쏘고 넘어가는 경우
+        dfs(depth + 1, info, shots, used, n);
+        
+        // 점수를 획득하는 데 필요한 최소 개수
         int need = info[depth] + 1;
         if (n - used >= need) {
             shots[depth] = need;
-            dfs(n, depth + 1, shots, info, used + need);
+            dfs(depth + 1, info, shots, used + need, n);
             shots[depth] = 0;
         }
     }
-
+    
     static void calculate(int[] shots, int[] info) {
-        int ryan = 0, apeach = 0;
-
+        int ryan = 0;
+        int apeach = 0;
+        
         for (int i = 0; i < 11; i++) {
-            int score = 10 - i;
             if (shots[i] == 0 && info[i] == 0) continue;
 
-            if (shots[i] > info[i]) ryan += score;
-            else apeach += score;
+            if (shots[i] > info[i]) {
+                ryan += 10 - i;
+            } else {
+                apeach += 10 - i;
+            }
         }
-
-        if (ryan <= apeach) return;
-
+        
+        if (apeach >= ryan) return;
         int diff = ryan - apeach;
-        if (diff > bestDiff) {
-            bestDiff = diff;
-            bestShots = shots.clone();
-        } else if (diff == bestDiff) {
+        if (diff > result) {
+            result = diff;
+            answer = shots.clone();
+        } else if (diff == result) {
             for (int i = 10; i >= 0; i--) {
-                if (shots[i] != bestShots[i]) {
-                    if (shots[i] > bestShots[i]) bestShots = shots.clone();
-                    break;
+                if (shots[i] == answer[i]) continue;
+                if (shots[i] > answer[i]) {
+                    answer = shots.clone();
                 }
+                break;
             }
         }
     }
