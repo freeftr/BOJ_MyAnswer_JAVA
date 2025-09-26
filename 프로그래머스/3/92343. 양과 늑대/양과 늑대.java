9b3d54eb0b7a5dc2a82108499fbdaebe77887c0e
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-26 10:29:13 +0900

    [level 3] Title: 양과 늑대, Time: 20.64 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,49 +1,57 @@
 import java.util.*;
 
 class Solution {
-    
     static int result = 0;
+    static int N;
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
     static boolean[] visited;
     
     public int solution(int[] info, int[][] edges) {
-        for (int i = 0; i < info.length; i++) {
+        int answer = 0;
+        N = info.length;
+        
+        visited = new boolean[N];
+        
+        for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
-        
-        visited = new boolean[info.length+1];
         
         for (int[] edge : edges) {
             graph.get(edge[0]).add(edge[1]);
         }
-
-        HashSet<Integer> state = new HashSet<>();
         
-        dfs(0, 1, 0, info, state);
+        dfs(0, 0, 1, info, new HashSet<Integer>());
         
         return result;
     }
     
-    static void dfs(int now, int sheep, int wolf, int[] info, HashSet<Integer> state) {
-        if (sheep <= wolf) return;
-        
+    static void dfs(int v, int wolf, int sheep, int[] info, HashSet<Integer> next) {
+        if (wolf >= sheep) return;
         result = Math.max(result, sheep);
         
-        HashSet<Integer> newState = new HashSet<>(state);
-
-        for (int nv : graph.get(now)) {
-            newState.add(nv);
+        HashSet<Integer> newSet = new HashSet<>(next);
+        
+        for (int nv : graph.get(v)) {
+            newSet.add(nv);
         }
-
-        for (int next : newState) {
-            if (visited[next]) continue;
-            visited[next] = true;
-            if (info[next] == 1) {
-                dfs(next, sheep, wolf + 1, info, newState);
-            } else {
-                dfs(next, sheep + 1, wolf, info, newState);
-            }
-            visited[next] = false;
+        
+        for (int nv : newSet) {
+            if (visited[nv]) continue;
+            visited[nv] = true;
+            if (info[nv] == 1) dfs(nv, wolf + 1, sheep, info, newSet);
+            if (info[nv] == 0) dfs(nv, wolf, sheep + 1, info, newSet);
+            visited[nv] = false;
         }
     }
 }
+
+/*
+조건
+양보다 늑대의 수가 같거나 많아지면 return;
+
+요구
+최대한 많은 양을 모으게.
+
+풀이
+dfs로 돌면서 다음에 갈 곳들을 미리 저장해놓고 돌기.
+*/
