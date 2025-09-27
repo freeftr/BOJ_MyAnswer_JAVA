2126ab4bc4d35a7a86fbd2c0b7db8d778de5aa06
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-27 19:05:31 +0900

    [Gold V] Title: 트리와 쿼리, Time: 592 ms, Memory: 86860 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,63 +1,54 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
 
     static int N, R, Q;
-    static int U, V;
-    static ArrayList<Integer> roots = new ArrayList<>();
+    static int[] dp;
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
     static boolean[] visited;
-    static int[] dp;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[100001];
-        dp = new int[100001];
-        Arrays.fill(dp,1);
+        for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
 
-        for(int i = 0; i <= N; i++){
-            graph.add(new ArrayList<>());
-        }
-
-        for(int i = 0; i < N-1; i++){
+        for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            U = Integer.parseInt(st.nextToken());
-            V = Integer.parseInt(st.nextToken());
+            int U = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
             graph.get(U).add(V);
             graph.get(V).add(U);
         }
 
-        for(int i = 0; i < Q; i++){
-            int a = Integer.parseInt(br.readLine());
-            roots.add(a);
-        }
+        visited = new boolean[N + 1];
+        dp = new int[N + 1];
 
         dfs(R);
 
-        for(int i = 0; i < Q; i++){
-            System.out.println(dp[roots.get(i)]);
+        for (int i = 0; i < Q; i++) {
+            int U = Integer.parseInt(br.readLine());
+            sb.append(dp[U]).append('\n');
         }
 
+        System.out.print(sb.toString());
     }
 
-    public static int dfs(int v){
-        if(visited[v]) return dp[v];
-
+    static int dfs(int v) {
         visited[v] = true;
+        dp[v] = 1;
 
-        for(int nv : graph.get(v)){
-            if(!visited[nv]){
+        for (int nv : graph.get(v)) {
+            if (!visited[nv]) {
                 dp[v] += dfs(nv);
             }
         }
-
         return dp[v];
     }
 }
