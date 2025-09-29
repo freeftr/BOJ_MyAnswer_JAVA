f804889e4925db96c7b7f5e51e1a9b4e8aaa6a1c
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-09-29 11:03:52 +0900

    [Silver II] Title: 점프 점프, Time: 116 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -1,36 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
-    static int[] A;
-    static int[] dp = new int[N];
-    static int answer = -1;
+    static int[] arr;
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        A = new int[N];
+        arr = new int[N];
         dp = new int[N];
 
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.fill(dp, 1001);
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        if (arr[0] == 0) {
+            System.out.println(-1);
+            return;
+        }
+
         dp[0] = 0;
 
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j <= i+A[i]; j++) {
-                if(j>=N) continue;
+            if (dp[i] == Integer.MAX_VALUE) continue;
+            for (int j = 1; j <= arr[i]; j++) {
+                int next = i + j;
 
-                dp[j] = Math.min(dp[i]+1, dp[j]);
+                if (next >= N) next = N - 1;
+
+                dp[next] = Math.min(dp[next], dp[i] + 1);
             }
         }
-        System.out.println(dp[N-1] == 1001 ? -1 : dp[N-1]);
+
+        System.out.println(dp[N - 1] == Integer.MAX_VALUE ? -1 : dp[N - 1]);
     }
 }
+
+/*
+조건
+- Ai칸에 적혀 있는 숫자 이하만큼 오른쪽으로 떨어진 칸으로 한 번에 점프 가능.
+
+요구
+- 맨 왼쪽에서 오른쪽 끝까지 최소 몇번 점프를 해야하는지.
+
+풀이
+- dp
+ */
