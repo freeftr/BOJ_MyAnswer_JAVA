225c9926fb37982c36b984ff04143de83d3a49ca
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-01 17:40:24 +0900

    [level 2] Title: 올바른 괄호, Time: 18.58 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                if (stack.isEmpty()) return false;
+                if (stack.peek() == '(') {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
