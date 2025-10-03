0a1a0a9a1d0d753ed9fb04e2b7dcbff3213d2371
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-03 16:21:15 +0900

    [level 1] Title: [1차] 비밀지도, Time: 4.01 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,42 +1,31 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        String[] s1 = new String[n];
-        String[] s2 = new String[n];
         
         for (int i = 0; i < n; i++) {
-            s1[i] = Integer.toBinaryString(arr1[i]);
-            s2[i] = Integer.toBinaryString(arr2[i]);
-            int len1 = s1[i].length();
-            int len2 = s2[i].length();
-            if (s1[i].length() < n) {
-                for (int j = 0; j < n - len1; j++) {
-                    s1[i] = "0" + s1[i];
-                }
+            String a = Integer.toBinaryString(arr1[i]);
+            String b = Integer.toBinaryString(arr2[i]);
+            
+            while (a.length() < n) {
+                a = "0" + a;
             }
-            if (s2[i].length() < n) {
-                for (int j = 0; j < n - len2; j++) {
-                    s2[i] = "0" + s2[i];
-                }
+            
+            while (b.length() < n) {
+                b = "0" + b;
             }
-            String s = "";
+            
+            String temp = "";
+            
             for (int j = 0; j < n; j++) {
-                if (s1[i].charAt(j) == '1' || s2[i].charAt(j) == '1') {
-                    s += "#";
+                if (a.charAt(j) == '1' || b.charAt(j) == '1') {
+                    temp += "#";
                 } else {
-                    s += " ";
+                    temp += " ";
                 }
             }
             
-            answer[i] = s;
+            answer[i] = temp;
         }
         return answer;
     }
 }
-
-/*
-n * n
-" ", # = 벽
-
-모두 공백만 공백
-*/
