b87428b3e12150ac20a0af705c9749470c1a344d
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-03 16:29:25 +0900

    [level 2] Title: [1차] 캐시, Time: 36.80 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,46 +1,34 @@
 import java.util.*;
-
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        ArrayList<String> cache = new ArrayList<>();
-        String[] citie = new String[4];
+        
         if (cacheSize == 0) {
             return cities.length * 5;
         }
-        citie[0] = "1";
-        citie[1] = "1";
-        citie[2] = "1";
-        citie[3] = "1";
+        
+        ArrayList<String> caches = new ArrayList<>();
+        
         for (String city : cities) {
             city = city.toLowerCase();
-            if (cache.isEmpty()) {
-                cache.add(city);
-                answer += 5;
-                continue;
-            }
-            if (!cache.contains(city)) {
-                if (cache.size() < cacheSize) {
-                    cache.add(city);
+            if (caches.contains(city)) {
+                answer += 1;
+                if (caches.size() < cacheSize) {
+                    caches.add(city);
                 } else {
-                    cache.remove(0);
-                    cache.add(city);
+                    caches.remove(caches.indexOf(city));
+                    caches.add(city);  
                 }
-                answer += 5;
             } else {
-                int idx = cache.indexOf(city);
-                cache.remove(idx);
-                cache.add(city);
-                answer++;
+                answer += 5;
+                if (caches.size() < cacheSize) {
+                    caches.add(city);
+                } else {
+                    caches.remove(0);
+                    caches.add(city);  
+                }
             }
         }
-        // System.out.println(answer + " " + cache.size());
         return answer;
     }
 }
-
-/*
-DB 캐시에 따른 실행 시간 측정
-hit - 1
-miss - 5
-*/
