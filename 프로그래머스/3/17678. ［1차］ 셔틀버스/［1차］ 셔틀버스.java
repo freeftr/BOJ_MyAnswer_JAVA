1e57374c78bf2c46db57d3c74190b6761c65d911
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-05 17:20:52 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 6.39 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,38 +1,63 @@
 import java.util.*;
-
 class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
-        int[] arr = new int[timetable.length];
-        for (int i = 0; i < timetable.length; i++) arr[i] = stringToInt(timetable[i]);
-        Arrays.sort(arr);
-
         int bus = 9 * 60;
         int idx = 0;
-        int lastBoarded = -1;
-
+        int last = -1;
+        
+        int[] lines = new int[timetable.length];
+        for (int i = 0; i < timetable.length; i++) {
+            lines[i] = strToInt(timetable[i]);
+        }
+        
+        Arrays.sort(lines);
+        
         for (int i = 0; i < n; i++) {
             int seats = m;
-            while (seats > 0 && idx < arr.length && arr[idx] <= bus) {
-                lastBoarded = arr[idx];
-                idx++;
+            
+            while (seats > 0 && idx < lines.length && lines[idx] <= bus) {
                 seats--;
+                last = lines[idx++];
             }
+            
             if (i == n - 1) {
-                if (seats > 0) return intToString(bus);
-                else           return intToString(lastBoarded - 1);
+                if (seats > 0) return intToStr(bus);
+                else return intToStr(last - 1);
             }
+            
             bus += t;
         }
-        return intToString(bus);
+        return intToStr(bus);
     }
-
-    static int stringToInt(String s) {
-        String[] p = s.split(":");
-        return Integer.parseInt(p[0]) * 60 + Integer.parseInt(p[1]);
+    
+    static int strToInt(String  s) {
+        int h = Integer.parseInt(s.split(":")[0]);
+        int m = Integer.parseInt(s.split(":")[1]);
+        return h * 60 + m;
     }
-
-    static String intToString(int minutes) {
-        int h = minutes / 60, m = minutes % 60;
+    
+    static String intToStr (int s) {
+        int h = s / 60;
+        int m = s % 60;
+        
         return String.format("%02d:%02d", h, m);
     }
 }
+
+/*
+조건
+- 9시부터 n회 t분 간격으로 셔틀 운행
+- 최대 m명 탑승
+
+요구
+- 콘이 셔틀을 타고 사무실로 갈 수 있는 도착 시각 중 제일 늦은 시각.
+
+풀이
+- 시분을 분으로 통일
+- n번 반복문 돌리기.
+- 각 버스마다 앞에서부터 탈 수 있는 사람 태우기.
+- 자리가 남아있고, 줄 선 시각이 버스 시간 전
+- 마지막으로 탄사람 기록
+- 마지막에 자리가 남아있으면 버스 시간 기록
+- 자리가 없으면 마지막 탄사람보다 1분빨리 기록
+*/
