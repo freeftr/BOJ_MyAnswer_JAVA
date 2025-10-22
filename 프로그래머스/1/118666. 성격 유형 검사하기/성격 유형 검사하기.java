de76c6f0708127ae9fced0f27e0833f2cd62d4e2
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-22 14:25:28 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 18.70 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -2,64 +2,62 @@ import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
-        int[] score = {0, 3, 2, 1, 0, 1, 2, 3};
-        HashMap<String, Integer> map = new HashMap<>();
+        int[] scores = {0, 3, 2, 1, 0, 1, 2, 3};
+        HashMap<String, Integer> result = new HashMap<>();
+        result.put("R", 0);
+        result.put("T", 0);
+        result.put("C", 0);
+        result.put("F", 0);
+        result.put("J", 0);
+        result.put("M", 0);
+        result.put("A", 0);
+        result.put("N", 0);
         
-        for (int i = 0; i < choices.length; i++) {
-            String type = survey[i];
-            String one = type.split("")[0];
-            String two = type.split("")[1];
-            
-            if (choices[i] < 4) {
-                map.merge(one, score[choices[i]], (a, b) -> a + b);   
+        
+        for (int i = 0; i < survey.length; i++) {
+            String apeach = survey[i].split("")[0];
+            String neo = survey[i].split("")[1];
+
+            if (choices[i] <= 3) {
+                result.merge(apeach, scores[choices[i]], (a, b) -> a + b);
+            } else {
+                result.merge(neo, scores[choices[i]], (a, b) -> a + b);
             }
-            if (choices[i] > 4) { 
-                map.merge(two, score[choices[i]], (a, b) -> a + b);   
-            }
-        }
-        int R = map.getOrDefault("R", 0);
-        int T = map.getOrDefault("T", 0);
-        int C = map.getOrDefault("C", 0);
-        int F = map.getOrDefault("F", 0);
-        int J = map.getOrDefault("J", 0);
-        int M = map.getOrDefault("M", 0);
-        int A = map.getOrDefault("A", 0);
-        int N = map.getOrDefault("N", 0);
-        
-        if (R >= T) {
-            answer += "R";
-        } else {
-            answer += "T";
         }
         
-        if (C >= F) {
-            answer += "C";
-        } else {
-            answer += "F";
-        }
+        int R = 0, T = 0;
+        int C = 0, F = 0;
+        int J = 0, M = 0;
+        int A = 0, N = 0;
         
-        if (J >= M) {
-            answer += "J";
-        } else {
-            answer += "M";
-        }
+        R = result.get("R");
+        T = result.get("T");
+        C = result.get("C");
+        F = result.get("F");
+        J = result.get("J");
+        M = result.get("M");
+        A = result.get("A");
+        N = result.get("N");
         
-        if (A >= N) {
-            answer += "A";
-        } else {
-            answer += "N";
-        }
+        answer += R >= T ? "R" : "T";
+        answer += C >= F ? "C" : "F";
+        answer += J >= M ? "J" : "M";
+        answer += A >= N ? "A" : "N";
         
         return answer;
     }
 }
 
 /*
-1 -> R, T
-2 -> C, F
-3 -> J, M
-4 -> A, N
-n개의 질문
-3 2 1 0 1 2 3
-각 지표에서 높은 점수 선택, 같으면 사전순
+조건
+- 1 -> R, T
+- 2 -> C, F
+- 3 -> J, M
+- 4 -> A, N
+- 점수표 -> 3 2 1 0 1 2 3
+- 결과 = 모든 질문의 성격 유형 점수를 더해 각 지표에서 더 높은 점수를 받은 것.
+- 같으면 사전순
+
+요구
+- 검사자의 성격 유형 검사 지표 번호 순대로.
 */
