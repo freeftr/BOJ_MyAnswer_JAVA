93ae078e0252a5251a0caec931cc6624fc0e398c
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-23 17:14:37 +0900

    [level 2] Title: [3차] 압축, Time: 25.12 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,36 +2,61 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
+        int[] answer = {};
+        ArrayList<String> alphabets = new ArrayList<>();
         ArrayList<Integer> result = new ArrayList<>();
-        ArrayList<String> dict = new ArrayList<>();
+        alphabets.add("1");
         
+        // 1. 사전 초기화 인덱스 1부터 시작.
         for (int i = 0; i < 26; i++) {
-            dict.add("" + (char)('A' + i));
+            String c = (char) (65 + i) + "";
+            alphabets.add(c);
         }
         
-        int i = 0;
-        while (i < msg.length()) {
-            String w = msg.charAt(i) + "";
-            int idx = i;
+        int len = msg.length();
+        StringBuilder sb = new StringBuilder();
+        sb.append(msg.charAt(0));
+        
+        int idx = 1;
+        while (idx < len) {
+            char c = msg.charAt(idx);
+            String w = sb.toString();
+            String temp = w + c;
             
-            while (idx + 1 < msg.length() && dict.contains(w + msg.charAt(idx + 1))) {
-                w += msg.charAt(++idx);
+            // 사전에 있는 경우.
+            if (alphabets.contains(temp)) {
+                idx++;
+                sb.append(c);
             }
             
-            result.add(dict.indexOf(w) + 1);
-            
-            if (idx + 1 < msg.length()) {
-                dict.add(w + msg.charAt(idx + 1));
+            // 사전에 없는 경우.
+            if (!alphabets.contains(temp)) {
+                result.add(alphabets.indexOf(w));
+                alphabets.add(temp);
+                sb = new StringBuilder();
+                sb.append(c);
+                idx++;
             }
             
-            i = idx + 1;
         }
         
-        int[] answer = new int[result.size()];
-        for (int j = 0; j < result.size(); j++) {
-            answer[j] = result.get(j);
+        result.add(alphabets.indexOf(sb.toString()));
+        
+        answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
         }
         
         return answer;
     }
 }
+
+/*
+조건
+- LZW로 압축함.
+- 알파벳 다 넣기.
+- 현재 입력과 일치하는 w 찾기.
+- w 인덱스 출력하고 입력 버퍼 비우기.
+- 남아있는 c를 w + c로 만들어서 사전 등록.
+- 대문자만 처리.
+*/
