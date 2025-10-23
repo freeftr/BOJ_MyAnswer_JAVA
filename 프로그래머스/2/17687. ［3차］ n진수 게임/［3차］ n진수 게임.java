591f470559009ca3e1fc3188f98c5d21221cc396
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-23 16:54:35 +0900

    [level 2] Title: [3차] n진수 게임, Time: 35.59 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,27 +1,40 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
         String answer = "";
-        String s = "";
-        int idx = 0;
-        while (true) {
-            String a = Integer.toString(idx++, n);
-            s += a;
-            if (s.length() > t * m) {
-                break;
-            }
+        
+        StringBuilder sb = new StringBuilder();
+        // 어디까지 탐색해야하는지 구해야 함.
+        
+        int num = 0;
+        int limit = m * (t + 1);
+    
+        while (sb.length() <= m * (t + 1)) {
+            sb.append(Integer.toString(num, n));
+            // System.out.println(Integer.toString(num, n));
+            num++;
         }
-        s = s.substring(0, t * m);
+        
+        String s = sb.toString();
         s = s.toUpperCase();
-        System.out.println(s);
         
-        if (m == p) p = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if ((i + 1) % m == p) {
-                answer += s.charAt(i) + "";
-            }
+        for (int i = 0; i <= m * (t + 1); i++) {
+            if (answer.length() == t) break;
+            if (i % m == p - 1) {
+                answer += s.charAt(i);
+            }   
         }
         
         return answer;
     }
 }
+
+
+/*
+조건
+- 0부터 차례대로 말함.
+- 10 이상 부터는 1, 0, 1, 1 이런식으로 나감.
+- 이걸 N진수 함.
+
+요구
+- n진법으로 t개 미리 구해서 m명에 p번째면 뭐뭐말해야 하는지.
+*/
