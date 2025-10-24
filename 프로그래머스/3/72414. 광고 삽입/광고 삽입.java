17913e7607bbc11b869e9da3ed46c8b4863ca87b
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-24 14:48:59 +0900

    [level 3] Title: 광고 삽입, Time: 3626.92 ms, Memory: 554 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,59 +1,71 @@
-import java.util.*;
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
         String answer = "";
-        int[] total = new int[100 * 60 * 60];
-
-        if (play_time.equals(adv_time)) return "00:00:00";
-
+        
+        int total = strToInt(play_time);
+        int adv = strToInt(adv_time);
+        
+        int[] timeLine = new int[total + 1];
+        
         for (String log : logs) {
             int s = strToInt(log.split("-")[0]);
             int e = strToInt(log.split("-")[1]);
+            
             for (int i = s; i < e; i++) {
-                total[i]++;
+                timeLine[i]++;
+            }
+        }
+        
+        long[] preSum = new long[total + 1];
+        
+        preSum[0] = timeLine[0];
+        
+        for (int i = 1; i <= total; i++) {
+            preSum[i] = preSum[i - 1] + timeLine[i - 1];
+        }
+        
+        int bestStart = 0;
+        long best = preSum[adv] - preSum[0];
+        long window = best;
+
+        for (int start = 1; start + adv <= total; start++) {
+            long curr = preSum[start + adv] - preSum[start];
+            if (curr > best) {
+                best = curr;
+                bestStart = start;
             }
         }
 
-        long sum = 0;
-        int len = strToInt(adv_time);
-        int end = strToInt(play_time);
-
-        for (int i = 0; i < len; i++) {
-            sum += total[i];
-        }
-        long max = sum;
-        answer = "00:00:00";
-
-        for (int i = len; i < end; i++) {
-            sum += total[i];
-            sum -= total[i - len];
-
-            if (sum > max) {
-                max = sum;
-                answer = intToStr(i - len + 1);
-            }
-        }
-
-        return answer;
+        return intToStr(bestStart);
     }
-
+    
     static int strToInt(String s) {
-        int hour = Integer.parseInt(s.split(":")[0]);
-        int min  = Integer.parseInt(s.split(":")[1]);
-        int sec  = Integer.parseInt(s.split(":")[2]);
-        return hour * 60 * 60 + min * 60 + sec;
+        int h = Integer.parseInt(s.split(":")[0]);
+        int m = Integer.parseInt(s.split(":")[1]);
+        int c = Integer.parseInt(s.split(":")[2]);
+        
+        return h * 60 * 60 + m * 60 + c;
     }
-
-    static String intToStr(int s) {
-        int hour = s / 3600;
-        s %= 3600;
-        int min  = s / 60;
-        int sec  = s % 60;
-
-        String h  = (hour < 10 ? "0" : "") + hour;
-        String m  = (min  < 10 ? "0" : "") + min;
-        String se = (sec  < 10 ? "0" : "") + sec;
-
-        return h + ":" + m + ":" + se;
+    
+    static String intToStr(int a) {
+        int s = a % 60;
+        a /= 60;
+        int m = a % 60;
+        a /= 60;
+        int h = a;
+        return String.format("%02d:%02d:%02d", h, m, s);
     }
 }
+
+/*
+조건
+- 사람들이 가장 많이 보는 구간에 광고 넣음.
+- 재생구간 기록 있음.
+- 최적 위치 찾기.
+
+요구
+- 누적 재생시간이 가장 많이 나오는 곳에 공익 광고 넣음.
+
+풀이
+- 투포인터? 누적합?
+*/
