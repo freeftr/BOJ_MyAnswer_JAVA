af6b7cd285f98e3a74016ff856fb927197ab561f
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-28 12:49:23 +0900

    [Gold V] Title: 두 용액, Time: 336 ms, Memory: 29800 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,53 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        long[] arr = new long[N];
+        StringTokenizer st;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        long ans = Long.MAX_VALUE;
-        long alcal = 0;
-        long acid = 0;
+        int left = 0;
+        int right = N - 1;
+        int a = 0;
+        int b = 0;
+        long max = Long.MAX_VALUE;
+        while (left < right) {
+            long sumVal = (long) arr[left] + arr[right];
+            long sum = Math.abs(sumVal);
 
-        for (int i = 0; i < N - 1; i++) {
-            long current = arr[i];
-
-            int left = i + 1;
-            int right = N - 1;
-
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                long sum = current + arr[mid];
-
-                if (Math.abs(sum) < Math.abs(ans)) {
-                    ans = sum;
-                    alcal = current;
-                    acid = arr[mid];
-                }
-
-                if (sum > 0) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            if (sum < max) {
+                max = sum;
+                a = arr[left];
+                b = arr[right];
+                if (sum == 0) break;
             }
+
+            if (sumVal > 0) right--;
+            else left++;
         }
 
-        if (alcal > acid) {
-            long temp = alcal;
-            alcal = acid;
-            acid = temp;
-        }
-
-        System.out.println(alcal + " " + acid);
+        System.out.println(a + " " + b);
     }
 }
+
+/*
+조건
+- 산성용액 특성값 1 ~ 1_000_000_000
+- 알칼리 특성값 -1 ~ -1_000_000_000
+- 혼합 특성값 = 혼합에 사용된 각 용액의 특성값의 합.
+- 0에 가장 가깝게 하려고 함.
+
+요구
+- 0에 가장 가까운 용액을 만들어내는 두 용액 특성값 찾기.
+ */
