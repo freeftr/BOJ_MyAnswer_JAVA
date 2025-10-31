830e2daf25a55a0a9016114fab6cbc31fc7e9d1e
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-10-31 13:15:45 +0900

    [Gold V] Title: 보물섬, Time: 512 ms, Memory: 221504 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static char[][] map;
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 'L') {
+                    answer = Math.max(answer, bfs(i, j));
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static int bfs(int a, int b) {
+        Queue<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][M];
+
+        q.add(new int[]{a, b, 0});
+        visited[a][b] = true;
+
+        int max = 0;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int dist = cur[2];
+
+            max = Math.max(max, dist);
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (visited[nx][ny]) continue;
+                if (map[nx][ny] == 'W') continue;
+
+                visited[nx][ny] = true;
+                q.add(new int[]{nx, ny, dist + 1});
+            }
+        }
+
+        return max;
+    }
+}
