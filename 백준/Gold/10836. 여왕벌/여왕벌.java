c137c88c62ef682feba854b5d81b9358f4b90d27
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-11-12 16:07:01 +0900

    [Gold III] Title: 여왕벌, Time: 3632 ms, Memory: 293512 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -14,8 +14,6 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[M][M];
-        int[] dx = {0, -1, -1};
-        int[] dy = {-1, -1, 0};
 
         for (int i = 0; i < M; i++) {
             Arrays.fill(map[i], 1);
@@ -27,11 +25,14 @@ public class Main {
             int one = Integer.parseInt(st.nextToken());
             int two = Integer.parseInt(st.nextToken());
 
-            int[][] diff = leadGrowth(M, zero, one, map, two);
+            map = leadGrowth(M, zero, one, map, two);
+        }
 
-            diff = afterGrowth(M, dx, dy, diff);
-
-            map = merge(M, map, diff);
+        for (int y = 1; y < M; y++) {
+            int max = map[0][y];
+            for (int x = 1; x < M; x++) {
+                map[x][y] = max;
+            }
         }
 
         for (int i = 0; i < M; i++) {
@@ -44,40 +45,9 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static int[][] merge(int M, int[][] map, int[][] diff) {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < M; j++) {
-                map[i][j] += diff[i][j];
-            }
-        }
-
-        return map;
-    }
-
-    private static int[][] afterGrowth(int M, int[] dx, int[] dy, int[][] diff) {
-        for (int x = 1; x < M; x++) {
-            for (int y = 1; y < M; y++) {
-                int max = 0;
-
-                for (int j = 0; j < 3; j++) {
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
-
-                    max = Math.max(max, diff[nx][ny]);
-                }
-
-                diff[x][y] = max;
-            }
-        }
-
-        return diff;
-    }
-
     private static int[][] leadGrowth(int M, int zero, int one, int[][] map, int two) {
         int x = M - 1;
         int y = 0;
-
-        int[][] temp = new int[M][M];
 
         while (zero != 0) {
             if (x == 0) {
@@ -89,7 +59,7 @@ public class Main {
         }
 
         while (one != 0) {
-            temp[x][y] += 1;
+            map[x][y] += 1;
             if (x == 0) {
                 y++;
             } else {
@@ -99,7 +69,7 @@ public class Main {
         }
 
         while (two != 0) {
-            temp[x][y] += 2;
+            map[x][y] += 2;
             if (x == 0) {
                 y++;
             } else {
@@ -108,7 +78,7 @@ public class Main {
             two--;
         }
 
-        return temp;
+        return map;
     }
 }
 
