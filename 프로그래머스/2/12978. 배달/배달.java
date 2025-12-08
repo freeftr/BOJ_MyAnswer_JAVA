d97b36c125cb40787ca522ef7059213c7d46d2d0
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-08 14:41:23 +0900

    [level 2] Title: 배달, Time: 3.62 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,58 +1,71 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-
+        int answer = 1;
+        
+        // 간선 정보 전처리
         ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+        int[] dist = new int[N + 1];
+        
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
         
         for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
         }
         
-        for (int[] roa : road) {
-            int a = roa[0];
-            int b = roa[1];
-            int cost = roa[2];
+        for (int[] r : road) {
+            int a = r[0];
+            int b = r[1];
+            int c = r[2];
             
-            graph.get(a).add(new int[]{b, cost});
-            graph.get(b).add(new int[]{a, cost});
+            graph.get(b).add(new int[]{a, c});
+            graph.get(a).add(new int[]{b, c});
         }
-        int[] dist = new int[N+1];
         
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
-        dist[1] = 0;
-        
+        // 다익스트라
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         pq.add(new int[]{1, 0});
         
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             int[] cur = pq.poll();
+            int v = cur[0];
+            int d = cur[1];
             
-            int now = cur[0];
-            int dis = cur[1];
+            if (d > dist[v]) continue;
             
-            if (dis > dist[now]) continue;
-            
-            for (int[] nv : graph.get(now)) {
-                int next = nv[0];
-                int nextDis = nv[1];
-                
-                if (dist[next] > dist[now] + nextDis) {
-                    dist[next] = dist[now] + nextDis;
-                    pq.add(new int[]{next, dist[next]});
+            for (int[] n : graph.get(v)) {
+                int nv = n[0];
+                int nd = n[1];
+
+                if (dist[nv] > dist[v] + nd) {
+                    dist[nv] = dist[v] + nd;
+                    pq.add(new int[]{nv, dist[nv]});
                 }
             }
         }
         
-        for (int i = 1; i <= N; i++) {
-            if (dist[i] <= K) {
-                answer++;
-            }
+        // K 이하 확인
+        for (int i = 2; i <= N; i++) {
+            if (dist[i] <= K) answer++;
         }
-
+        
         return answer;
     }
 }
+
+/*
+조건
+- N개의 마을로 이루어진 나라.
+- 양방향
+- 도로별 가중치 있음.
+- 가중치 합 제한 = K
+- 음식점은 1번 마을에 있음.
+
+요구
+- 음식 주문을 받을 수 있는 마을 의 개수 구하기
+
+풀이
+- 1번마을에서 다익스트라 돌리기
+*/
