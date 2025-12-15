4832a5cd899d8690755956cf7e722b90d3404664
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-15 09:48:19 +0900

    [Gold IV] Title: 해킹, Time: 788 ms, Memory: 156420 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.java
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -1,40 +1,34 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n, d, c;
-    static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
-    static int[] dist;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int t = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for (int tc = 1; tc <= t; tc++) {
-
-            graph = new ArrayList<>();
-
+        for (int tc = 0; tc < T; tc++) {
             st = new StringTokenizer(br.readLine());
 
-            n = Integer.parseInt(st.nextToken());
-            d = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
 
             for (int i = 0; i <= n; i++) {
                 graph.add(new ArrayList<>());
             }
 
-            dist = new int[n+1];
-            Arrays.fill(dist, Integer.MAX_VALUE);
-            dist[c] = 0;
-
             for (int i = 0; i < d; i++) {
                 st = new StringTokenizer(br.readLine());
-
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
                 int s = Integer.parseInt(st.nextToken());
@@ -42,37 +36,52 @@ public class Main {
                 graph.get(b).add(new int[]{a, s});
             }
 
-            dijkstra(c);
+            int[] dist = new int[n + 1];
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+            Arrays.fill(dist, Integer.MAX_VALUE);
 
-            int answer = 0;
+            dist[c] = 0;
+            pq.add(new int[]{c, 0});
+
+            while (!pq.isEmpty()) {
+                int[] cur = pq.poll();
+                int V = cur[0];
+                int D = cur[1];
+
+                if (dist[V] < D) continue;
+
+                for (int[] N : graph.get(V)) {
+                    int nv = N[0];
+                    int nd = N[1];
+
+                    if (dist[nv] > dist[V] + nd) {
+                        dist[nv] = dist[V] + nd;
+                        pq.add(new int[]{nv, dist[nv]});
+                    }
+                }
+            }
+
+            int cnt = 0;
             int max = 0;
-            for (int i = 1; i <= n; i++) {
-                if (dist[i]!=Integer.MAX_VALUE) {
-                    answer++;
+
+            for (int i = 1; i < n + 1; i++) {
+                if (dist[i] != Integer.MAX_VALUE) {
+                    cnt++;
                     max = Math.max(max, dist[i]);
                 }
             }
-            sb.append(answer + " " + max + "\n");
+
+            sb.append(cnt + " " + max).append("\n");
         }
 
         System.out.println(sb);
     }
-
-    private static void dijkstra(int start) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        pq.offer(new int[]{start, 0});
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-
-            if (dist[cur[0]] < cur[1]) continue;
-
-            for (int[] next : graph.get(cur[0])) {
-                if (dist[next[0]] > dist[cur[0]] + next[1]) {
-                    dist[next[0]] = dist[cur[0]] + next[1];
-                    pq.offer(new int[]{next[0], dist[next[0]]});
-                }
-            }
-        }
-    }
 }
+
+/*
+조건
+- a가 b에 의존 = b -> a
+
+요구
+- 몇대의 컴퓨터가 감염되고, 얼마나 걸리는지
+ */
