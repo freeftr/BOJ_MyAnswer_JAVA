3c81744891f041ab4fae0212ac385ec1f554bce1
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-17 14:40:22 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.18 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/README.md
+++ 프로그래머스/2/12941. 최솟값 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최솟값 만들기 - 12941 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941) 
 
 ### 성능 요약
 
-메모리: 54 MB, 시간: 2.15 ms
+메모리: 54.3 MB, 시간: 2.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 17:44:06
+2025년 12월 17일 14:40:20
 
 ### 문제 설명
 
--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,14 +1,21 @@
 import java.util.*;
-class Solution {
-    public int solution(int[] A, int []B)
+
+class Solution
+{
+    public int solution(int []A, int []B)
     {
         int answer = 0;
         
         Arrays.sort(A);
         Arrays.sort(B);
         
-        for (int i = 0; i < A.length; i++) {
-            answer += A[i] * B[A.length - i - 1];
+        int len = A.length;
+        
+        for (int i = 0; i < len; i++) {
+            int a = A[i];
+            int b = B[len - i - 1];
+            
+            answer += a * b;
         }
 
         return answer;
@@ -17,9 +24,15 @@ class Solution {
 
 /*
 조건
-- A, B 배열
-- 두 수를 곱한 값을 누적하여 더하기.
+- 길이가 같은 두 배열 A, B
+- 각각 한 숫자씩 뽑아서 곱한값 누적.
+- 배열의 길이만큼 반복.
+- 누적값이 최소가 되게.
 
 요구
-- 누적된 값이 최소가 되게.
+- 누적된 최솟값 구해라.
+
+풀이
+1. 배열 둘 다 정렬
+2. A 가장 큰값 * B 가장 작은값 + B 가장 작은값 * A 가장 큰값
 */
