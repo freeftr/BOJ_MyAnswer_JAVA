939d8a22ec0120793b5781a9e005793008c2b501
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-17 16:10:27 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 47.46 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -3,63 +3,84 @@ import java.util.*;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
-        int N = friends.length;
-        int[][] record = new int[N][N];
-        HashMap<String, Integer> map = new HashMap<>();
+        int n = friends.length;
         
-        for (int i = 0; i < N; i++) {
-            map.put(friends[i], i);
+        HashMap<String, Integer> indexMap = new HashMap<>();
+        
+        for(int i = 0; i < n; i++) {
+            indexMap.put(friends[i], i);
         }
+        
+        int[][] record = new int[n][n];
+        int[] result = new int[n];
         
         for (String gift : gifts) {
-            String[] parts = gift.split(" ");
-            String give = parts[0];
-            String get = parts[1];
+            String give = gift.split(" ")[0];
+            String receive = gift.split(" ")[1];
             
-            int gi = map.get(give);
-            int ge = map.get(get);
-            record[gi][ge]++;
+            int gIdx = indexMap.get(give);
+            int rIdx = indexMap.get(receive);
+            
+            record[gIdx][rIdx]++;
         }
         
-        // 선물 지수: (준 수 - 받은 수)
-        int[] score = new int[N];
-        for (int i = 0; i < N; i++) {
-            int s = 0;
-            for (int k = 0; k < N; k++) {
-                s += record[i][k];   // i가 k에게 준 수
-                s -= record[k][i];   // k가 i에게 준 수
-            }
-            score[i] = s;
-        }
-
-        int[] result = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 int A = record[i][j];
                 int B = record[j][i];
-
+                
                 if (A > B) {
                     result[i]++;
-                } else if (B > A) {
+                } else if (A == B) {
+                    int aScore = getScore(record, i);
+                    int bScore = getScore(record, j);
+                    
+                    if (aScore > bScore) {
+                        result[i]++;
+                    }
+                    
+                    if (bScore > aScore) {
+                        result[j]++;
+                    }
+                } else {
                     result[j]++;
-                } else { // A == B 이면 선물 지수 비교
-                    if (score[i] > score[j]) result[i]++;
-                    else if (score[j] > score[i]) result[j]++;
                 }
             }
         }
-
-        for (int x : result) {
-            if (x > answer) answer = x;
+        
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, result[i]);
         }
-        return answer;
+        
+        return answer /= 2;
+    }
+    
+    static int getScore(int[][] record, int idx) {
+        int give = 0;
+        int receive = 0;
+        
+        for (int i = 0; i < record.length; i++) {
+            give += record[idx][i];
+            receive += record[i][idx];
+        }
+        
+        return give - receive;
     }
 }
 
 /*
 조건
-- 주고받은 기록 있음 -> 더 많은 선물을 준 사람이 다음달에 수령
-- 주고받은 기록이 같음(0 포함) -> 선물 지수가 큰 사람이 수령
-- 선물 지수 = (준 수 - 받은 수)
+- 선물 기록을 통해 다음달에 누가 가장 많이 받을지 찾기
+- 주고받은 기록 있으면 -> 더 많이 준 사람이 다음 달에 하나
+- 같거나 없으면 -> 선물 지수가 더 큰사람이 다음달에
+- 선물지수 = 여태까지 준거 - 받은거
+
+요구
+- 가장 많이 받는 친구의 선물 수
+
+풀이
+- <이름, 내가 준 사람>
+- <이름, 나한테 준 사람>
+- 이렇게 두 개 만들고 구하기
+- 이차원 배열 만들어서 기록해야 할듯?
 */
