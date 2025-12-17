0928be86ed05489cf106681d8430b9789a12bea4
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-17 14:32:46 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.51 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 87.1 MB, 시간: 15.79 ms
+메모리: 76.9 MB, 시간: 10.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 17:35:59
+2025년 12월 17일 14:32:43
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,16 +1,28 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] words = s.split(" ");
-        int[] nums = new int[words.length];
         
-        for (int i = 0; i < words.length; i++) {
-            nums[i] = Integer.parseInt(words[i]);
+        String[] numbers = s.split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for (String number : numbers) {
+            int num = Integer.parseInt(number);
+            
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
         
-        Arrays.sort(nums);
-        return nums[0] + " " + nums[nums.length - 1];
+        answer = min + " " + max;
+        
+        return answer;
     }
 }
+/*
+조건
+- 공백으로 구분된 숫자
+
+요구
+- 최댓값, 최솟값 찾아서 리턴
+*/
