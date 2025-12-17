9e85318ee6d27622a6d3b225c12310b306685d88
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-17 14:53:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.13 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,27 +1,41 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         
-        boolean space = true;
+        StringBuilder sb = new StringBuilder();
+        char prev = ' ';
+        
         for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
+            char now = s.charAt(i);
             
-            if (cur == ' ') {
-                sb.append(' ');
-                space = true;
-                continue;
-            }
-            
-            cur = Character.toLowerCase(cur);
-            
-            if (space && 'a' <= cur && cur <= 'z') {
-                sb.append(Character.toUpperCase(cur));
+            if (now == ' ') {
+                sb.append(" ");
+            } else if (prev == ' ' && now != ' ') {
+                if (Character.isLetter(now)) {
+                    sb.append(Character.toUpperCase(now));
+                } else {
+                    sb.append(now);
+                }
             } else {
-                sb.append(cur);
+               sb.append(Character.toLowerCase(now)); 
             }
             
-            space = false;
+            prev = now;
         }
-        return sb.toString();
+        
+        answer = sb.toString();
+        
+        return answer;
     }
 }
+
+/*
+조건
+- JadenCase = 첫 문자 대문자, 나머지 소문자.
+
+요구
+- s를 JadenCase로 만들어라.
+
+풀이
+- 문자열 잘라서 해 그냥.
+*/
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 84.9 MB, 시간: 0.15 ms
+메모리: 88.1 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 17:31:43
+2025년 12월 17일 14:53:56
 
 ### 문제 설명
 
