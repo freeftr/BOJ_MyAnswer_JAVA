9b3a48858981b1f14c1008fd4393701fb74cebc6
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-17 14:36:37 +0900

    [level 2] Title: 올바른 괄호, Time: 59.04 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,17 +3,34 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push('(');
+        
+        Stack<String> stack = new Stack<>();
+        
+        String[] letters = s.split("");
+        
+        for (String letter : letters) {
+            if (letter.equals("(")) {
+                stack.push("(");
             } else {
                 if (stack.isEmpty()) return false;
-                if (stack.peek() == '(') {
-                    stack.pop();
-                }
+                
+                stack.pop();
             }
         }
-        return stack.isEmpty();
+        
+        answer = stack.isEmpty() ? true : false;
+        
+        return answer;
     }
 }
+
+/*
+조건
+- 괄호가 바르게 짝지어졋다. = ()
+
+요구
+- 올바르면 true, 틀리면 false;
+
+풀이
+- 스택
+*/
