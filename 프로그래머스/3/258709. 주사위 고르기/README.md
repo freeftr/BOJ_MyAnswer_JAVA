8fd2b54ac3509847708ed11c192462c7b993cf71
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-19 14:50:52 +0900

    [level 3] Title: 주사위 고르기, Time: 693.62 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/README.md
+++ 프로그래머스/3/258709. 주사위 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 주사위 고르기 - 258709 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258709) 
 
 ### 성능 요약
 
-메모리: 280 MB, 시간: 970.93 ms
+메모리: 143 MB, 시간: 693.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 26일 16:59:50
+2025년 12월 19일 14:50:47
 
 ### 문제 설명
 
--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,102 +1,107 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
 
-    public static List<Integer> solution(int[][] dices) {
-        Map<Integer, List<Integer>> dic = new HashMap<>();
-        int L = dices.length;
+    static int N;
+    static int[][] diceInfo;
+    static int cnt = 0;
+    static int max = 0;
+    static ArrayList<Integer> result = new ArrayList<>();
 
-        List<List<Integer>> combinations = combi(L, L / 2);
+    public int[] solution(int[][] dice) {
+        int[] answer = {};
 
-        for (List<Integer> aidx : combinations) {
-            List<Integer> bidx = new ArrayList<>();
-            for (int i = 0; i < L; i++) {
-                if (!aidx.contains(i)) {
-                    bidx.add(i);
-                }
+        N = dice.length;
+        diceInfo = new int[N][6];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 6; j++) {
+                diceInfo[i][j] = dice[i][j];
             }
-
-            List<Integer> A = new ArrayList<>();
-            List<Integer> B = new ArrayList<>();
-
-            List<List<Integer>> orderProduct = generateProduct(L / 2, 6);
-            for (List<Integer> order : orderProduct) {
-                int sumA = 0, sumB = 0;
-                for (int i = 0; i < aidx.size(); i++) {
-                    sumA += dices[aidx.get(i)][order.get(i)];
-                }
-                for (int i = 0; i < bidx.size(); i++) {
-                    sumB += dices[bidx.get(i)][order.get(i)];
-                }
-                A.add(sumA);
-                B.add(sumB);
-            }
-
-            Collections.sort(B);
-
-            int wins = 0;
-            for (int num : A) {
-                wins += bs(B, num);
-            }
-
-            dic.put(wins, aidx);
         }
 
-        int maxKey = Collections.max(dic.keySet());
-        List<Integer> result = new ArrayList<>();
-        for (int index : dic.get(maxKey)) {
-            result.add(index + 1); 
+        combinations(0, 0, new ArrayList<Integer>());
+
+        Collections.sort(result);
+        answer = new int[N / 2];
+
+        for (int i = 0; i < N / 2; i++) {
+            answer[i] = result.get(i) + 1;
         }
 
-        return result;
+        return answer;
     }
 
-    private static List<List<Integer>> combi(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        combineHelper(result, new ArrayList<>(), 0, n, k);
-        return result;
-    }
-
-    private static void combineHelper(List<List<Integer>> result, List<Integer> temp, int start, int n, int k) {
-        if (temp.size() == k) {
-            result.add(new ArrayList<>(temp));
+    static void combinations(int depth, int start, ArrayList<Integer> selected) {
+        if (depth == N / 2) {
+            calculate(selected);
+            if (max < cnt) {
+                max = cnt;
+                result = new ArrayList<>();
+                result.addAll(selected);
+            }
             return;
         }
-        for (int i = start; i < n; i++) {
-            temp.add(i);
-            combineHelper(result, temp, i + 1, n, k);
-            temp.remove(temp.size() - 1);
+
+        for (int i = start; i < N; i++) {
+            selected.add(i);
+            combinations(depth + 1, i + 1, selected);
+            selected.remove(selected.size() - 1);
         }
     }
 
-    private static List<List<Integer>> generateProduct(int n, int range) {
-        List<List<Integer>> result = new ArrayList<>();
-        productHelper(result, new ArrayList<>(), n, range);
-        return result;
+    static void calculate(ArrayList<Integer> A) {
+        ArrayList<Integer> B = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            if (!A.contains(i)) B.add(i);
+        }
+
+        ArrayList<Integer> sumAList = new ArrayList<>();
+        ArrayList<Integer> sumBList = new ArrayList<>();
+
+        getSumA(0, 0, A, sumAList);
+        getSumB(0, 0, B, sumBList);
+
+        Collections.sort(sumBList);
+
+        cnt = 0;
+        for (int a : sumAList) {
+            cnt += lowerBound(sumBList, a);
+        }
     }
 
-    private static void productHelper(List<List<Integer>> result, List<Integer> temp, int n, int range) {
-        if (temp.size() == n) {
-            result.add(new ArrayList<>(temp));
+    static void getSumA(int depth, int sum, ArrayList<Integer> A, ArrayList<Integer> list) {
+        if (depth == N / 2) {
+            list.add(sum);
             return;
         }
-        for (int i = 0; i < range; i++) {
-            temp.add(i);
-            productHelper(result, temp, n, range);
-            temp.remove(temp.size() - 1);
+
+        int dice = A.get(depth);
+        for (int i = 0; i < 6; i++) {
+            getSumA(depth + 1, sum + diceInfo[dice][i], A, list);
         }
     }
 
-    private static int bs(List<Integer> sortedList, int target) {
-        int left = 0, right = sortedList.size();
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (sortedList.get(mid) < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+    static void getSumB(int depth, int sum, ArrayList<Integer> B, ArrayList<Integer> list) {
+        if (depth == N / 2) {
+            list.add(sum);
+            return;
         }
-        return left;
+
+        int dice = B.get(depth);
+        for (int i = 0; i < 6; i++) {
+            getSumB(depth + 1, sum + diceInfo[dice][i], B, list);
+        }
+    }
+
+    static int lowerBound(ArrayList<Integer> list, int target) {
+        int l = 0, r = list.size();
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (list.get(m) < target) l = m + 1;
+            else r = m;
+        }
+        return l;
     }
 }
