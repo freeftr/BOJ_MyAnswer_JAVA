014cc1ed18bfca63665bd3fddd8580ba5c59c54b
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-22 14:16:35 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.40 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,39 +2,64 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        Map<String, Integer> termMap = new HashMap<>();
-        for (String t : terms) {
-            String[] sp = t.split(" ");
-            termMap.put(sp[0], Integer.parseInt(sp[1]));
+        int[] answer = {};
+        
+        int now = toNum(today);
+        
+        HashMap<String, Integer> termMap = new HashMap<>();
+        
+        for (String term : terms) {
+            String[] temp = term.split(" ");
+            
+            termMap.put(temp[0], Integer.parseInt(temp[1]));
         }
-
-        int todayDays = toDays(today);
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < privacies.length; i++) {
-            String[] sp = privacies[i].split(" ");
-            String collected = sp[0];
-            String termKey = sp[1];
-            int periodMonths = termMap.get(termKey);
-
-            int collectedDays = toDays(collected);
-            int expireDays = collectedDays + periodMonths * 28 - 1;
-
-            if (todayDays > expireDays) {
-                list.add(i + 1);
-            }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        int idx = 1;
+        for (String p : privacies) {
+            String[] temp = p.split(" ");
+            int start = toNum(temp[0]);
+            int end = start + termMap.get(temp[1]) * 28;
+            
+            if (end <= now) result.add(idx);
+            
+            // System.out.println(now + " " + end);
+            
+            idx++;
         }
-
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) answer[i] = list.get(i);
+        
+        answer = new int[result.size()];
+        
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        
         return answer;
     }
-
-    static int toDays(String date) {
-        String[] sp = date.split("\\.");
-        int y = Integer.parseInt(sp[0]);
-        int m = Integer.parseInt(sp[1]);
-        int d = Integer.parseInt(sp[2]);
-        return (y * 12 + (m - 1)) * 28 + d;
+    
+    static int toNum(String day) {
+        String[] temp = day.split("\\.");
+        
+        int result = 0;
+        
+        result += Integer.parseInt(temp[2]);
+        result += Integer.parseInt(temp[1]) * 28;
+        result += Integer.parseInt(temp[0]) * 12 * 28;
+        
+        return result;
     }
 }
+
+/*
+조건
+- 개인정보 n개
+- 약관은 여러 개 있음. 개인정보 - 약관
+- 유효기간 전까지만 보관 가능. -> 달 기준
+- 모든 달은 28일까지
+
+요구
+- 파기해야할 개인정보 구하기
+
+풀이
+- 그냥 구현
+*/
