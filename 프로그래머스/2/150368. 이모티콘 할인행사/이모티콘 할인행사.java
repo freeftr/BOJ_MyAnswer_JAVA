e1238a043a706e3f558944f93d8ce3b9e02ae3b1
Pilot Park <130902228+freeftr@users.noreply.github.com>
2025-12-30 09:38:46 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 69.60 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,41 +1,85 @@
 import java.util.*;
 
 class Solution {
-    static int[] dr = {10, 20, 30, 40};
-    static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-        if (a[0] == b[0]) return b[1] - a[1];
+    
+    static int n, m;
+    static PriorityQueue<int[]> result = new PriorityQueue<>((a, b) -> {
+        if (b[0] == a[0]) {
+            return b[1] - a[1];
+        }
         return b[0] - a[0];
     });
-
+    static int[] discountRatios = {10, 20, 30, 40};
+    
     public int[] solution(int[][] users, int[] emoticons) {
-        dfs(0, new int[emoticons.length], emoticons, users);
-        return pq.poll();
+        int[] answer = new int[2];
+        n = users.length;
+        m = emoticons.length;
+        
+        makeComb(0, new int[m], users, emoticons);
+        
+        answer = result.poll();
+        
+        return answer;
     }
     
-    static void dfs(int depth, int[] ratios, int[] emoticons, int[][] users) {
-        if (depth == emoticons.length) {
-            int p = 0;
-            int am = 0;
-
-            for (int[] user : users) {
-                int disReq = user[0];
-                int plusReq = user[1];
-                
-                int sum = 0;
-                for (int i = 0; i < ratios.length; i++) {
-                    if (ratios[i] < disReq) continue;
-                    sum += emoticons[i] * (100 - ratios[i]) / 100;
-                }
-                if (sum >= plusReq) p++;
-                else am += sum;
-            }
-            pq.add(new int[]{p, am});
+    static void makeComb(int depth, int[] selected, int[][] users, int[] emoticons) {
+        if (depth == m) {
+            calculate(selected, users, emoticons);
             return;
         }
         
         for (int i = 0; i < 4; i++) {
-            ratios[depth] = dr[i];
-            dfs(depth + 1, ratios, emoticons, users);
+            selected[depth] = discountRatios[i];
+            makeComb(depth + 1, selected, users, emoticons);
         }
     }
+    
+    static void calculate(int[] ratio, int[][] users, int[] emoticons) {
+        int emPlus = 0;
+        int sum = 0;
+        
+        for (int[] user : users) {
+            int minRatio = user[0];
+            int limit = user[1];
+            int bought = 0;
+            boolean plus = false;
+            
+            for (int i = 0; i < m; i++) {
+                if (ratio[i] >= minRatio) {
+                    bought += emoticons[i] * (100 - ratio[i]) / 100;
+                }
+                
+                if (bought >= limit) {
+                    emPlus++;
+                    plus = true;
+                    break;
+                }
+            }
+            
+            if (!plus) {
+                sum += bought;
+            }
+        }
+        
+        result.add(new int[]{emPlus, sum});
+    }
 }
+
+/*
+조건
+- 이모티콘 할인 행사의 목표는 다음과 같다.
+1. 임티플 가입자 최대한 늘리기
+2. 임티 판매액 최대한 늘리기
+- 할인율은 10, 20, 30, 40중 하나로 설정
+- 사용자들은 자신의 기준 이상 할인하는 임티를 전부 구매한다.
+- 합이 일정 기준이 이상이면 임티플에 가입한다.
+
+요구
+- 행사 목적을 최대한으로 달성했을 때 임티 플 가입자, 매출액 반환
+
+풀이
+- 구현
+1. 이모티콘 할인율 순열로 전부 구하기
+2. 유저별 기준치 확인해서 집계
+*/
