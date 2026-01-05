ab5a17871dcac341d3b27118bcde77dd08fbb4af
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-05 14:28:56 +0900

    [Gold IV] Title: 키 순서, Time: 620 ms, Memory: 37392 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,62 +1,68 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N, M;
-    static ArrayList<ArrayList<Integer>> graph;
-    static ArrayList<ArrayList<Integer>> rgraph;
-    static boolean[] visited;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        graph = new ArrayList<>();
-        rgraph = new ArrayList<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[N + 1][N + 1];
 
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-            rgraph.add(new ArrayList<>());
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph.get(a).add(b);
-            rgraph.get(b).add(a);
+
+            dist[a][b] = 1;
         }
 
-        int ans = 0;
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][k] == 1 && dist[k][j] == 1) {
+                        dist[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        int[] cnt = new int[N + 1];
+
         for (int i = 1; i <= N; i++) {
-            if(bfs(i,graph) + bfs(i,rgraph) == N+1){
-                ans++;
-            }
-        }
-        System.out.println(ans);
-    }
-
-    static int bfs(int start, ArrayList<ArrayList<Integer>> g){
-        visited = new boolean[N+1];
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        visited[start] = true;
-        int cnt = 0;
-        while(!q.isEmpty()){
-            int v = q.poll();
-            cnt++;
-            for (int nv : g.get(v)) {
-                if(visited[nv]) continue;
-                visited[nv] = true;
-                q.add(nv);
+            for (int j = 1; j <= N; j++) {
+                if (dist[i][j] == 1 || dist[j][i] == 1) cnt[i]++;
             }
         }
 
-        return cnt;
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if (cnt[i] == N - 1) answer++;
+        }
+
+        System.out.println(answer);
     }
 }
+
+/*
+조건
+- N 번의 학생
+- 키가 모두 다름.
+- 자신의 순위를 알수가 있음.
+
+요구
+- 자신의 키를 알수 있는 학생 수.
+
+풀이
+- 플로이드 워셜
+
+*/
