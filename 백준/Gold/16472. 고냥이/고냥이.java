94087211f98916053d24d6b0e9b02e56d8552af9
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-07 12:35:16 +0900

    [Gold IV] Title: 고냥이, Time: 140 ms, Memory: 15552 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,50 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
         String s = br.readLine();
 
-        int[] alphabet = new int[26];
-
         int left = 0;
-        int right = 1;
-        int ans = 0;
+        int right = 0;
+        int[] cnts = new int[26];
+        cnts[s.charAt(0) - 'a']++;
+        int cnt = 1;
+        int answer = 1;
 
-        alphabet[s.charAt(0)-'a']++;
-        alphabet[s.charAt(1)-'a']++;
+        while (right < s.length()) {
+            int len = right - left + 1;
 
-        while(left<=right) {
-            int cnt = 0;
-
-            for (int i = 0; i < 26; i++) {
-                if (alphabet[i]!=0) {
-                    cnt++;
-                }
-            }
-//            System.out.println("left: "+left+" right: "+right+" ans: "+ans);
-
-            if (cnt<=n){
-                ans = Math.max(ans, right - left + 1);
+            if (cnt <= N) {
                 right++;
-                if (right == s.length()) {
-                    break;
-                }
-                alphabet[s.charAt(right)-'a']++;
+                answer = Math.max(len, answer);
+                if (right == s.length()) break;
+
+                cnts[s.charAt(right) - 'a']++;
+
+                if (cnts[s.charAt(right) - 'a'] == 1) cnt++;
             } else {
-                alphabet[s.charAt(left)-'a']--;
+                if (cnts[s.charAt(left) - 'a'] == 1) {
+                    cnt--;
+                }
+                cnts[s.charAt(left) - 'a']--;
                 left++;
             }
-
-
         }
 
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
+
+/*
+조건
+- 고양이 번역
+- N개 종류의 알파벳을 가진 연속된 문자열 인식
+
+요구
+- 번역할 수 있는 최대 문자열 길이 구하기
+
+풀이
+- 투포인터로 돌면서 최대 길이 구하기
+ */
