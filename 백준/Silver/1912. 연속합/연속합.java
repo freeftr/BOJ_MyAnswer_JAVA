8574282b94e355f93e1c0896e1d4dfcc55b18634
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-07 12:42:38 +0900

    [Silver II] Title: 연속합, Time: 240 ms, Memory: 24056 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,22 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n+1];
-        int max = 0;
-        for(int i = 1; i <= n; i++) {
-            arr[i] = sc.nextInt();
-            max=Math.min(max, arr[i]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        int[] dp = new int[n+1];
-        dp[1] = arr[1];
-        for(int i = 2; i <= n; i++) {
-            dp[i]= Math.max(dp[i-1] + arr[i],arr[i]);
-            max= Math.max(max,dp[i]);
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        int answer = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            answer = Math.max(answer, dp[i]);
         }
-        max=Math.max(max,dp[1]);
-        System.out.println(max);
+
+        System.out.println(answer);
     }
 }
+
+
+/*
+조건
+- n개의 정수 수열
+- 연속된 수 골라서 가장 큰 합 고르기
+ */
