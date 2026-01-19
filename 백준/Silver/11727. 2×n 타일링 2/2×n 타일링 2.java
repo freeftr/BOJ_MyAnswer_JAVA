84e6cb1c10f0275a327aeab6f6eaee62a8023c07
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-19 18:58:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,15 +1,88 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 3;
-        for(int i=2;i<n;i++) {
-            dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println(1);
+            return;
         }
-        System.out.println(dp[n-1]);
+
+        if (n == 2) {
+            System.out.println(3);
+            return;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
     }
 }
+
+/*
+조건
+- 1 x 2, 2 x 1, 2 x 2
+
+요구
+- 2 x n 직사격형 채우는 방법의 수 구하기
+
+풀이
+- dp
+dp[i] = dp[i - 1] + 2 * dp[i - 2]
+1 -> 1
+2 -> 3
+3 -> 5
+4 -> 11
+5 -> 21
+
+=|
+|=
+|||
+ㅁ|
+|ㅁ
+
+ㅁㅁ
+ㅁ=
+=ㅁ
+ㅁ||
+|ㅁ|
+||ㅁ
+=||
+|=|
+||=
+==
+||||
+
+ㅁ=|
+=ㅁ|
+ㅁ|=
+=|ㅁ
+|=ㅁ
+|ㅁ=
+==|
+=|=
+|==
+=|||
+|=||
+||=|
+|||=
+|||||
+ㅁㅁ|
+ㅁ|ㅁ
+|ㅁㅁ
+ㅁ|||
+|ㅁ||
+||ㅁ|
+|||ㅁ
+ */
