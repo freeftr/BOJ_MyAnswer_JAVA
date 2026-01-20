9b062d21e0c318ca94b777331af56b82c5174bef
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-20 10:18:28 +0900

    [Gold III] Title: 크게 만들기, Time: 536 ms, Memory: 62900 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -1,34 +1,60 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        String s = br.readLine();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        String[] s = br.readLine().split("");
+
+        Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < N; i++) {
-            int now = s.charAt(i) - '0';
-            while (!stack.isEmpty() && K > 0 && stack.peekLast() < now) {
-                stack.removeLast();
+            int cur = Integer.parseInt(s[i]);
+
+            while (!stack.isEmpty() && K > 0 && stack.peek() < cur) {
+                stack.pop();
                 K--;
             }
-            stack.addLast(now);
+
+            stack.add(cur);
+        }
+        
+        while (K > 0) {
+            K--;
+            stack.pop();
         }
 
-        while (K-- > 0) {
-            stack.removeLast();
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
         }
 
-        for (int num : stack) {
-            System.out.print(num);
-        }
+        sb.reverse();
+
+        System.out.println(sb);
     }
 }
+
+/*
+조건
+- N자리 숫자에서 숫자 K개 지워서 얻을 수 있는 가장 큰수 구하기
+
+요구
+- 지워서 얻을 수 있는 가장 큰수 출력
+
+풀이
+- 스택?
+4   1   7   7   2   5   2   8   4   1
+
+4
+
+ */
