253df4d60725f6a41e6fcaefe140a3fec90d7f1b
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-20 11:02:44 +0900

    [Gold V] Title: 극장 좌석, Time: 104 ms, Memory: 14016 KB -BaekjoonHub

--- 백준/Gold/2302. 극장 좌석/극장 좌석.java
+++ 백준/Gold/2302. 극장 좌석/극장 좌석.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,33 +10,68 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> vips = new ArrayList<>();
+        ArrayList<Integer> ranges = new ArrayList<>();
+        int prev = 0;
+        int limit = 0;
+        int last = 0;
+
         for (int i = 0; i < M; i++) {
-            vips.add(Integer.parseInt(br.readLine()));
+            int vip = Integer.parseInt(br.readLine());
+            if (i == M - 1) last = vip;
+            ranges.add(vip - prev - 1);
+            prev = vip;
+            limit = Math.max(limit, ranges.get(i));
         }
 
-        Collections.sort(vips);
-
-        int[] dp = new int[N + 1];
+        int[] dp = new int[41];
         dp[0] = 1;
-        if (N >= 1) dp[1] = 1;
-        if (N >= 2) dp[2] = 2;
+        dp[1] = 1;
+        dp[2] = 2;
 
-        for (int i = 3; i <= N; i++) {
+        for (int i = 3; i <= 40; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        int answer = 1;
-        int prev = 1;
+        long answer = 1;
 
-        for (int vip : vips) {
-            int len = vip - prev;
-            answer *= dp[len];
-            prev = vip + 1;
+        for (int r : ranges) {
+            answer *= dp[r];
         }
 
-        answer *= dp[N - prev + 1];
+        answer = answer * dp[(N - last)];
 
         System.out.println(answer);
     }
 }
+
+/*
+조건
+- N개의 좌석
+- 자기 양옆으로 옮길 수 있음.
+- VIP는 고정
+
+요구
+- 앉는 경우의 수 구하기
+
+풀이
+- VIP는 고정시켜 놓고 구간들끼리 경우의 수 구하면 된다.
+- 구간들끼리 경우의 수는 dp로 구할 수 있음.
+
+dp[1] = 1
+
+dp[2] = 2
+12
+21
+
+dp[3] = 3
+123
+213
+132
+
+dp[4] = 5
+1234
+1243
+1324
+3124
+2143
+ */
