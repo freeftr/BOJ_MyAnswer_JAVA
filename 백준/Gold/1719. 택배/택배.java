313126d510b86bc45d1c0a6490d23cc9fbb929f8
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-21 10:27:49 +0900

    [Gold III] Title: 택배, Time: 376 ms, Memory: 31892 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -1,84 +1,91 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int n, m;
     static ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
-    static boolean[] visited;
-    static int[] dist;
-    static int[] path;
-    static int[][] ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        StringBuilder sb = new StringBuilder();
-        ans = new int[n+1][n+1];
+        int[][] result = new int[n + 1][n + 1];
 
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
+            result[i][i] = 0;
         }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            graph.get(a).add(new int[]{b, c});
-            graph.get(b).add(new int[]{a, c});
+            graph.get(a).add(new int[]{b, cost});
+            graph.get(b).add(new int[]{a, cost});
         }
 
         for (int i = 1; i <= n; i++) {
-            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-            pq.offer(new int[]{i, 0});
-            visited = new boolean[n+1];
-            dist = new int[n+1];
-            path = new int[n+1];
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+            int[] dist = new int[n + 1];
 
             Arrays.fill(dist, Integer.MAX_VALUE);
+
             dist[i] = 0;
+
+            pq.add(new int[]{i, 0});
 
             while (!pq.isEmpty()) {
                 int[] cur = pq.poll();
-                if(visited[cur[0]]) continue;
-                visited[cur[0]] = true;
+                int v = cur[0];
+                int d = cur[1];
+                if (dist[v] < d) continue;
 
-                for(int[] next : graph.get(cur[0])) {
-                    if(dist[next[0]] > dist[cur[0]] + next[1]) {
-                        path[next[0]] = cur[0];
-                        dist[next[0]] = dist[cur[0]] + next[1];
-                        pq.offer(new int[]{next[0], dist[next[0]]});
+                for (int[] n : graph.get(v)) {
+                    int nv = n[0];
+                    int nd = n[1];
+
+                    if (dist[nv] > dist[v] + nd) {
+                        dist[nv] = dist[v] + nd;
+                        result[nv][i] = v;
+                        pq.add(new int[]{nv, dist[nv]});
                     }
                 }
             }
+        }
 
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if(i == j){
-                    sb.append("- ");
-                } else {
-                    sb.append(find(j, i) + " ");
-                }
+                sb.append(result[i][j] == 0 ? "- " : result[i][j] + " ");
             }
             sb.append("\n");
         }
 
-        System.out.print(sb);
-    }
-
-    static int find(int v, int start) {
-        if(path[v] == start) {
-            return v;
-        }
-        if(path[v] == 0) {
-            return -1;
-        }
-        return find(path[v], start);
+        System.out.println(sb);
     }
 }
+
+/*
+조건
+- 노드 = 집하장
+- 경로표 = 한 집하장에서 다른 집하장으로 최단 경로로 갈시 가장 먼저 거쳐야 하는 집하장.
+
+요구
+- 경로표 구하기
+
+풀이
+1 -> 4
+
+1 -> 3 -> 4
+ */
