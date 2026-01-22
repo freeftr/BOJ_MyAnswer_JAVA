3bfa344b2a9899b8706c9a88fe76b2fdf7a13248
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-22 11:12:54 +0900

    [Gold V] Title: 입국심사, Time: 256 ms, Memory: 23436 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,43 +1,68 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
     static int N, M;
-    static long max = 0, result=0;
-    static int[] immi;
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw= new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        immi = new int[N];
-        for(int i = 0; i < N; i++){
-            immi[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max,immi[i]);
+
+        int[] times = new int[N];
+        int maxT = 0;
+
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(br.readLine());
+            if (times[i] > maxT) maxT = times[i];
         }
-        BinarySearch();
-    }
-    public static void BinarySearch(){
-        long low = 1;
-        long high = max*M;
-        while(low<=high){
-            long target = 0;
-            long mid = (low+high)/2;
-            for(long i : immi){
-                target+=mid/i;
-                if(target>=M)break;
-            }
-            if(target>=M){
+
+        long low = 0;
+        long high = (long) maxT * M;
+        long answer = 0;
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+
+            if (check(mid, times)) {
                 high = mid - 1;
-                if(result == 0) result = mid;
-                else if(result>mid){
-                    result = mid;
-                }
-            }
-            else{
-                low = mid+1;
+                answer = mid;
+            } else {
+                low = mid + 1;
             }
         }
-        System.out.println(result);
+
+        System.out.println(answer);
+    }
+
+    static boolean check(long mid, int[] times) {
+        long cnt = 0;
+        for (int t : times) {
+            cnt += mid / t;
+            if (cnt >= M) return true;
+        }
+        return cnt >= M;
     }
 }
+
+
+/*
+조건
+- M명, N개의 심사대
+- 항상 빈자리 안채워도 되고 더 빨리 끝나면 거기 가도댐.
+
+요구
+- 걸리는 시간 최솟값 구하기
+
+풀이
+- 매개변수 탐색
+- 걸리는 시간으로 기준으로 설정
+
+a(7)    b(10)
+7       10
+
+ */
