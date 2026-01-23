f03f8444b170a9358bad1fa6907bb6c5ada75612
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-23 10:52:23 +0900

    [Gold IV] Title: 테트로미노, Time: 576 ms, Memory: 33680 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,58 +1,89 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int N , M;
-    static int[][] paper;
-    static boolean[][] visited;
-    static int max = 0;
-    public static void main(String[] args) throws IOException{
+    static int N, M;
+    static int[][] map;
+    static int[][][] d = {
+            {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+            {{0, 0}, {1, 0}, {2, 0}, {2, 1}},
+            {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
+            {{0, 0}, {0, 1}, {0, 2}, {1, 1}},
+            {{0, 0}, {1, 0}, {2, 0}, {2, -1}},
+            {{0, 0}, {1, 0}, {2, -1}, {1, -1}}
+    };
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        paper = new int[N][M];
-        visited = new boolean[N][M];
+        map = new int[N][M];
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++){
-                paper[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                visited[i][j] = true;
-                dfs(i,j,1, paper[i][j]);
-                visited[i][j] = false;
+        int answer = 0;
+        for (int a = 0; a < 4; a++) {
+            answer = Math.max(answer, getMax());
+            turn();
+        }
+
+        System.out.println(answer);
+    }
+
+    static int getMax() {
+        int result = 0;
+
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; y < M; y++) {
+                for (int[][] shape : d) {
+                    int sum = 0;
+                    boolean ok = true;
+
+                    for (int[] pos : shape) {
+                        int nx = x + pos[0];
+                        int ny = y + pos[1];
+
+                        if (checkRange(nx, ny)) { ok = false; break; }
+                        sum += map[nx][ny];
+                    }
+
+                    if (ok) result = Math.max(result, sum);
+                }
             }
         }
-        System.out.println(max);
+
+        return result;
     }
-    //빠큐모양 탐색 불가
-    public static void dfs(int x, int y, int depth, int sum){
-        if(depth==4){
-            max = Math.max(max,sum);
-            return;
-        }
-        int[] dx = {1,-1,0,0};
-        int[] dy = {0,0,1,-1};
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx>=N || ny>=M || nx < 0 || ny < 0) continue;
-            if(visited[nx][ny])continue;
-            if(depth==2){
-                visited[nx][ny] = true;
-                dfs(x,y,depth+1, sum+paper[nx][ny]);
-                visited[nx][ny] = false;
+
+    static boolean checkRange(int x, int y) {
+        return x < 0 || y < 0 || x >= N || y >= M;
+    }
+
+    static void turn() {
+        int[][] temp = new int[M][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                temp[j][N - 1 - i] = map[i][j];
             }
-            visited[nx][ny] = true;
-            dfs(nx,ny,depth+1, sum+paper[nx][ny]);
-            visited[nx][ny] = false;
         }
+
+        map = temp;
+
+        int oldN = N;
+        N = M;
+        M = oldN;
     }
+
 }
