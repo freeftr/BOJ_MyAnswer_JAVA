1021e85ae9d2a5b4da0b3eb153832c93508b01cd
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-24 16:08:41 +0900

    [level 3] Title: 표 편집, Time: 228.74 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,133 +1,87 @@
 import java.util.*;
 
 class Solution {
-    
-    static Stack<Node> deleted = new Stack<>();
-    static boolean[] check;
-    static ArrayList<Node> nodes = new ArrayList<>();
-    
-    public String solution(int n, int k, String[] cmd) {
-        String answer = "";
-        
-        check = new boolean[n];
-        // 현재 위치 설정.
-        int cur = k;
-        
-        // 노드 초기화
-        for (int i = 0; i < n; i++) {
-            Node now = new Node(i, null, null);
-            nodes.add(now);
-            if (i != 0) {
-                now.prev = nodes.get(i - 1);
-                nodes.get(i - 1).next = now;
-            }
-        }
-        
-        for (String c : cmd) {
-            String type = c.split(" ")[0];
-            Node now = nodes.get(cur);
-            
-            if (type.equals("U")) {
-                int X = Integer.parseInt(c.split(" ")[1]);
-                cur = now.up(X);
-            }
-            
-            if (type.equals("D")) {
-                int X = Integer.parseInt(c.split(" ")[1]);
-                cur = now.down(X);
-            }
-            
-            if (type.equals("C")) {
-                cur = now.delete();
-            }
-            
-            if (type.equals("Z")) {
-                Node last = deleted.pop();
-                check[last.idx] = false;
-                last.restore();
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < n; i++) {
-            sb.append(check[nodes.get(i).idx] ? "X" : "O");
-        }
-        
-        return sb.toString();
-    }
-    
+
+    static boolean[] deleted;
+    static ArrayList<Node> nodes;
+    static Deque<Node> stack;
+
     static class Node {
         int idx;
         Node prev;
         Node next;
-        
-        Node (int idx, Node prev, Node next) {
+
+        Node(int idx) {
             this.idx = idx;
-            this.prev = prev;
-            this.next = next;
         }
-        
-        public int up (int X) {
-            if (X == 0) {
-                return this.idx;
-            }
-            
-            return this.prev.up(X - 1);
+
+        Node up(int x) {
+            Node cur = this;
+            while (x-- > 0 && cur.prev != null) cur = cur.prev;
+            return cur;
         }
-        
-        public int down (int X) {
-            if (X == 0) {
-                return this.idx;
-            }
-            
-            return this.next.down(X - 1);
+
+        Node down(int x) {
+            Node cur = this;
+            while (x-- > 0 && cur.next != null) cur = cur.next;
+            return cur;
         }
-        
-        public int delete () {
-            int idx = this.idx;
-            
-            if (this.next == null) {
-                idx = this.prev.idx;
-                this.prev.next = null;
-            } else if (this.prev == null) {
-                idx = this.next.idx;
-                this.next.prev = null;
-            } else {
-                idx = this.next.idx;
-                Node temp = this.prev;
-                this.prev.next = this.next;
-                this.next.prev = temp;
-            }
-            
-            deleted.add(this);
-            check[this.idx] = true;
-            return idx;
+
+        void delete() {
+            if (prev != null) prev.next = next;
+            if (next != null) next.prev = prev;
+            stack.push(this);
         }
-        
-        public void restore() {
-            if (this.next != null) {
-                this.next.prev = this;
-            }
-            
-            if (this.prev != null) {
-                this.prev.next = this;
-            }
+
+        void restore() {
+            if (prev != null) prev.next = this;
+            if (next != null) next.prev = this;
         }
     }
+
+    public String solution(int n, int k, String[] cmd) {
+        deleted = new boolean[n];
+        nodes = new ArrayList<>(n);
+        stack = new ArrayDeque<>();
+
+        // 노드 생성 + 리스트에 저장
+        for (int i = 0; i < n; i++) nodes.add(new Node(i));
+
+        // 양방향 연결
+        for (int i = 0; i < n; i++) {
+            if (i > 0) nodes.get(i).prev = nodes.get(i - 1);
+            if (i < n - 1) nodes.get(i).next = nodes.get(i + 1);
+        }
+
+        Node now = nodes.get(k);
+
+        for (String c : cmd) {
+            char type = c.charAt(0);
+
+            if (type == 'U') {
+                int x = Integer.parseInt(c.substring(2));
+                now = now.up(x);
+
+            } else if (type == 'D') {
+                int x = Integer.parseInt(c.substring(2));
+                now = now.down(x);
+
+            } else if (type == 'C') {
+                deleted[now.idx] = true;
+
+                Node nextNode = (now.next != null) ? now.next : now.prev; // 이동할 위치 먼저 확보
+                now.delete();
+                now = nextNode;
+
+            } else { // 'Z'
+                Node top = stack.pop();
+                deleted[top.idx] = false;
+                top.restore();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) sb.append(deleted[i] ? 'X' : 'O');
+        return sb.toString();
+    }
 }
-
-/*
-조건
-- 행을 선택, 삭제, 복구
-- U X: X 칸 위에 있는 행 선택
-- D X: X 칸 아래 있는 행 선택
-- C: 현재 행 삭제 후 아래 행 선택. (만약 마지막행인 경우 바로 윗 행 선택)
-- Z: 가장 마지막에 삭제된 행 복구, 선택 행은 안바뀜
-
-요구
-- 삭제된 행은 X 삭제되지 않은 행은 O로 표시해서 반환.
-
-풀이
-- 연결리스트
-*/
