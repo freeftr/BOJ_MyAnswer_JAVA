69053d4d368f46c138cb3e8fd9eeab83b90c8ddc
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-01-26 09:22:39 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.18 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,36 +1,37 @@
 import java.util.*;
 
 class Solution {
-    
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
     
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-        
         int idx = 0;
+        
         for (String[] place : places) {
-            boolean flag = true;
-            for (int i = 0; i < 5; i++) {
-                for (int j = 0; j < 5; j++) {
-                    if (place[i].charAt(j) == 'P') {
-                        if (!bfs(i, j, place)) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                }
-                if (!flag) break;
-            }
-            
-            answer[idx++] = flag ? 1 : 0;
+            int result = checkPlace(place);
+            answer[idx++] = result;
         }
+        
         return answer;
+    }
+    
+    static int checkPlace(String[] place) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (place[i].charAt(j) == 'P') {
+                    if (!bfs(i, j, place)) return 0;
+                }
+            }
+        }
+        
+        return 1;
     }
     
     static boolean bfs(int a, int b, String[] place) {
         Queue<int[]> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[5][5];
+        
         q.add(new int[]{a, b, 0});
         visited[a][b] = true;
         
@@ -38,44 +39,42 @@ class Solution {
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-            int dist = cur[2];
+            int d = cur[2];
             
-            // 거리 2까지만.
-            if (dist > 2) continue;
-            
-            // 맨해튼 거리 2이하일때 만나면 미준수.
-            // 파티션 끼는 경우는 어차피 2 초과.
-            if (place[x].charAt(y) == 'P' && dist != 0) {
-                return false;
-            }
+            if (d > 2) continue;
+            if (!(x == a && y == b) && place[x].charAt(y) == 'P') return false;
             
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
+                if (checkRange(nx, ny)) continue;
                 if (visited[nx][ny]) continue;
                 if (place[nx].charAt(ny) == 'X') continue;
                 
-                q.add(new int[]{nx, ny, dist + 1});
+                q.add(new int[]{nx, ny, d + 1});
                 visited[nx][ny] = true;
             }
         }
         
         return true;
     }
+    
+    static boolean checkRange(int x, int y) {
+        return x < 0 || y < 0 || x >= 5 || y >= 5;
+    }
 }
 
 /*
 조건
-- 5 * 5 맵
-- 맨해튼 거리 2 이하 금지
-- 사이 파티션 있을 경우 오케이.
-- o 빈테이블
-- x 파티션
-- P 응시자
+- 5 * 5 대기실
+- 맨해튼 거리가 2 이하가 되도록 앉으면 안된다.
+- 파티션은 허용. => 근데 어차피 파티션 있으면 거리가 2이상일 수 밖에 없음.
+- P 응시자 X 파티션
 
 요구
-- 지키면 1
-- 아니면 0
+- 가능한 배치인지 아닌지 확인
+
+풀이
+- bfs로 탐색
 */
