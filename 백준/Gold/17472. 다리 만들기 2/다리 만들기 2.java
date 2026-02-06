fe72b7e1fcda5cc42e19d30b6a8dfab18f783b98
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-02-06 11:29:19 +0900

    [Gold I] Title: 다리 만들기 2, Time: 116 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -1,142 +1,159 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int N, M;
+    static int[][] board;
+    static int[][] union;
+    static int[] parent;
+    static HashMap<Integer, ArrayList<int[]>> u = new HashMap<>();
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static PriorityQueue<Edge> edges = new PriorityQueue<>((a, b) -> a.c - b.c);
 
-    static int N, M, island_idx = 0;
-    static int[][] map;
-    static boolean[][] union;
-    static int[] parents;
-    static int[] dx = {0,0,1,-1}, dy= {1,-1,0,0};
-    static ArrayList<Edges> list = new ArrayList<>();
-    public static class Edges implements Comparable<Edges>{
-        int from;
-        int to;
-        int cost;
-        Edges(int from, int to, int cost){
-            this.from = from;
-            this.to = to;
-            this.cost = cost;
-        }
+    static class Edge{
+        int u;
+        int v;
+        int c;
 
-        @Override
-        public int compareTo(Edges o){
-            return this.cost - o.cost;
+        Edge(int u, int v, int c) {
+            this.u = u;
+            this.v = v;
+            this.c = c;
         }
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        union = new boolean[N][M];
-        for(int i = 0; i < N; i++){
+
+        board = new int[N][M];
+        union = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        //  섬을 구성하고 인덱스 주기
-        //  다리 놓고 섬간 최소 거리 구하기(다리길이는 2이상)
-        //  크루스칼 알고리즘으로 최소 거리 구하기
-
-        // 섬 인덱싱
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(map[i][j]==1 && !union[i][j]){
-                    island_idx++;
-                    island_idxing(i,j);
-                }
-            }
-        }
-        //다리 만들기
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(map[i][j]!=0){
-                    Make_bridge(i,j);
-                }
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        //쿠르스칼 알고리즘
-        Collections.sort(list);
-        parents = new int[island_idx+1];
-        for(int i = 1; i <= island_idx; i++){
-            parents[i] = i;
+        int max = makeUnion();
+
+        parent = new int[max + 1];
+
+        for (int i = 0; i <= max; i++) {
+            parent[i] = i;
         }
-        int sum = 0;
+
+        makeBridge();
+
         int cnt = 0;
-        for(Edges edge: list){
-            int from = edge.from;
-            int to = edge.to;
-            int cost = edge.cost;
-            if(find(to)!=find(from)) {
+        int sum = 0;
+        while (!edges.isEmpty()) {
+            Edge e = edges.poll();
+
+            if (find(e.v) != find(e.u)) {
+                union(e.v, e.u);
                 cnt++;
-                union(to, from);
-                sum+=cost;
+                sum += e.c;
             }
         }
-        if(cnt!=island_idx-1){
-            System.out.println(-1);
-            System.exit(0);
-        }
-        System.out.println(sum);
+
+        if (cnt != max - 1) System.out.println(-1);
+        else System.out.println(sum);
 
     }
-    // 섬 인덱스 주기
-    public static void island_idxing(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x,y});
-        map[x][y] = island_idx;
-        union[x][y] = true;
-        while(!q.isEmpty()) {
-            int[] arr = q.poll();
-            int X = arr[0];
-            int Y = arr[1];
-            for (int i = 0; i < 4; i++) {
-                int nx = X + dx[i];
-                int ny = Y + dy[i];
-                if(nx<0 || ny<0 || nx>=N || ny>=M)continue;
-                if(union[nx][ny])continue;
-                if(map[nx][ny]!=0){
-                    map[nx][ny] = island_idx;
-                    union[nx][ny] = true;
-                    q.add(new int[]{nx,ny});
+
+    static void makeBridge() {
+        for (int key : u.keySet()) {
+            for (int[] p : u.get(key)) {
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = p[0];
+                    int ny = p[1];
+                    int len = 0;
+                    int target = -1;
+
+                    while (true) {
+                        nx += dx[d];
+                        ny += dy[d];
+                        len++;
+
+                        if (checkRange(nx, ny)) break;
+                        if (union[nx][ny] == key) break;
+
+                        if (union[nx][ny] != 0) {
+                            target = union[nx][ny];
+                            break;
+                        }
+                    }
+
+                    len--;
+
+                    if (len >= 2 && target != -1) {
+                        edges.add(new Edge(key, target, len));
+                    }
                 }
             }
         }
     }
-    //다리 만들고 최소거리 구해서 Edges에 추가
-    public static void Make_bridge(int x, int y){
-        for(int i =0 ; i < 4; i++){
-            int nx = x;
-            int ny = y;
-            int dist = 0;
-            while(true){
-                dist++;
-                nx += dx[i];
-                ny += dy[i];
-                if(nx<0 || ny<0 || nx>=N || ny>=M)break;
-                if(map[nx][ny]==map[x][y])break;
-                if(map[nx][ny]!=0 && dist<=2)break;
-                if(map[nx][ny]!=0 && map[nx][ny]!=map[x][y] && dist>2){
-                    list.add(new Edges(map[x][y],map[nx][ny],dist-1));
-                    break;
-                }
-            }
-        }
+
+
+    static int find(int v) {
+        if (v == parent[v]) return v;
+        return parent[v] = find(parent[v]);
     }
-    public static int find(int v){
-        if(parents[v]==v)return v;
-        return parents[v] = find(parents[v]);
-    }
-    public static void union(int a, int b){
+
+    static void union(int a, int b) {
         a = find(a);
         b = find(b);
-        if(a<b)parents[b] = a;
-        else parents[a] = b;
+
+        parent[a] = b;
+    }
+
+    static int makeUnion() {
+        int idx = 1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (union[i][j] != 0) continue;
+                if (board[i][j] != 1) continue;
+
+                Queue<int[]> q = new ArrayDeque<>();
+                q.add(new int[]{i, j});
+
+                while (!q.isEmpty()) {
+                    int[] cur = q.poll();
+                    int x = cur[0];
+                    int y = cur[1];
+                    union[x][y] = idx;
+                    u.computeIfAbsent(idx, k -> new ArrayList<>()).add(new int[]{x, y});
+
+                    for (int k = 0; k < 4; k++) {
+                        int nx = x + dx[k];
+                        int ny = y + dy[k];
+
+                        if (checkRange(nx, ny)) continue;
+                        if (board[nx][ny] != 1) continue;
+                        if (union[nx][ny] == idx) continue;
+
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+
+                idx++;
+            }
+        }
+
+        return idx - 1;
+    }
+
+    static boolean checkRange(int nx, int ny) {
+        return nx < 0 || ny < 0 || nx >= N || ny >= M;
     }
 }
