3b745f45e70dd660e9be103f83e1b8a828d223e7
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-02-13 11:02:20 +0900

    [Gold IV] Title: 전화번호 목록, Time: 504 ms, Memory: 35392 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,35 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int t = Integer.parseInt(br.readLine());
 
         for (int tc = 0; tc < t; tc++) {
             int n = Integer.parseInt(br.readLine());
-
-            String[] arr = new String[n];
+            ArrayList<String> numbers = new ArrayList<>();
 
             for (int i = 0; i < n; i++) {
-                arr[i] = br.readLine();
+                String s = br.readLine();
+                numbers.add(s);
             }
 
-            Arrays.sort(arr);
-            
-            String ans = "YES";
+            Collections.sort(numbers);
+            boolean found = false;
 
             for (int i = 0; i < n - 1; i++) {
-                if (arr[i + 1].startsWith(arr[i])) {
-                    ans = "NO";
-                    continue;
+                if (numbers.get(i + 1).startsWith(numbers.get(i))) {
+                    found = true;
+                    break;
                 }
             }
 
-            System.out.println(ans);
+            sb.append(found ? "NO" : "YES").append("\n");
         }
+
+        System.out.println(sb);
     }
 }
+
+/*
+조건
+- 전화번호 목록
+- 한 번호가 다른 번호의 접두어가 되면 안된다.
+
+요구
+- 일관성이 있는지 없는지
+
+풀이
+-
+ */
