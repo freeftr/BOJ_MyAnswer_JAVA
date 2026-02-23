f263330e94810c946451ff718dc44c3e20bd6bf4
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-02-23 10:40:09 +0900

    [Gold II] Title: 회의준비, Time: 160 ms, Memory: 16376 KB -BaekjoonHub

--- 백준/Gold/2610. 회의준비/회의준비.java
+++ 백준/Gold/2610. 회의준비/회의준비.java
@@ -7,21 +7,24 @@ public class Main {
 
     static int N, M;
     static int[] parent;
-    static int[][] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
 
         parent = new int[N + 1];
-        dist = new int[N + 1][N + 1];
 
         for (int i = 0; i < N + 1; i++) {
             parent[i] = i;
+        }
+
+        int[][] dist = new int[N + 1][N + 1];
+
+        for (int i = 0; i < N + 1; i++) {
             Arrays.fill(dist[i], Integer.MAX_VALUE);
             dist[i][i] = 0;
         }
@@ -31,7 +34,7 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if (find(a) != find(b)) union(a, b);
+            union(a, b);
             dist[a][b] = 1;
             dist[b][a] = 1;
         }
@@ -39,60 +42,51 @@ public class Main {
         for (int k = 1; k < N + 1; k++) {
             for (int i = 1; i < N + 1; i++) {
                 for (int j = 1; j < N + 1; j++) {
-                    if (dist[i][k] == Integer.MAX_VALUE) continue;
-                    if (dist[k][j] == Integer.MAX_VALUE) continue;
+                    if (dist[i][k] == Integer.MAX_VALUE || dist[k][j] == Integer.MAX_VALUE) continue;
 
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    dist[i][j] = Math.min(dist[i][k] + dist[k][j], dist[i][j]);
                 }
             }
         }
 
-        HashSet<Integer> groups = new HashSet<>();
-        ArrayList<Integer> presidents = new ArrayList<>();
+        HashMap<Integer, ArrayList<Integer>> teams = new HashMap<>();
 
-        for (int i = 1; i < N + 1; i++) {
-            parent[i] = find(parent[i]);
-            groups.add(parent[i]);
+        for (int i = 1; i <= N; i++) {
+            teams.computeIfAbsent(find(i), k -> new ArrayList<>()).add(i);
         }
 
-        int K = groups.size();
+        ArrayList<Integer> result = new ArrayList<>();
 
-        sb.append(K).append("\n");
+        for (int key : teams.keySet()) {
+            ArrayList<Integer> team = teams.get(key);
 
-        for (int group : groups) {
-            ArrayList<Integer> members = new ArrayList<>();
+            int min = N * N;
+            int best = -1;
+            for (int leader : team) {
+                int longest = 0;
 
-            for (int i = 1; i < N + 1; i++) {
-                if (group == parent[i])  members.add(i);
-            }
-
-            int president = -1;
-            int best = Integer.MAX_VALUE;
-
-            for (int a : members) {
-                int max = 0;
-                for (int b : members) {
-                    if (dist[a][b] == Integer.MAX_VALUE) continue;
-
-                    max = Math.max(max, dist[a][b]);
+                for (int follower : team) {
+                    longest = Math.max(dist[follower][leader], longest);
                 }
 
-                if (best >= max) {
-                    best = max;
-                    president = a;
+                if (longest < min) {
+                    min = longest;
+                    best = leader;
                 }
             }
 
-            presidents.add(president);
+            result.add(best);
         }
 
-        Collections.sort(presidents);
+        Collections.sort(result);
 
-        for (int president : presidents) {
-            sb.append(president).append("\n");
+        sb.append(teams.size()).append("\n");
+
+        for (int r : result) {
+            sb.append(r).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 
     static int find(int v) {
@@ -104,31 +98,27 @@ public class Main {
         a = find(a);
         b = find(b);
 
-        parent[a] = b;
+        if (a > b) {
+            parent[a] = b;
+        } else {
+            parent[b] = a;
+        }
     }
 }
 
 /*
 조건
-- 회의 개최한다.
-- 위원회 구성해야 함.
-- 서로 알고 있으면 같은 위원회.
-- 위원회의 수는 최대가 되게.
-- 위원회 별 대표 뽑아야 함.
-- 의사전달시간: 위원회내에서 대표한테 의사를 전달하는데 거치는 사람 수.
-- 의사전달시간의 최댓값이 가장 낮게 대표 정해야 함.
+- 위원회를 구성
+    - 서로 알고 있으면 같은 위원회
+    - 위원회의 수는 최대가 되게
+- 각 위원회 별 대표를 뽑음
+- 참석자는 자신이 알고 있는 사람에게만 의견 전달 가능
+- 의사전달시간: 대표에게 의견을 전달하는 가장 적은 사람 거치는 수
 
-8
-7
-1 2
-2 3
-4 5
-5 6
-4 6
-6 7
-7 4
+요구
+- 위원회 별로 모든 참석자들의 의사전달 시간 중 최댓값이 최소가 되게
 
-1 2 3
-4 5 6 7
-8
+풀이
+- 플로이드 워셜로 구성원간 의사 전달 시간 구하기
+- 대표는 분리 집합으로 찾기
  */
