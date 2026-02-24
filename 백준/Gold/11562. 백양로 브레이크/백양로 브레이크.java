5031fce7ccfb3012a9dd81b78bc7b94830592812
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-02-24 12:54:48 +0900

    [Gold III] Title: 백양로 브레이크, Time: 604 ms, Memory: 36008 KB -BaekjoonHub

--- 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.java
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int n, m;
-    static final int INF = 1_000_000_000;
-
+    static int[][] dist;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,9 +14,10 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        int[][] dist = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
+        dist = new int[n + 1][n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
             dist[i][i] = 0;
         }
 
@@ -28,31 +27,52 @@ public class Main {
             int v = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            dist[u][v] = Math.min(dist[u][v], 0);
-            if (b == 1) {
-                dist[v][u] = Math.min(dist[v][u], 0);
+            if (b == 0) {
+                dist[u][v] = 0;
+                dist[v][u] = 1;
             } else {
-                dist[v][u] = Math.min(dist[v][u], 1);
+                dist[u][v] = 0;
+                dist[v][u] = 0;
             }
         }
 
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                if (dist[i][k] == INF) continue;
-                for (int j = 1; j <= n; j++) {
-                    if (dist[k][j] == INF) continue;
-                    int cand = dist[i][k] + dist[k][j];
-                    if (cand < dist[i][j]) dist[i][j] = cand;
+        for (int k = 1; k < n + 1; k++) {
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    if (dist[i][k] == Integer.MAX_VALUE || dist[k][j] == Integer.MAX_VALUE) continue;
+                    dist[i][j] = Math.min(dist[i][k] + dist[k][j], dist[i][j]);
                 }
             }
         }
 
-        int q = Integer.parseInt(br.readLine());
-        for (int i = 0; i < q; i++) {
+//        for (int i = 1; i < n + 1; i++) {
+//            System.out.println();
+//            for (int j = 1; j < n + 1; j++) {
+//                System.out.printf("%2d", dist[i][j] == Integer.MAX_VALUE ? -1 : dist[i][j]);
+//            }
+//        }
+//        System.out.println();
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
+
             System.out.println(dist[s][e]);
         }
     }
 }
+
+/*
+조건
+- 목적지까지 가는 길이 없음
+- 일방통행인 길을 양방향으로 바꿔야 하는 길을 구해야 함.
+
+요구
+- 목적지까지 가려면 최소 몇개의 길을 양방향으로 바꿔야 하는지
+
+풀이
+-
+ */
