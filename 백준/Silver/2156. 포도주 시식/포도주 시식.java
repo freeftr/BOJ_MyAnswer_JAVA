72e44ef651e5f969690dad6f8cc96f20c985c4a0
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-02-24 12:37:55 +0900

    [Silver I] Title: 포도주 시식, Time: 120 ms, Memory: 14912 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,32 +1,37 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N + 1];
-        int[] dp = new int[N + 1]; // i 번째 포도주를 마실 때 최대 값
+        int n = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        int[] cups = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            cups[i] = Integer.parseInt(br.readLine());
         }
 
-        if (N == 1) {
-            System.out.println(arr[1]);
+        if (n == 1) {
+            System.out.println(cups[1]);
             return;
         }
 
-        dp[1] = arr[1];
-        dp[2] = arr[1] + arr[2];
+        int[] dp = new int[n + 1];
 
-        for (int i = 3; i <= N; i++) {
-            // 현재 거 마시고 그전 거 마시고 i-2는 건너 뛰기 vs 현재거 전에 거만 안마시기
-            dp[i] = Math.max(dp[i - 3] + arr[i] + arr[i - 1], dp[i - 2] + arr[i]);
-            // 현재 거 안마시기 마시기
-            dp[i] = Math.max(dp[i], dp[i - 1]);
+        dp[1] = cups[1];
+        dp[2] = cups[1] + cups[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1],
+                    Math.max(
+                            cups[i] + dp[i - 2],
+                            cups[i] + cups[i - 1] + dp[i - 3]
+                    )
+            );
         }
 
-        System.out.println(dp[N]);
+        System.out.println(dp[n]);
     }
 }
