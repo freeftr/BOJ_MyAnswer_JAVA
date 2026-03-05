f422eb6d563596698c682562ba1d926d0d885026
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-05 13:16:29 +0900

    [Gold IV] Title: 좋은 친구, Time: 264 ms, Memory: 38040 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.java
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.java
@@ -1,34 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] nm = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(nm[0]);
-        int K = Integer.parseInt(nm[1]);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        Map<Integer, ArrayDeque<Integer>> map = new HashMap<>();
-        long answer = 0;
+        String[] friends = new String[N];
 
         for (int i = 0; i < N; i++) {
-            int len = br.readLine().length();
-            ArrayDeque<Integer> q = map.computeIfAbsent(len, k -> new ArrayDeque<>());
+            friends[i] = br.readLine();
+        }
 
-            while (!q.isEmpty() && i - q.peekFirst() > K) {
-                q.pollFirst();
+        int left = 0;
+        int right = K;
+        long answer = 0;
+        int[] cnt = new int[300001];
+
+        for (int i = 0; i <= right; i++) {
+            cnt[friends[i].length()]++;
+        }
+
+        answer += cnt[friends[0].length()] - 1;
+
+        while (left < N - 1) {
+            cnt[friends[left].length()]--;
+            left++;
+            right++;
+            if (right < N) {
+                cnt[friends[right].length()]++;
             }
-
-            answer += q.size();
-
-            q.addLast(i);
+            answer += cnt[friends[left].length()] - 1;
         }
 
         System.out.println(answer);
     }
 }
+
+/*
+조건
+- 반 등수와 친구
+- 자신과 반 등수의 차이가 K를 넘으면 친구가 아님
+- 좋은 친구는 이름의 길이가 같아야 함.
+ */
