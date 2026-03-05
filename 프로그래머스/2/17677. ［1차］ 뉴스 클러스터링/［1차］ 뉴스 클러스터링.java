286c53cf303601018024ce62451fd82dae55e113
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-05 09:25:49 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 23.38 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,69 +3,57 @@ import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        ArrayList<String> list1 = new ArrayList<>();
-        ArrayList<String> list2 = new ArrayList<>();
-        ArrayList<String> same = new ArrayList<>();
-        ArrayList<String> all = new ArrayList<>();
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        
+        ArrayList<String> arr1 = new ArrayList<>();
+        ArrayList<String> arr2 = new ArrayList<>();
         
         for (int i = 0; i < str1.length() - 1; i++) {
-            String s = str1.substring(i, i + 2).toLowerCase();
-            if ('a' <= s.charAt(0) && s.charAt(0) <= 'z' && 'a' <= s.charAt(1) && s.charAt(1) <= 'z') {
-                list1.add(s);
+            char a = str1.charAt(i);
+            char b = str1.charAt(i + 1);
+            
+            if (Character.isLetter(a) && Character.isLetter(b)) {
+                arr1.add("" + a + b);
             }
         }
         
         for (int i = 0; i < str2.length() - 1; i++) {
-            String s = str2.substring(i, i + 2).toLowerCase();
-            if ('a' <= s.charAt(0) && s.charAt(0) <= 'z' && 'a' <= s.charAt(1) && s.charAt(1) <= 'z') {
-                list2.add(s);
+            char a = str2.charAt(i);
+            char b = str2.charAt(i + 1);
+            
+            if (Character.isLetter(a) && Character.isLetter(b)) {
+                arr2.add("" + a + b);
             }
         }
         
-        if (list1.isEmpty() && list2.isEmpty()) {
-            return 65536;
-        }
-
-        Collections.sort(list1);
-        Collections.sort(list2);
+        boolean[] v1 = new boolean[arr1.size()];
+        boolean[] v2 = new boolean[arr2.size()];
         
-        int one = 0;
-        int two = 0;
+        int cnt = 0;
         
-        int gyo = 0;
-        
-        int i = 0, j = 0;
-        while (i < list1.size() && j < list2.size()) {
-            int cmp = list1.get(i).compareTo(list2.get(j));
-            if (cmp == 0) {
-                gyo++;
-                i++;
-                j++;
-            } else if (cmp < 0) {
-                i++;
-            } else {
-                j++;
+        for (int i = 0; i < arr1.size(); i++) {
+            if (v1[i]) continue;
+            
+            for (int j = 0; j < arr2.size(); j++) {
+                if (v2[j]) continue;
+                
+                if (arr1.get(i).equals(arr2.get(j))) {
+                    v1[i] = true;
+                    v2[j] = true;
+                    cnt++;
+                    break;
+                }
             }
         }
         
-        int hap = list1.size() + list2.size() - gyo;
-    
-        if (hap == 0) {
-            return 65536;
-        }
+        int total = arr1.size() + arr2.size();
+        int hap = total - cnt;
         
-        answer = (int) gyo * 65536 / hap;
+        if (hap == 0) return 65536;
+        
+        answer = (cnt * 65536) / hap;
+        
         return answer;
     }
 }
-
-/*
-자카드 유사도 = 교집합 크기 / 합집합 크기
-둘다 공집합 이면 1
-
-1 1 2 2 3
-1 2 2 4 5
-1 1 2 3 4 5
-aa aa
-aa aa aa
-*/
