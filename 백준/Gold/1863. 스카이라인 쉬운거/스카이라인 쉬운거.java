05da35426394badc72ffb06c6877c9a0e65d4e72
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-06 16:13:33 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 188 ms, Memory: 19784 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            st.nextToken();
+            int y = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty() && stack.peek() > y) {
+                stack.pop();
+                answer++;
+            }
+
+            if (stack.isEmpty() || stack.peek() < y) {
+                if (y != 0) stack.push(y);
+            }
+        }
+
+        System.out.println(answer + stack.size());
+    }
+}
