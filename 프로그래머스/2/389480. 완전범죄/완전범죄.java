82ed73e8a23cf1b4abdbeb75acc3498eb04a5cda
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-09 12:19:14 +0900

    [level 2] Title: 완전범죄, Time: 0.44 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -2,47 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] info, int n, int m) {
-        int INF = 100000;
-        int[][] dp = new int[n][m]; 
+
+        int N = info.length;
+        int INF = 1_000_000_000;
+
+        int[][] dp = new int[N][m];
         for (int[] row : dp) Arrays.fill(row, INF);
-        dp[0][0] = 0;
 
-        for (int[] item : info) {
-            int aTrace = item[0];
-            int bTrace = item[1];
-            int[][] next = new int[n][m];
-            for (int[] row : next) Arrays.fill(row, INF);
+        dp[0][0] = info[0][0];
+        if (info[0][1] < m) dp[0][info[0][1]] = 0;
 
-            for (int a = 0; a < n; a++) {
-                for (int b = 0; b < m; b++) {
-                    if (dp[a][b] == INF) continue;
-
-                    int nextA = a + aTrace;
-                    int nextB = b;
-                    if (nextA < n) {
-                        next[nextA][nextB] = Math.min(next[nextA][nextB], dp[a][b] + aTrace);
-                    }
-
-                    nextA = a;
-                    nextB = b + bTrace;
-                    if (nextB < m) {
-                        next[nextA][nextB] = Math.min(next[nextA][nextB], dp[a][b]);
-                    }
-                }
-            }
-
-            dp = next;
-        }
-
-        int minTrace = INF;
-        for (int a = 0; a < n; a++) {
+        for (int i = 1; i < N; i++) {
             for (int b = 0; b < m; b++) {
-                if (dp[a][b] != INF) {
-                    minTrace = Math.min(minTrace, dp[a][b]);
+
+                if (dp[i - 1][b] == INF) continue;
+
+                // A가 훔침
+                dp[i][b] = Math.min(dp[i][b], dp[i - 1][b] + info[i][0]);
+
+                // B가 훔침
+                int nb = b + info[i][1];
+                if (nb < m) {
+                    dp[i][nb] = Math.min(dp[i][nb], dp[i - 1][b]);
                 }
             }
         }
 
-        return minTrace == INF ? -1 : minTrace;
+        int ans = INF;
+
+        for (int b = 0; b < m; b++) {
+            ans = Math.min(ans, dp[N - 1][b]);
+        }
+
+        if (ans >= n) return -1;
+
+        return ans;
     }
 }
