f0f9f68396c037287431701299673e417a95bbd0
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-10 09:50:54 +0900

    [level 2] Title: 비밀 코드 해독, Time: 60.23 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,17 +1,15 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
+    
     public int solution(int n, int[][] q, int[] ans) {
         int answer = 0;
         
-        //11 12 13 14 15
-        for (int a = 1; a <= n - 4; a++){
-            for (int b = a + 1; b <= n - 3; b++){
-                for (int c = b + 1; c <= n - 2; c++){
-                    for (int d = c + 1; d <= n - 1; d++){
-                        for (int e = d + 1; e <= n; e++){
-                            
+        for (int a = 1; a <= n - 4; a++) {
+            for (int b = a + 1; b <= n - 3; b++) {
+                for (int c = b + 1; c <= n - 2; c++) {
+                    for (int d = c + 1; d <= n - 1; d++) {
+                        for (int e = d + 1; e <= n; e++) {
                             HashSet<Integer> set = new HashSet<>();
                             set.add(a);
                             set.add(b);
@@ -19,27 +17,24 @@ class Solution {
                             set.add(d);
                             set.add(e);
                             
-                            boolean flag = true;
-                            
+                            boolean flag = false;
                             int idx = 0;
-                            for (int[] candidate : q){
+                            for (int[] Q : q) {
                                 int cnt = 0;
                                 
-                                for (int i = 0; i < 5; i++){
-                                    if (set.contains(candidate[i])){
-                                        cnt++;
-                                    }
+                                for (int i : Q) {
+                                    if (set.contains(i)) cnt++;
                                 }
                                 
-                                if (cnt!=ans[idx]){
-                                    flag = false;
+                                if (ans[idx] != cnt) {
+                                    flag = true;
                                     break;
                                 }
                                 idx++;
                             }
                             
-                            if (flag){
-                                    answer++;
+                            if (!flag) {
+                                answer++;
                             }
                         }
                     }
@@ -49,3 +44,12 @@ class Solution {
         return answer;
     }
 }
+
+/*
+조건
+- 서로 다른 정수 5개 오름차순
+- m번의 시도 -> 몇개가 비밀코드에 있는지 알려줌
+
+요구
+- 비밀코드로 가능한 조합 개수 구하기
+*/
