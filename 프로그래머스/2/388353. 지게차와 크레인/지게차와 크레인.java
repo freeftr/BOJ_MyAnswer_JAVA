0c527474b8266f7a78873e1b18ebef952d525086
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-10 09:41:56 +0900

    [level 2] Title: 지게차와 크레인, Time: 29.30 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,103 +1,86 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    
-    static int N, M;
-    static char[][] newStorage;
+    static char[][] board;
+    static int n, m;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
     
     public int solution(String[] storage, String[] requests) {
         int answer = 0;
-        N = storage.length;
-        M = storage[0].length();
-        newStorage = new char[N + 2][M + 2];
+        n = storage.length;
+        m = storage[0].length();
         
-        for (int i = 0; i < N + 2; i++) {
-            for (int j = 0; j < M + 2; j++) {
-                if (i == 0 || i == N + 1 || j == 0 || j == M + 1) {
-                    newStorage[i][j] = '0'; 
-                } else {
-                    newStorage[i][j] = storage[i - 1].charAt(j - 1); 
-                }
+        board = new char[n + 2][m + 2];
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                board[i][j] = storage[i - 1].charAt(j - 1);
             }
         }
         
-        for (String s : requests) {
-            // print();
-            if (s.length() == 1) {
-                fork(s.charAt(0)); 
+        for (String req : requests) {
+            if (req.length() == 1) {
+                char target = req.charAt(0);
+                Queue<int[]> q = new ArrayDeque<>();
+                boolean[][] visited = new boolean[n + 2][m + 2];
+                
+                for (int i = 0; i <= n + 1; i++) {
+                    q.add(new int[]{i, 0});
+                    q.add(new int[]{i, m + 1});
+                    visited[i][0] = true;
+                    visited[i][m + 1] = true;
+                }
+                
+                for (int j = 0; j <= m + 1; j++) {
+                    q.add(new int[]{0, j});
+                    q.add(new int[]{n + 1, j});
+                    visited[0][j] = true;
+                    visited[n + 1][j] = true;
+                }
+                
+                while (!q.isEmpty()) {
+                    int[] cur = q.poll();
+                    int x = cur[0];
+                    int y = cur[1];
+                    
+                    for (int i = 0; i < 4; i++) {
+                        int nx = x + dx[i];
+                        int ny = y + dy[i];
+                        
+                        if (nx < 0 || nx >= n + 2 || ny < 0 || ny >= m + 2) continue;
+                        if (visited[nx][ny]) continue;
+                        
+                        if (board[nx][ny] == '\0') {
+                            q.add(new int[]{nx, ny});
+                            visited[nx][ny] = true;
+                        }
+                        
+                        if (board[nx][ny] == target) {
+                            board[nx][ny] = '\0';
+                            visited[nx][ny] = true;
+                        }
+                    }
+                }
             } else {
-                crane(s.charAt(0));
+                char target = req.charAt(0);
+                
+                for (int i = 1; i <= n; i++) {
+                    for (int j = 1; j <= m; j++) {
+                        if (board[i][j] == target) board[i][j] = '\0';
+                    }
+                }
             }
-            // System.out.println();
         }
         
-        answer = count();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (board[i][j] != '\0') {
+                    answer++;
+                }
+            }
+        }
+        
         return answer;
-    }
-    
-    static void fork(char target) {
-        Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N + 2][M + 2];
-        
-        q.add(new int[]{0, 0});
-        visited[0][0] = true;
-        
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            
-            if (newStorage[x][y] == target) { 
-                newStorage[x][y] = '0';
-                continue;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if (nx < 0 || ny < 0 || nx >= N + 2 || ny >= M + 2) continue;
-                if (visited[nx][ny]) continue;
-                
-                if (newStorage[nx][ny] == '0' || newStorage[nx][ny] == target) {
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny});
-                }
-            }
-        }
-    }    
-    
-    static void crane(char target) {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (target==newStorage[i][j]) { 
-                    newStorage[i][j] = '0';
-                }
-            }
-        }
-    }
-    
-    static int count() {
-        int cnt = 0;
-        for (char[] a : newStorage) {
-            for (char b : a) {
-                if (b != '0') { 
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-    
-    static void print() {
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                System.out.print(newStorage[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 }
