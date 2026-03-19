f90b1a7d2b71e1883c54405bbc58ef50c71d6046
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-19 10:45:48 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 444 ms, Memory: 36624 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,58 +1,71 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int[][] map;
-    static int[][] dist;
-
-    static int[] dx = {-1, 1, 0, 0};
+    
+    static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-
-    static int ans = 0;
-
+    static int n;
+    static int[][] board;
+    static int[][] dp;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+        
         n = Integer.parseInt(br.readLine());
-
-        map = new int[n][n];
-        dist = new int[n][n];
+        dp = new int[n][n];
+        board = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+        int answer = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                ans = Math.max(ans, dfs(i, j));
+                answer = Math.max(answer, dfs(i, j));
             }
         }
 
-        System.out.println(ans);
+        System.out.println(answer);
     }
+    
+    static int dfs(int x, int y) {
+        if (dp[x][y] != 0) return dp[x][y];
 
-    public static int dfs(int i, int j) {
-        if (dist[i][j] != 0) {
-            return dist[i][j];
+        dp[x][y] = 1;
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+            if (board[nx][ny] <= board[x][y]) continue;
+
+            dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
         }
 
-        dist[i][j] = 1;
-
-        for (int k = 0; k < 4; k++) {
-            int x = i + dx[k];
-            int y = j + dy[k];
-
-            if (x < 0 || x >= n || y < 0 || y >= n) continue;
-
-            if (map[x][y] > map[i][j]) {
-                dist[i][j] = Math.max(dist[i][j], dfs(x, y) + 1);
-            }
-        }
-        return dist[i][j];
+        return dp[x][y];
     }
 }
+
+/*
+조건
+- n * n
+- 이동한 지역에 대나무가 더 많아야 함.
+
+요구
+- 판다가 최대로 이동할 수 있는 경로의 칸
+
+풀이
+- dp
+- top - down
+- x, y에서 dfs -> 낮은 곳으로 이동
+ */
