fd2d1aac3d98ee8a65b51d97d594b9154f298674
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-19 10:26:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53784 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -11,29 +11,57 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N][2];
+        int[][] arr = new int[N + 1][2];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = W;
+            arr[i][1] = V;
         }
 
         int[][] dp = new int[N + 1][K + 1];
 
         for (int i = 1; i <= N; i++) {
-            int w = arr[i - 1][0];
-            int v = arr[i - 1][1];
-
+            int w = arr[i][0];
+            int v = arr[i][1];
             for (int j = 0; j <= K; j++) {
-                dp[i][j] = dp[i - 1][j];
-
-                if (j >= w) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - w] + v);
+                if (j - w < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                    continue;
                 }
+                dp[i][j] = Math.max(dp[i - 1][j - w] + v, dp[i - 1][j]);
             }
         }
 
         System.out.println(dp[N][K]);
     }
 }
+
+
+/*
+조건
+- N개의 물건
+- 무게 W, 가치 V
+
+요구
+- 무게 K를 넘지 않으면서 최대 가치
+
+풀이
+- 배낭
+무게 가치
+6 13
+4 8
+3 6
+5 12
+세로축 물건의 개수, 가로축 무게 -> 최대 가치
+
+0   1   2   3   4   5   6   7
+0   0   0   0   0   0   0   0
+1   0   0   0   0   0   13  13
+2   0   0   0   8   8   13  13
+3   0   0   6   8   8   13  14
+4   0   0   6   8   12  13  14
+ */
