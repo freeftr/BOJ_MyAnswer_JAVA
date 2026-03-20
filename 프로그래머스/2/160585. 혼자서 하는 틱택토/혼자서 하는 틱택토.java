50cb751e2f71d7a991f641d5e8ae764e8feb3080
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-20 15:19:01 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.06 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,35 +1,64 @@
 class Solution {
     public int solution(String[] board) {
         int o = 0, x = 0;
-        char[][] map = new char[3][3];
 
+        // 개수 세기
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                map[i][j] = board[i].charAt(j);
-                if (map[i][j] == 'O') o++;
-                if (map[i][j] == 'X') x++;
+                char c = board[i].charAt(j);
+                if (c == 'O') o++;
+                if (c == 'X') x++;
             }
         }
 
-        if (!(o == x || o == x + 1)) return 0;
+        // 개수 조건
+        if (x > o || o > x + 1) return 0;
 
-        boolean oWin = check(map, 'O');
-        boolean xWin = check(map, 'X');
+        int O = 0, X = 0;
 
-        if (oWin && xWin) return 0; 
-        if (oWin && o != x + 1) return 0; 
-        if (xWin && o != x) return 0;     
+        // 가로
+        for (int i = 0; i < 3; i++) {
+            if (board[i].equals("OOO")) O++;
+            if (board[i].equals("XXX")) X++;
+        }
+
+        // 세로
+        for (int j = 0; j < 3; j++) {
+            if (board[0].charAt(j) == 'O' &&
+                board[1].charAt(j) == 'O' &&
+                board[2].charAt(j) == 'O') O++;
+
+            if (board[0].charAt(j) == 'X' &&
+                board[1].charAt(j) == 'X' &&
+                board[2].charAt(j) == 'X') X++;
+        }
+
+        // 대각선
+        if (board[0].charAt(0) == 'O' &&
+            board[1].charAt(1) == 'O' &&
+            board[2].charAt(2) == 'O') O++;
+
+        if (board[0].charAt(2) == 'O' &&
+            board[1].charAt(1) == 'O' &&
+            board[2].charAt(0) == 'O') O++;
+
+        if (board[0].charAt(0) == 'X' &&
+            board[1].charAt(1) == 'X' &&
+            board[2].charAt(2) == 'X') X++;
+
+        if (board[0].charAt(2) == 'X' &&
+            board[1].charAt(1) == 'X' &&
+            board[2].charAt(0) == 'X') X++;
+
+        // 둘 다 이김
+        if (O > 0 && X > 0) return 0;
+
+        // O가 이겼는데 턴이 맞지 않음
+        if (O > 0 && o == x) return 0;
+
+        // X가 이겼는데 턴이 맞지 않음
+        if (X > 0 && o > x) return 0;
 
         return 1;
-    }
-
-    private boolean check(char[][] map, char c) {
-        for (int i = 0; i < 3; i++) {
-            if (map[i][0] == c && map[i][1] == c && map[i][2] == c) return true;
-            if (map[0][i] == c && map[1][i] == c && map[2][i] == c) return true;
-        }
-        if (map[0][0] == c && map[1][1] == c && map[2][2] == c) return true;
-        if (map[0][2] == c && map[1][1] == c && map[2][0] == c) return true;
-        return false;
     }
 }
