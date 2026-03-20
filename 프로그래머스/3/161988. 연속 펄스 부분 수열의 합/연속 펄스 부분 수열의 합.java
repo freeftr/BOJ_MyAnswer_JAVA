2e13b64893a3d6954b2ad4647a310aa3327fdfb0
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-20 15:03:07 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 16.06 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,37 +1,40 @@
-import java.io.*;
-import java.util.*;
-
 class Solution {
-    
     public long solution(int[] sequence) {
-        long answer1 = Integer.MIN_VALUE;
-        long answer2 = Integer.MIN_VALUE;
+        long answer = 0;
+        long max2 = Integer.MIN_VALUE;
+        long max1 = Integer.MIN_VALUE;
         long sum1 = 0;
         long sum2 = 0;
-        long min1 = 0;
-        long min2 = 0;
+        int N = sequence.length;
         
-        for(int i = 0; i < sequence.length; i++){
-            if(i%2==0){
-                sum1+=sequence[i];  
-            }
-            else{
-                sum1+=-sequence[i];
-            }
-            answer1 = Math.max(sum1-min1,answer1);
-            min1 = Math.min(sum1, min1);
+        for (int i = 0; i < N; i++) {
+            int sign = i % 2 == 0 ? 1 : -1;
+            
+            int val1 = sequence[i] * sign;
+            int val2 = sequence[i] * -sign;
+            
+            sum1 = Math.max(val1, val1 + sum1);
+            sum2 = Math.max(val2, val2 + sum2);
+            
+            max1 = Math.max(max1, sum1);
+            max2 = Math.max(max2, sum2);
         }
-        
-        for(int i = 0; i < sequence.length; i++){
-            if(i%2==0){
-                sum2+=-sequence[i];
-            }
-            else{
-                sum2+=sequence[i];
-            }
-            answer2 = Math.max(sum2-min2,answer2);
-            min2 = Math.min(sum2, min2);
-        }
-        return Math.max(answer1, answer2);
+        return Math.max(max1, max2);
     }
 }
+
+/*
+조건
+- 연속 부분 수열에 같은 길이의 펄수 수열을 원소기리 곱함
+- 펄스 수열 : 1, -1 번갈아 나오는 수열
+
+요구
+- 연속 펄스 부분 수열 중 합이 가장 큰 것
+
+풀이
+- 짝수부 홀수부 나눠서 누적합?
+
+2 3 -6 1 3 -1 2 4
+2 -6 3 2
+3 1 -1 4
+*/
