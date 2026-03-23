dbc30cb1084b1a3105ba96f829b95cffc3c95626
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-23 16:16:37 +0900

    [Gold II] Title: 컵라면, Time: 796 ms, Memory: 69780 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,40 +11,44 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        Problem[] problems = new Problem[N];
+
+        int[][] p = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            problems[i] = new Problem(a, b);
+
+            p[i][0] = Integer.parseInt(st.nextToken());
+            p[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(problems, (p1, p2) -> p1.deadline - p2.deadline);
+        Arrays.sort(p, (a, b) -> a[0] - b[0]);
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (Problem p : problems) {
-            pq.offer(p.ramen);
-            if (pq.size() > p.deadline) {
-                pq.poll();
-            }
+
+        for (int i = 0; i < N; i++) {
+            pq.add(p[i][1]);
+
+            if (pq.size() > p[i][0]) pq.poll();
         }
 
-        int ans = 0;
-        while (!pq.isEmpty()) {
-            ans += pq.poll();
-        }
+        int answer = 0;
 
-        System.out.println(ans);
+        while (!pq.isEmpty()) answer += pq.poll();
+
+        System.out.println(answer);
     }
 }
 
-class Problem {
-    int deadline;
-    int ramen;
+/*
+조건
+- 문제와 데드라인이 있음
+- 문제당 컵라면 존재
 
-    public Problem(int deadline, int ramen) {
-        this.deadline = deadline;
-        this.ramen = ramen;
-    }
-}
+요구
+- 최대 컵라면 수 구하기
+
+풀이
+1. 가장 데드라인 가까운 문제 해결
+2. 만약 여러 개면 점수 높은 순대로.
+3. 시간++ 하고 지난 데드라인 전부 poll
+ */
