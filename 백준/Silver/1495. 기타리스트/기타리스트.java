66b634e398c18990c4527909fcf845605824fb4b
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-23 15:06:02 +0900

    [Silver I] Title: 기타리스트, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -4,10 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] dp, V;
     static int N, S, M;
-    static int[] V;
-    static boolean[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,32 +14,47 @@ public class Main {
         S = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        boolean[][] dp = new boolean[N + 1][M + 1];
         V = new int[N];
-        dp = new boolean[N + 1][M + 1];
+
+        dp[0][S] = true;
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             V[i] = Integer.parseInt(st.nextToken());
         }
-        dp[0][S] = true;
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < N + 1; i++) {
             for (int j = 0; j <= M; j++) {
-                if (dp[i][j]) {
-                    if (j + V[i] <= M) dp[i + 1][j + V[i]] = true;
-                    if (j - V[i] >= 0) dp[i + 1][j - V[i]] = true;
+                if (dp[i - 1][j]) {
+                    if (j + V[i - 1] <= M) dp[i][j + V[i - 1]] = true;
+                    if (j - V[i - 1] >= 0) dp[i][j - V[i - 1]] = true;
                 }
             }
         }
 
-        int max = -1;
-        for (int j = M; j >= 0; j--) {
-            if (dp[N][j]) {
-                max = j;
-                break;
+        for (int i = M; i >= 0; i--) {
+            if (dp[N][i]) {
+                System.out.println(i);
+                return;
             }
         }
 
-        System.out.println(max);
+        System.out.println(-1);
     }
 }
+
+/*
+조건
+- V[i] : i 번째 곡을 연주하기 전에 바꿀 수 있는 볼륨
+- P - V[i] <= i번 곡 <= P + V[i]
+- 0보다 작거나 보다 크게는 못함
+
+요구
+- 마지막 곡을 연주할 수 있는 볼륨 중 최댓값
+
+풀이
+- dp
+- 올 수 있는 상태
+dp[i - 1] + V[i] vs dp[i - 1] - V[i];
+ */
