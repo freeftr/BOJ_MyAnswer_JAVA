2ed662cb5865f1aa121ec92e8c45ef175155c653
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-24 11:12:00 +0900

    [level 2] Title: 호텔 대실, Time: 10.56 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,25 +1,47 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
+        int answer = 0;
+        
+        Arrays.sort(book_time, (a, b) -> a[0].compareTo(b[0]));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        ArrayList<int[]> bookList = new ArrayList<>();
         
-        for (String[] book : book_time) {
-            int start = Integer.parseInt(book[0].split(":")[0]) * 60 + Integer.parseInt(book[0].split(":")[1]);
-            int end = Integer.parseInt(book[1].split(":")[0]) * 60 + Integer.parseInt(book[1].split(":")[1]) + 10;
-            bookList.add(new int[]{start, end});
-        }
-        
-        Collections.sort(bookList, (a, b) -> a[0] - b[0]);
-        
-        for (int i = 0; i < bookList.size(); i++) {
-            if (!pq.isEmpty() && pq.peek() <= bookList.get(i)[0]) {
+        for (String[] time : book_time) {
+            int start = strToInt(time[0]);
+            int end = strToInt(time[1]) + 10;
+            
+            while (!pq.isEmpty() && pq.peek() <= start) {
                 pq.poll();
             }
-            pq.add(bookList.get(i)[1]);
+            
+            pq.add(end);
+            // System.out.println(time[0] + " " + pq.size());
+            
+            answer = Math.max(answer, pq.size());
         }
-        return pq.size();
+        
+        return answer;
+    }
+    
+    static int strToInt(String s) {
+        String[] temp = s.split(":");
+        int h = Integer.parseInt(temp[0]);
+        int m = Integer.parseInt(temp[1]);
+        
+        return h * 60 + m;
     }
 }
+
+/*
+조건
+- 최소한의 객실 사용
+- 10분간 청소 이용
+
+요구
+- 필요한 최소한의 객실 수 
+
+풀이
+- 필요하면 늘리는 방식
+- 현재거를 봤을 때 가장 빨리 끝나는 방을 찾고 없으면 추가
+*/
