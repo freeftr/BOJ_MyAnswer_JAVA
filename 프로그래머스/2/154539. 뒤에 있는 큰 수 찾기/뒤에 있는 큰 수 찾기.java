a6a187d5c212de05012b3afca896a751a6b64834
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-24 11:34:14 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 77.15 ms, Memory: 215 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -3,16 +3,33 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
-        Stack<Integer> stack = new Stack<>();
         Arrays.fill(answer, -1);
-
+        Stack<int[]> stack = new Stack<>();
+        
         for (int i = 0; i < numbers.length; i++) {
-            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
-                answer[stack.pop()] = numbers[i];
+            int cur = numbers[i];
+            
+            while (!stack.isEmpty() && stack.peek()[1] < cur) {
+                int[] temp = stack.pop();
+                int idx = temp[0];
+                
+                answer[idx] = cur;
             }
-            stack.push(i);
+            
+            stack.add(new int[]{i, cur});
         }
-
         return answer;
     }
 }
+
+/*
+조건
+- 뒷 큰수 : 자신보다 뒤에 있는 숫자 중에서 자신보다 크면서 가장 가까이 있는 수
+
+요구
+- 모든 원소에 대한 뒷큰수 구하기
+
+풀이
+- 
+
+*/
