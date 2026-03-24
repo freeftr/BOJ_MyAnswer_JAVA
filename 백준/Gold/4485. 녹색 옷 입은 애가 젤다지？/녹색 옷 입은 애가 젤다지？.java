66fbd4b473752f7ec1f33ee39ae329e66827faa7
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-24 09:14:34 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 224 ms, Memory: 19852 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,79 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-    static int N;
-    static int[][] graph;
-    static int[][] dist;
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-
-    public static class Node implements Comparable<Node> {
-        int x;
-        int y;
-        int cost;
-
-        public Node(int x, int y, int cost) {
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.cost - o.cost;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        int idx = 0;
+        int idx = 1;
         while (true) {
-            idx++;
-            N = Integer.parseInt(br.readLine());
-            graph = new int[N][N];
+            int N = Integer.parseInt(br.readLine());
 
             if (N == 0) break;
+
+            int[][] board = new int[N][N];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    graph[i][j] = Integer.parseInt(st.nextToken());
+                    board[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            System.out.printf("Problem %d: %d\n", idx, dijkstra());
-        }
-    }
 
-    public static int dijkstra() {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        dist = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        dist[0][0] = graph[0][0]; 
-        pq.add(new Node(0, 0, dist[0][0]));
+            int[][] dist = new int[N][N];
 
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(dist[i], Integer.MAX_VALUE);
+            }
 
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if (dist[nx][ny] > dist[now.x][now.y] + graph[nx][ny]) {
-                    dist[nx][ny] = dist[now.x][now.y] + graph[nx][ny];
-                    pq.add(new Node(nx, ny, dist[nx][ny]));
+            dist[0][0] = board[0][0];
+
+            int[] dx = {0, 0, 1, -1};
+            int[] dy = {1, -1, 0, 0};
+
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+            pq.add(new int[]{0, 0, board[0][0]});
+
+            while (!pq.isEmpty()) {
+                int[] cur = pq.poll();
+                int x = cur[0];
+                int y = cur[1];
+                int d = cur[2];
+
+                if (dist[x][y] < d) continue;
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+                    if (dist[nx][ny] > dist[x][y] + board[nx][ny]) {
+                        dist[nx][ny] = dist[x][y] + board[nx][ny];
+                        pq.add(new int[]{nx, ny, dist[nx][ny]});
+                    }
                 }
             }
+
+            sb.append("Problem " + idx + ": " + dist[N - 1][N - 1]).append("\n");
+            idx++;
         }
-        return dist[N - 1][N - 1];
+
+        System.out.println(sb);
     }
 }
+
+/*
+조건
+- N * N
+- 도둑푸피의 크기만큼 소지금을 잃음
+
+요구
+- 최소 손해 구하ㅣ기
+
+풀이
+- 다익스트라
+ */
