10220678de3d06133398037acc2ea7dddcc4be25
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-25 10:38:12 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 344 ms, Memory: 59900 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.java
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.java
@@ -1,54 +1,55 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static int[][] walk, bicycle;
-    static int[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        walk = new int[N][2];
-        bicycle = new int[N][2];
-        dp = new int[N + 1][K + 1];
+        int[][] walk = new int[N + 1][2];
+        int[][] bike = new int[N + 1][2];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             walk[i][0] = Integer.parseInt(st.nextToken());
             walk[i][1] = Integer.parseInt(st.nextToken());
-            bicycle[i][0] = Integer.parseInt(st.nextToken());
-            bicycle[i][1] = Integer.parseInt(st.nextToken());
+            bike[i][0] = Integer.parseInt(st.nextToken());
+            bike[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int[] row : dp) Arrays.fill(row, -1);
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= K; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
         dp[0][0] = 0;
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             for (int j = 0; j <= K; j++) {
-                if (dp[i][j] == -1) {
-                    continue;
+                if (j - walk[i][0] >= 0 && dp[i - 1][j - walk[i][0]] != -1) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - walk[i][0]] + walk[i][1]);
                 }
-
-                if (j + walk[i][0] <= K) {
-                    dp[i + 1][j + walk[i][0]] = Math.max(dp[i + 1][j + walk[i][0]], dp[i][j] + walk[i][1]);
-                }
-
-                if (j + bicycle[i][0] <= K) {
-                    dp[i + 1][j + bicycle[i][0]] = Math.max(dp[i + 1][j + bicycle[i][0]], dp[i][j] + bicycle[i][1]);
+                if (j - bike[i][0] >= 0 && dp[i - 1][j - bike[i][0]] != -1) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - bike[i][0]] + bike[i][1]);
                 }
             }
         }
 
-        int maxMoney = 0;
+        int answer = 0;
         for (int j = 0; j <= K; j++) {
-            maxMoney = Math.max(maxMoney, dp[N][j]);
+            if (dp[N][j] != -1) {
+                answer = Math.max(answer, dp[N][j]);
+            }
         }
 
-        System.out.println(maxMoney);
+        System.out.println(answer);
     }
 }
