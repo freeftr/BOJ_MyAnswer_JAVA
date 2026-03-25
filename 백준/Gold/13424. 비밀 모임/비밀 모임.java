04a01fc5c28af9308c16e8ac532fec32e0d061b7
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-25 10:07:48 +0900

    [Gold IV] Title: 비밀 모임, Time: 444 ms, Memory: 29444 KB -BaekjoonHub

--- 백준/Gold/13424. 비밀 모임/비밀 모임.java
+++ 백준/Gold/13424. 비밀 모임/비밀 모임.java
@@ -1,28 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static ArrayList<ArrayList<int[]>> graph;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
 
-        int t = Integer.parseInt(br.readLine());
-
-        for (int tc = 0; tc < t; tc++) {
+        for (int tc = 0; tc < T; tc++) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
+
             int[][] dist = new int[N + 1][N + 1];
 
-            for (int i = 0; i <= N; i++) {
+            for (int i = 0; i < N + 1; i++) {
                 Arrays.fill(dist[i], Integer.MAX_VALUE);
                 dist[i][i] = 0;
             }
@@ -41,7 +38,6 @@ public class Main {
             int[] friends = new int[K];
 
             st = new StringTokenizer(br.readLine());
-
             for (int i = 0; i < K; i++) {
                 friends[i] = Integer.parseInt(st.nextToken());
             }
@@ -50,40 +46,42 @@ public class Main {
                 for (int i = 1; i <= N; i++) {
                     for (int j = 1; j <= N; j++) {
                         if (dist[i][k] == Integer.MAX_VALUE || dist[k][j] == Integer.MAX_VALUE) continue;
-                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                        dist[i][j] = Math.min(dist[i][k] + dist[k][j], dist[i][j]);
                     }
                 }
             }
 
             int min = Integer.MAX_VALUE;
-            int answer = -1;
+            int answer = 0;
 
             for (int i = 1; i <= N; i++) {
                 int sum = 0;
 
-                for (int friend : friends) {
-                    sum += dist[i][friend];
+                for (int j = 0; j < K; j++) {
+                    if (dist[i][friends[j]] == Integer.MAX_VALUE) continue;
+                    sum += dist[i][friends[j]];
                 }
 
                 if (sum < min) {
-                    min = sum;
                     answer = i;
+                    min = sum;
                 }
             }
 
-            sb.append(answer + "\n");
+            sb.append(answer).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
 
 /*
 조건
-- N개의 방, M개의 통로
-- 양방향
-- 모두한테서 이동거리가 최소가 된느 방 고르기
+- N 개의 방, M개의 양방향 통로
 
 요구
-- 거리 총합 최소 모임 장소 출력
+- 친구들로부터 거리가 최소인 곳 찾기
+
+풀이
+- 플로이드 워셜
  */
