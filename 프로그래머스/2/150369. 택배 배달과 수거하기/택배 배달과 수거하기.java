e75beb8cc2782d4f955489a0636353ea76f82f23
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-26 09:00:06 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 9.96 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        
         int del = 0;
         int pick = 0;
         
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >=0; i--) {
             del += deliveries[i];
             pick += pickups[i];
             
@@ -14,10 +13,11 @@ class Solution {
             while (del > 0 || pick > 0) {
                 del -= cap;
                 pick -= cap;
-                cnt ++;
+                
+                cnt++;
             }
             
-            answer += (i + 1) * cnt * 2;
+            answer += (i + 1) * 2 * cnt;
         }
         
         return answer;
