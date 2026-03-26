029ac370e52c2437fc1fc632702862123893a5b1
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-03-26 10:51:38 +0900

    [Gold II] Title: 물대기, Time: 416 ms, Memory: 28964 KB -BaekjoonHub

--- 백준/Gold/1368. 물대기/물대기.java
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -1,86 +1,83 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int N;
+
+    static ArrayList<Edge> edges = new ArrayList<>();
     static int[] parent;
-    static ArrayList<Edge> Edge_List = new ArrayList<>();
-    public static class Edge implements Comparable<Edge>{
+    static class Edge{
         int from;
         int to;
-        int cost;
+        int c;
 
-        Edge(int from, int to, int cost){
+        Edge(int from, int to, int c) {
             this.from = from;
             this.to = to;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Edge o){
-            return this.cost - o.cost;
+            this.c = c;
         }
     }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
 
-        parent = new int[N+1];
+        parent = new int[N + 1];
 
-        for(int i = 0; i <= N; i++){
+        for (int i = 1; i <= N; i++) {
+            int W = Integer.parseInt(br.readLine());
             parent[i] = i;
+            edges.add(new Edge(0, i, W));
         }
 
-        for(int i = 1; i <= N; i++){
-            int a = Integer.parseInt(br.readLine());
-            Edge_List.add(new Edge(0,i,a));
-        }
-
-        for(int i = 1; i <= N; i++){
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j <= N; j++){
-                //if(i==j)continue;
-                int a = Integer.parseInt(st.nextToken());
-                Edge_List.add(new Edge(i,j,a));
+            for (int j = 1; j <= N; j++) {
+                int c = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(i, j, c));
             }
         }
 
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.c - b.c);
+        pq.addAll(edges);
+        int sum = 0;
 
-        Collections.sort(Edge_List);
+        while (!pq.isEmpty()) {
+            Edge e = pq.poll();
 
-        int cnt = 0;
-        int ans = 0;
-
-        for(Edge edge: Edge_List){
-            int from = edge.from;
-            int to = edge.to;
-            int cost = edge.cost;
-            if(find(from) != find(to)){
-                cnt++;
-                union(to,from);
-                ans+=cost;
+            if (find(e.from) != find(e.to)) {
+                union(e.from, e.to);
+                sum += e.c;
             }
         }
 
-        if(cnt==N){
-            System.out.println(ans);
-        }
+        System.out.println(sum);
     }
 
-    public static int find(int v){
-        if(parent[v]==v) return v;
+    static int find(int v) {
+        if (v == parent[v]) return v;
         return parent[v] = find(parent[v]);
     }
 
-    public static void union(int a, int b){
+    static void union(int a, int b) {
         a = find(a);
         b = find(b);
 
-        if(a>b) parent[a] = b;
-        else parent[b] = a;
+        parent[b] = a;
     }
 }
+
+/*
+조건
+- 물대는 방법
+- 직접 우물
+- 다른 논으로부터 물을 끌어옴
+
+요구
+- 최소 비용으로 모든 논에 물을 대기
+ */
