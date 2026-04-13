706bbfcfe706cb0263884168917b5359f6dba58b
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-04-13 11:55:00 +0900

    [Gold II] Title: 보석 도둑, Time: 1328 ms, Memory: 121256 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,45 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+	/*
+	조건
+	- 가방에는 한개의 보석만 넣기. 대신 무게를 넘으면 안댐.
 
+	요구
+	- 훔칠 수 있는 최대 가격 구하기
+
+	풀이
+	-
+	 */
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 
-		PriorityQueue<int[]> jewels = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+		PriorityQueue<int[]> gems = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int M = Integer.parseInt(st.nextToken());
-			int V = Integer.parseInt(st.nextToken());
-			jewels.offer(new int[]{M, V});
+			int W = Integer.parseInt(st.nextToken());
+
+			gems.add(new int[] { M, W });
 		}
 
 		int[] bags = new int[K];
+
 		for (int i = 0; i < K; i++) {
-			bags[i] = Integer.parseInt(br.readLine());
+			int C = Integer.parseInt(br.readLine());
+			bags[i] = C;
 		}
+
 		Arrays.sort(bags);
 
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
 
 		long answer = 0;
+
 		for (int i = 0; i < K; i++) {
 			int limit = bags[i];
-
-			while (!jewels.isEmpty() && jewels.peek()[0] <= limit) {
-				pq.offer(jewels.poll()[1]);
+			while (!gems.isEmpty() && gems.peek()[0] <= limit) {
+				pq.add(gems.poll()[1]);
 			}
 
-			if (!pq.isEmpty()) {
-				answer += pq.poll();
-			}
+			if (!pq.isEmpty()) answer += pq.poll();
 		}
 
 		System.out.println(answer);
