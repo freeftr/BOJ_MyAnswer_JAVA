aefb8b13eda37ba3704b9db5ca974080a9941818
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-04-15 11:59:10 +0900

    [Gold I] Title: 인터넷 설치, Time: 292 ms, Memory: 22800 KB -BaekjoonHub

--- 백준/Gold/1800. 인터넷 설치/인터넷 설치.java
+++ 백준/Gold/1800. 인터넷 설치/인터넷 설치.java
@@ -1,98 +1,102 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int N, P, K;
+	static ArrayList<ArrayList<int[]>> graph;
+	/*
+	조건
+	- 1번은 인터넷 연결되어 있음
+	- N번이 연결되도록 해야 함.
+	- k개 까지는 연결해줌
+	- 나머지 필요한 것 중에서는 가격이 제일 비싼 것만 받음
 
-    static int N, P, K;
-    static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+	요구
+	- 원장이 내는 최솟값 구하기
 
-    static class Edge implements Comparable<Edge> {
-        int a;
-        int cost;
-        public Edge(int a, int cost) {
-            this.a = a;
-            this.cost = cost;
-        }
-        @Override
-        public int compareTo(Edge o) {
-            return cost - o.cost;
-        }
-    }
+	풀이
+	- 다익스트라를 돌리는데 이제 연결했던 선들을 모두 기록을 해야함
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+		N = Integer.parseInt(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        P = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+		graph = new ArrayList<>();
 
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
 
-        int left = 0;
-        int right = 0;
+		int left = 0;
+		int right = 0;
 
-        for (int i = 0; i < P; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            graph.get(a).add(new Edge(b, cost));
-            graph.get(b).add(new Edge(a, cost));
-            right = Math.max(right, cost);
-        }
+		for (int i = 0; i < P; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-        int ans = -1;
+			graph.get(a).add(new int[] {b, c});
+			graph.get(b).add(new int[] {a, c});
 
-        while (left <= right) {
-            int mid = (right + left) / 2;
-            if (check(mid)) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
+			right = Math.max(right, c);
+		}
 
-        System.out.println(ans);
-    }
+		int answer = -1;
 
-    static boolean check(int mid) {
-        boolean[] visited = new boolean[N + 1];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(1, 0));
-        int[] cnt = new int[N + 1];
-        Arrays.fill(cnt, Integer.MAX_VALUE);
-        cnt[1] = 0;
+		while (left <= right) {
+			int mid = (left + right) / 2;
 
-        while (!pq.isEmpty()) {
-            Edge cur = pq.poll();
-            int v = cur.a;
-            int cost = cur.cost;
+			if (check(mid)) {
+				answer = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
 
-            if (visited[v]) continue;
-            visited[v] = true;
+		System.out.println(answer);
+	}
 
-            for (Edge nv : graph.get(v)) {
-                int next = nv.a;
-                int nextCost = cost;
-                if (nv.cost > mid) {
-                    nextCost += 1;
-                }
+	static boolean check(int k) {
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+		int[] cnt = new int[N + 1];
+		Arrays.fill(cnt, Integer.MAX_VALUE);
+		cnt[1] = 0;
 
+		pq.add(new int[] {1, 0});
 
-                if (nextCost < cnt[next]) {
-                    cnt[next] = nextCost;
-                    pq.add(new Edge(next, cnt[next]));
-                }
-            }
-        }
-        if (cnt[N] <= K) {
-            return true;
-        }
-        return false;
-    }
+		while(!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			int v = cur[0];
+			int d = cur[1];
+
+			if (cnt[v] < d) continue;
+
+			for (int[] n : graph.get(cur[0])) {
+				int nv = n[0];
+				int c = n[1];
+				int nd = d;
+
+				if (c > k) nd++;
+
+				if (cnt[nv] > nd) {
+					cnt[nv] = nd;
+					pq.add(new int[] {nv, nd});
+				}
+			}
+		}
+
+		if (cnt[N] > K) return false;
+		return true;
+	}
 }
