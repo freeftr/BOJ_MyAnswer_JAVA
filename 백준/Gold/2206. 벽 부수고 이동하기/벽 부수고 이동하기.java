886e8e259fbe36b96c1621ed55670e44c1707e1d
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-04-15 11:31:53 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 684 ms, Memory: 143344 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,76 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N, M;
-    static char[][] map;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
+	static int N, M;
+	static String[] map;
+	static int[] dx = {0, 0, 1, -1};
+	static int[] dy = {-1, 1, 0, 0};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+		map = new String[N];
 
-        map = new char[N][M];
+		for(int i = 0; i < N; i++) {
+			map[i] = br.readLine();
+		}
 
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = s.charAt(j);
-            }
-        }
+		Queue<int[]> q = new LinkedList<>();
+		int[][][] dp = new int[N][M][2];
 
-        System.out.println(bfs());
-    }
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				Arrays.fill(dp[i][j], Integer.MAX_VALUE);
+			}
+		}
 
-    static int bfs() {
-        Queue<int[]> q = new ArrayDeque<>();
-        boolean[][][] visited = new boolean[N][M][2];
+		dp[0][0][0] = 1;
+		q.add(new int[] {0, 0, 1, 0});
 
-        q.add(new int[]{0,0,0,1});
-        visited[0][0][0] = true;
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int x = cur[0];
+			int y = cur[1];
+			int d = cur[2];
+			int wall = cur[3];
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int wall = cur[2];
-            int dist = cur[3];
+			if (x == N-1 && y == M-1) {
+				System.out.println(d);
+				return;
+			}
 
-            if (x == N - 1 && y == M - 1) {
-                return dist;
-            }
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (visited[nx][ny][wall]) continue;
+				if (map[nx].charAt(ny) == '1' && wall == 0) {
+					if (dp[nx][ny][1] <= d + 1) continue;
+					dp[nx][ny][1] = d + 1;
+					q.add(new int[] {nx, ny, d + 1, 1});
+				}
 
-                if (wall == 0) {
-                    if (map[nx][ny] == '0') {
-                        q.add(new int[]{nx, ny, 0, dist + 1});
-                        visited[nx][ny][0] = true;
-                    } else {
-                        q.add(new int[]{nx, ny, 1, dist + 1});
-                        visited[nx][ny][1] = true;
-                    }
-                } else {
-                    if (map[nx][ny] == '1') continue;
-                    q.add(new int[]{nx, ny, 1, dist + 1});
-                    visited[nx][ny][1] = true;
-                }
-            }
-        }
-        return -1;
-    }
+				if (map[nx].charAt(ny) == '0') {
+					if (dp[nx][ny][wall] <= d + 1) continue;
+					dp[nx][ny][wall] = d + 1;
+					q.add(new int[] {nx, ny, d + 1, wall});
+				}
+			}
+		}
+
+		// for (int i = 0; i < N; i++) {
+		// 	System.out.println();
+		// 	for (int j = 0; j < M; j++) {
+		// 		System.out.print(dp[i][j][1] + " ");
+		// 	}
+		// }
+
+		System.out.println(-1);
+	}
 }
