2071e739fd47f74659be53998f36c836a818d3da
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-04-15 11:36:36 +0900

    [Gold V] Title: A와 B, Time: 112 ms, Memory: 15816 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -3,31 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-        StringBuilder T = new StringBuilder(br.readLine());
+	/*
+	조건
+	- 문자열 뒤에 A추가
+	- 문자열 뒤집고 뒤에 B추가
 
-        while (!T.toString().equals(S) && T.length() >= S.length()) {
-            if (T.charAt(T.length() - 1) == 'A') {
-                T.deleteCharAt(T.length() - 1);
-            } else {
-                T.deleteCharAt(T.length() - 1);
-                T.reverse();
-            }
-        }
+	요구
+	- S를 T로 만들수 있는지
 
-        System.out.println(T.toString().equals(S) ? 1 : 0);
-    }
+	풀이
+	- T에서 시작
+	- T끝이 B면 B제거 후 뒤집기
+	- T끝이 A면 1번
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		String T = br.readLine();
+
+		while (T.length() >= S.length()) {
+			if (T.charAt(T.length() - 1) == 'B') {
+				T = T.substring(0, T.length() - 1);
+				T = new StringBuilder(T).reverse().toString();
+			} else {
+				T = T.substring(0, T.length() - 1);
+			}
+
+			if (T.equals(S)) {
+				System.out.println(1);
+				return;
+			}
+		}
+		System.out.println(0);
+	}
 }
-
-/*
-조건
-- A와 B로 이루어진 영어 단어.
-- S를 T로 바꿈.
-- 문자열 뒤에 A 추가
-- 문자열 뒤집고 B 추가
-
-요구
-- 만들 수 있는지 없는지.
- */
