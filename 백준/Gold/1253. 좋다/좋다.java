921159009ce4ba68105e74ffb40499791ab73b55
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-04-17 13:18:34 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14940 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,47 +1,70 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	/*
+	조건
+	- 좋다 = 어떤 수가 다른 두수의 합임
+	- 위치가 다르면 다른 수임
+	- 음수도 잇음
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int ans = 0;
+	요구
+	- 좋은 수가 몇개인지
 
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+	풀이
+	- 이분 탐색
 
-        Arrays.sort(arr);
+	 */
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        for (int i = 0; i < N; i++) {
-            int left = 0;
-            int right = N - 1;
-            while (left < right) {
-                if (left == i) {
-                    left++;
-                    continue;
-                }
-                if (right == i) {
-                    right--;
-                    continue;
-                }
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
 
-                if (arr[left] + arr[right] > arr[i]) {
-                    right--;
-                }
-                else if (arr[left] + arr[right] < arr[i]) {
-                    left++;
-                }
-                else {
-                    ans++;
-                    break;
-                }
-            }
-        }
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        System.out.println(ans);
-    }
+		Arrays.sort(arr);
+		int answer = 0;
+
+		for (int i = 0; i < N; i++) {
+			int target = arr[i];
+
+			int left = 0;
+			int right = N - 1;
+			boolean flag = false;
+
+			while(left < right) {
+				if (left == i) {
+					left++;
+					continue;
+				}
+				
+				if (right == i) {
+					right--;
+					continue;
+				}
+
+				int sum = arr[left] + arr[right];
+
+				if (sum == target) {
+					flag = true;
+					break;
+				}
+
+				if (sum > target) right--;
+				else left++;
+			}
+
+			if (flag) answer++;
+		}
+
+		System.out.println(answer);
+	}
 }
