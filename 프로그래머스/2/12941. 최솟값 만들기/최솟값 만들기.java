0061a1f675eedb38ba4ea8e456f7a3ef581c30f9
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-06-22 20:14:19 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.34 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -5,17 +5,12 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-        
+
         Arrays.sort(A);
         Arrays.sort(B);
         
-        int len = A.length;
-        
-        for (int i = 0; i < len; i++) {
-            int a = A[i];
-            int b = B[len - i - 1];
-            
-            answer += a * b;
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - i - 1];
         }
 
         return answer;
@@ -24,15 +19,5 @@ class Solution
 
 /*
 조건
-- 길이가 같은 두 배열 A, B
-- 각각 한 숫자씩 뽑아서 곱한값 누적.
-- 배열의 길이만큼 반복.
-- 누적값이 최소가 되게.
-
-요구
-- 누적된 최솟값 구해라.
-
-풀이
-1. 배열 둘 다 정렬
-2. A 가장 큰값 * B 가장 작은값 + B 가장 작은값 * A 가장 큰값
+- 두 배열에서 한 개씩 뽑아 곱해서 곱하 값의 합의 최소가 되게.
 */
