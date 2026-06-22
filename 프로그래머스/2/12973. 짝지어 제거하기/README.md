50b606c566206fd2900dd74e79772bb0eeb6c991
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-06-22 20:39:37 +0900

    [level 2] Title: 짝지어 제거하기, Time: 51.03 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 63.2 MB, 시간: 61.78 ms
+메모리: 67.5 MB, 시간: 51.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 21일 15:10:27
+2026년 06월 22일 20:39:33
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -2,20 +2,28 @@ import java.util.*;
 
 class Solution
 {
-    public int solution(String s) {
-        
+    public int solution(String s)
+    {
+        int answer = 0;
+
         Stack<Character> stack = new Stack<>();
         
         for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            
             if (stack.isEmpty()) {
-                stack.push(s.charAt(i));
-            } else if (stack.peek() == s.charAt(i)) {
-                stack.pop();
+                stack.add(cur);
             } else {
-                stack.push(s.charAt(i));
+                if (stack.peek() == cur) {
+                    stack.pop();
+                } else {
+                    stack.add(cur);
+                }
             }
         }
         
-        return stack.isEmpty() ? 1 : 0;
+        if (stack.isEmpty()) answer = 1;
+
+        return answer;
     }
 }
