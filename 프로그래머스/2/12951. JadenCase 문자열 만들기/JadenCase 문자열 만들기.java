c5b963185174e6e3dde2bb9829a385fe6ff21a0c
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-06-22 20:07:33 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.08 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,26 +1,29 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
         String answer = "";
-        
+        boolean isFirst = true;
         StringBuilder sb = new StringBuilder();
-        char prev = ' ';
         
         for (int i = 0; i < s.length(); i++) {
-            char now = s.charAt(i);
-            
-            if (now == ' ') {
-                sb.append(" ");
-            } else if (prev == ' ' && now != ' ') {
-                if (Character.isLetter(now)) {
-                    sb.append(Character.toUpperCase(now));
-                } else {
-                    sb.append(now);
-                }
-            } else {
-               sb.append(Character.toLowerCase(now)); 
+            if (s.charAt(i) == ' ') {
+                isFirst = true;
+                sb.append(' ');
+                continue;
             }
             
-            prev = now;
+            if (isFirst) {
+                isFirst = false;
+                
+                if (Character.isLetter(s.charAt(i))) {
+                    sb.append(Character.toUpperCase(s.charAt(i)));
+                } else {
+                    sb.append(s.charAt(i));
+                }
+            } else {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
         }
         
         answer = sb.toString();
@@ -30,12 +33,11 @@ class Solution {
 }
 
 /*
-조건
-- JadenCase = 첫 문자 대문자, 나머지 소문자.
-
-요구
-- s를 JadenCase로 만들어라.
+모든 단어의 첫문자가 대문자, 나머지는 소문자
 
 풀이
-- 문자열 잘라서 해 그냥.
+1. 단어 별로 분할
+2. 단어 별로 맨 앞글자 검사
+3. 단어 별로 나머지 문자 소문자화
+4. 합치기
 */
