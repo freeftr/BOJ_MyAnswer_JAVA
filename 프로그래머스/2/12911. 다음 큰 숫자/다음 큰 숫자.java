d060e55f2563dcdca08d47001b508995a5dc37f2
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-06-22 20:35:26 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,50 +1,22 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int next = n + 1;
         
-        int candidate = n + 1;
-        int one = countOne(n);
-        
-        while (true) {
-            if (countOne(candidate) == one) {
-                answer = candidate;
-                break;
-            } else {
-                candidate++;
+        while (next <= Integer.MAX_VALUE) {
+            if (Integer.bitCount(next) == Integer.bitCount(n)) {
+                return next;
             }
+            
+            next++;
         }
-        
-        answer = candidate;
         
         return answer;
-    }
-    
-    static int countOne(int num) {
-        String s = Integer.toBinaryString(num);
-        
-        int result = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '1') {
-                result++;
-            }
-        }
-        
-        return result;
     }
 }
 
 /*
-조건
-- n보다 큰 자연수
-- 이진수로 변환 시 n과 1의 개수가 동일
-- 위를 만족하는 가장 작은 수
-
-요구
-- 다음 큰 숫자 찾아라
-
-풀이
-- 이진수로 바꾸고 0을 앞에다가 넣으면 되는거 아닌가
-- 문자열에 0이 없으면 -> 맨 앞 1 뒤에 0 추가
-- 문자열에 0이 있으면 -> 0인 곳 다음이 1이면 땅겨옴.
+- n의 다음 큰 숫자 = n보다 큰 자연수
+- n의 다음 큰 숫자와 n은 이진수로 변환 시 1의 개수가 같다.
+- 다큰수는 위 두 개를 만족하는 수중 가장 작은 수
 */
