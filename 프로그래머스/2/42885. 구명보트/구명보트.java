4e572e467fa7228380bdf7c6006654fc6d17d88b
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-06-24 20:10:06 +0900

    [level 2] Title: 구명보트, Time: 13.96 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,20 +1,29 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        
         Arrays.sort(people);
-        int left = 0;
+        
         int right = people.length - 1;
+        int left = 0;
+        
         while (left <= right) {
-            int sum = people[left] + people[right];
+            int sum = people[right] + people[left];
+            // System.out.println(left + " " + right);
             
             if (sum > limit) {
                 answer++;
                 right--;
-            } else if (sum <= limit) {
+            } else if (sum == limit) {
+                answer++;
                 left++;
                 right--;
+            } else {
                 answer++;
+                right--;
+                left++;
             }
         }
         
@@ -23,11 +32,15 @@ class Solution {
 }
 
 /*
-조건
-- 2명씩 밖에 구명보트 못탐.
-- 무게제한 있음.
-- 최대한 적은 구명보트로 구출
+일단 people을 정렬
+무거운 사람잡고 가벼운 사람부터 탐색해서 제거
 
-요구
-- 모든 사람을 구출하기 위해 필요한 구명보트 개수 최솟값 구하기.
+50 50 70 80
+50 80   1
+50 70   1
+50 50   1
+50 70 80
+50 80   1
+50 70   1
+
 */
