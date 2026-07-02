beb6d00226ba1b8b5306c02ed32f18fd5f03eb4d
Pilot Park <130902228+freeftr@users.noreply.github.com>
2026-07-02 21:45:24 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
@@ -8,42 +7,39 @@ class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        parent = new int[n+1];
+        parent = new int[n];
         
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (computers[i][j] == 1 && i != j ) {
+            for (int j = 0; j < i; j++) {
+                if (i == j) continue;
+                if (computers[i][j] == 1) {
                     union(i, j);
                 }
             }
         }
-                    
+        
         HashSet<Integer> set = new HashSet<>();
         
-        for (int i = 0; i < n; i++) {
-            set.add(find(i));
+        for (int p : parent) {
+            set.add(find(p));
         }
         
-        answer = set.size();
-        
-        return answer;
+        return set.size();
+    }
+    
+    static int find(int v) {
+        if (v == parent[v]) return v;
+        return parent[v] = find(parent[v]);
     }
     
     static void union(int a, int b) {
         a = find(a);
         b = find(b);
         
-        parent[a] = b;
-    }
-    
-    static int find(int v) {
-        if (v == parent[v]) {
-            return v;
-        }
-        return parent[v] = find(parent[v]);
+        parent[b] = a;
     }
 }
